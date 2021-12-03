d03fad72ff5aaa295500872d122f912def2e5846
yewenwen3 <357595032@qq.com>
2021-12-03 11:08:34 +0800

    fix: notify

--- src/packages/notify/demo.tsx
+++ src/packages/notify/demo.tsx
@@ -1,12 +1,57 @@
 import React from 'react'
 import { Notify } from './notify'
+import { Cell } from '../cell/cell'
 
 const NotifyDemo = () => {
+  const textNotify = (msg: string) => {
+    // Notify.text(msg)
+  }
+  // const successNotify = (msg: string) => {
+  //   Notify.success(msg)
+  // }
+  // const errorNotify = (msg: string) => {
+  //   Notify.fail(msg)
+  // }
+  // const warningNotify = (msg: string) => {
+  //   Notify.warn(msg)
+  // }
+  // const loadingNotify = (msg: string) => {
+  //   Notify.loading(msg)
+  // }
+  // const duringNotify = (msg: string) => {
+  //   Notify.text(msg, { duration: 10 })
+  // }
+  // const iconNotify = (msg: string) => {
+  //   Notify.customIcon(msg, {
+  //     duration: 0, //duration为0则一直展示
+  //     icon: 'JD',
+  //     id: '',
+  //     center: true, // toast是否居中展示
+  //     type: 'text',
+  //     customClass: '', // 自定义样式名
+  //     bottom: 30, // toast不居中时距离底部位置
+  //     size: 'base', // 设置字体大小，默认base,可选large\small\base
+  //     textAlignCenter: true, // 文字是否居中显示,true为居中，false为left
+  //     bgColor: 'rgba(0, 0, 0, .8)',
+  //     cover: true, //是否展示透明遮罩层
+  //     coverColor: 'rgba(0, 0, 0, 0.4)', // 遮罩颜色设定
+  //     closeOnClickOverlay: true, // 点击遮罩可关闭
+  //     onClose: () => {
+  //       console.log('closeToast')
+  //     },
+  //   })
+  // }
   return (
     <>
       <div className="demo">
         <h2>基础用法</h2>
-        <Notify></Notify>
+        <Cell
+          title="Text文字提示"
+          isLink={true}
+          click={(event: React.MouseEvent) => textNotify('网络失败，请稍后再试~')}
+        >
+          <Notify msg={'test'}></Notify>
+        </Cell>
       </div>
     </>
   )
--- src/packages/notify/notify.tsx
+++ src/packages/notify/notify.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import './notify.scss'
+import classNames from 'classnames'
+import * as ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
 
 export interface NotifyProps {
   id: string
@@ -9,10 +12,10 @@ export interface NotifyProps {
   className: string
   background: string
   type: string
-  visible: boolean
+  showPopup: boolean
   onClick: () => void
   onClose: () => void
-  unmount: () => void
+  unmount: (id: any) => void
 }
 const defaultProps = {
   color: '',
@@ -21,19 +24,85 @@ const defaultProps = {
   className: '',
   background: '',
   type: 'danger',
-  visible: false,
+  showPopup: false,
 } as NotifyProps
 export const Notify: FunctionComponent<
   Partial<NotifyProps> & React.HTMLAttributes<HTMLDivElement>
 > = (props) => {
-  const { children, id, color, msg, duration, className, background, type, visible } = {
+  const {
+    children,
+    id,
+    color,
+    msg,
+    duration,
+    className,
+    background,
+    type,
+    showPopup,
+    onClick,
+    onClose,
+    unmount,
+  } = {
     ...defaultProps,
     ...props,
   }
 
   let timer: null | number = null
-  useEffect(() => {}, [])
-  return <div className="nut-notify">{children ? children : msg}</div>
+  const [mounted, SetMounted] = useState(false)
+
+  useEffect(() => {
+    SetMounted(true)
+    return () => {
+      clearTimer()
+      unmount && unmount(id)
+      onClose && onClose()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (duration) {
+      show()
+    }
+  }, [duration])
+
+  const clickCover = () => {
+    onClick && onClick()
+  }
+  const clearTimer = () => {
+    if (timer) {
+      clearTimeout(timer)
+      timer = null
+    }
+  }
+  const hide = () => {
+    SetMounted(false)
+  }
+  const show = () => {
+    clearTimer()
+    if (duration) {
+      timer = setTimeout(() => {
+        hide()
+      }, duration)
+    }
+  }
+
+  // if (duration) {
+  //   show();
+  // }
+  const classes = classNames({
+    ['popup-top']: true,
+    ['nut-notify']: true,
+    [`nut-notify--${type}`]: true,
+    className,
+  })
+
+  return mounted ? (
+    <React.Fragment>
+      <div className={classes} style={{ color: `${color}`, background: `${background}` }}>
+        {children ? children : msg}
+      </div>
+    </React.Fragment>
+  ) : null
 }
 
 Notify.defaultProps = defaultProps
