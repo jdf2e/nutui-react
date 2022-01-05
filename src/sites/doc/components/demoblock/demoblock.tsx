a52b28cbac0ec1179334307e481a20f82d1d0604
zhenyulei <yulei20080106@126.com>
2022-01-05 11:55:46 +0800

    fix: 增加button的在线调试代码2

--- src/sites/doc/components/demoblock/demoblock.tsx
+++ src/sites/doc/components/demoblock/demoblock.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { compressText, copyCodeHtml, decompressText } from './basedUtil'
 interface A {
   text: string
 }
 const DemoBlock: React.FunctionComponent<A> = (props) => {
+  const [onlineUrl, setOnlineUrl] = useState('')
+  useEffect(() => {
+    const sourceMainReactJsStr = `//import VConsole from "vconsole";
+//var vConsole = new VConsole();
+import React from "react";
+import ReactDOM from "react-dom";
+import '@nutui/nutui-react/dist/style.css'
+import App from "./app.jsx";
+import "./app.scss";
+ReactDOM.render(
+  <App/>,
+  document.getElementById("app")
+);`
+
+    const sourceMainReactJs = compressText(sourceMainReactJsStr)
+    const sourceReactJs = compressText(props.text)
+    const onlineUrl = `https://codehouse.jd.com/?source=share&type=react&mainJs=${sourceMainReactJs}&appValue=${sourceReactJs}&scssValue=`
+    setOnlineUrl(onlineUrl)
+  }, [])
   return (
     <>
       {props.children}
-      <div className="nutui-react--demo-button">{props.text}</div>
+      <div className="online-code nutui-react--demo-button">
+        <div className="online-part">
+          <a className="list" target="_blank" href={onlineUrl} rel="noreferrer">
+            <img
+              className="online-icon"
+              src="https://img12.360buyimg.com/imagetools/jfs/t1/214225/34/8715/7002/61c31bf1E69324ee9/7a452063eba88be4.png"
+            />
+            <div className="online-tips">在线调试</div>
+          </a>
+          <div className="list">
+            <img
+              className="online-icon"
+              src="https://img10.360buyimg.com/imagetools/jfs/t1/142615/10/25537/3671/61c31e6eE3ba7fb90/d1953e2b47e40e86.png"
+            />
+            <div className="online-tips">复制代码</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
