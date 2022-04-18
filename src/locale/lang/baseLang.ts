8296c36c52dae083be5ac7e3f6bcaacf9cbf538c
hanyuxinting <hanyuxinting@sina.cn>
2022-04-18 16:19:02 +0800

    feat: 迁移vue 的多语言版本的实现。

---
+++ src/locale/lang/baseLang.ts
@@ -0,0 +1,76 @@
+export interface BaseLang {
+  save: string
+  confirm: string
+  cancel: string
+  done: string
+  noData: string
+  placeholder: string
+  select: string
+  video: {
+    errorTip: string
+    clickRetry: string
+  }
+  fixednav: {
+    activeText: string
+    unActiveText: string
+  }
+  pagination: {
+    prev: string
+    next: string
+  }
+  calendaritem: {
+    weekdays: Array<string>
+    end: string
+    start: string
+    title: string
+    monthTitle: Function
+    today: string
+  }
+  shortpassword: {
+    title: string
+    desc: string
+    tips: string
+  }
+  uploader: {
+    ready: string
+    readyUpload: string
+    waitingUpload: string
+    uploading: string
+    success: string
+    error: string
+  }
+  countdown: {
+    day: string
+    hour: string
+    minute: string
+    second: string
+  }
+  address: {
+    selectRegion: string
+    deliveryTo: string
+    chooseAnotherAddress: string
+  }
+  signature: {
+    reSign: string
+    unSupportTpl: string
+  }
+  ecard: {
+    chooseText: string
+    otherValueText: string
+    placeholder: string
+  }
+  timeselect: {
+    pickupTime: string
+  }
+  sku: {
+    buyNow: string
+    buyNumber: string
+    addToCard: string
+  }
+  skuheader: {
+    skuId: string
+  }
+  addresslist: {
+    addAddress: string
+  }
+}
