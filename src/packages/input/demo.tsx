5e95962d5b8ae3b1d1ddcbe1f7c548b50c8a1d4b
hanyuxinting <hanyuxinting@sina.cn>
2022-04-22 13:43:02 +0800

    feat: input 添加多语言

--- src/packages/input/demo.tsx
+++ src/packages/input/demo.tsx
@@ -4,9 +4,9 @@ import { Input } from './input'
 const InputDemo = () => {
   const [value1, UpdateValue1] = useState('')
   const [value2, UpdateValue2] = useState('')
-  const [value3, UpdateValue3] = useState('')
-  const [value4, UpdateValue4] = useState('')
-  const [value5, UpdateValue5] = useState('')
+  const [value3] = useState('')
+  const [value4] = useState('')
+  const [value5] = useState('')
   useEffect(() => {
     setTimeout(() => {
       UpdateValue1('异步文本')
@@ -31,7 +31,6 @@ const InputDemo = () => {
       <div className="demo">
         <h2>基础用法</h2>
         <Input label="文本(异步)" defaultValue={value1} focus={focus} blur={blur} />
-
         <Input label="文本" defaultValue={value2} change={change} clear={clear} requireShow />
         <h2>禁用输入框</h2>
         <Input label="文本" defaultValue="禁止修改" disabled />
