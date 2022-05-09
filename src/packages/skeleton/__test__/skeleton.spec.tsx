392f5bc13e142b7aa5090715ab3c4931d4273596
liuyijun17 <3476078473@qq.com>
2022-05-09 16:27:31 +0800

    feat: skeleton组件更新为3.1.20版本并编写测试用例

---
+++ src/packages/skeleton/__test__/skeleton.spec.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import { Skeleton } from '../skeleton'
+
+test('should render with row width array correctly', () => {
+  const { container } = render(<Skeleton width="100px" />)
+  expect(container.querySelector('.blockClass')).toHaveStyle({ width: '100px' })
+})
+
+test('should allow to disable animation', () => {
+  const { container } = render(<Skeleton row={1} animated={false} />)
+  expect(container.querySelector('.skeleton-animation')).toBeFalsy()
+})
+
+test('should change avatar size when using avatarSize prop', () => {
+  const { container } = render(<Skeleton avatar={true} avatarSize="20px" />)
+  expect(container.querySelector('.avatarClass')).toHaveStyle({ width: '20px', height: '20px' })
+  expect(container).toMatchSnapshot()
+})
+
+test('should change avatar shape when using avatarShape prop', () => {
+  const { container } = render(<Skeleton avatar={true} avatarShape="square" />)
+  expect(container.querySelector('.avatarClass')).toMatchSnapshot()
+})
+
+test('should be round when using round prop', () => {
+  const { container } = render(<Skeleton title={true} round={true} avatar={true} />)
+  expect(container.querySelector('.avatarClass')).toBeTruthy()
+})
