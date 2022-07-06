8cf38c94f217c85a0f4967315df6592dd8b18677
zhaoqian16 <547106430@qq.com>
2022-07-06 10:43:40 +0800

    feat: timeselect

---
+++ src/packages/timeselect/demo.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { TimeSelect } from './timeselect'
+import { Cell } from '../cell/cell'
+import Toast from '../toast'
+
+const TimeSelectDemo = () => {
+  const [visible1, SetVisible1] = useState(false)
+  const currentKey = 0
+
+  const dates = [
+    {
+      'pannel-key': '0',
+      date: '5月20日(今天)',
+    },
+    {
+      'pannel-key': '1',
+      date: '5月21日(星期三)',
+    },
+  ]
+  const times = [
+    {
+      key: '0',
+      list: ['9:00-10:00', '10:00-11:00', '11:00-12:00'],
+    },
+    {
+      key: '1',
+      list: ['9:00-10:00', '10:00-11:00'],
+    },
+  ]
+
+  const handleClick = () => {
+    SetVisible1(true)
+  }
+  // 点击弹层 X 或者弹层外区域触发事件
+  const handleSelect = (selectTimeData) => {
+    SetVisible1(false)
+    Toast.text(`您选择了: ${JSON.stringify(selectTimeData)}`)
+  }
+  // 选择日期触发回调事件
+  const handlePannelChange = (pannelKey, selectTimeData) => {
+    console.log('pannelKey, selectTimeData: ', pannelKey, selectTimeData)
+  }
+  // 选择配送时间触发回调事件
+  const handleTimeChange = (time, selectTimeData) => {
+    console.log('time, selectTimeData: ', time, selectTimeData)
+  }
+  return (
+    <>
+      <div className="demo">
+        <h2>用法</h2>
+        <Cell title="请选择配送时间" click={handleClick} />
+        <TimeSelect
+          visible={visible1}
+          height="50%"
+          title="取件时间 1"
+          multiple
+          currentKey={currentKey}
+          dates={dates}
+          times={times}
+          select={handleSelect}
+          pannelChange={handlePannelChange}
+          timeChange={handleTimeChange}
+        />
+      </div>
+    </>
+  )
+}
+
+export default TimeSelectDemo
---
+++ src/packages/timeselect/timeselect.tsx
@@ -0,0 +1,183 @@
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import './timeselect.scss'
+import Popup from '@/packages/popup'
+import TimePannel from '../timepannel'
+import TimeDetail from '../timedetail'
+import bem from '@/utils/bem'
+import { useConfig } from '@/packages/configprovider'
+import { TimeType } from '../timedetail/timedetail'
+
+export interface DateType {
+  'pannel-key'?: string | number
+  date: string
+}
+export interface TimeSelectProps {
+  className?: string
+  visible?: boolean
+  height?: string
+  multiple?: boolean
+  title?: string
+  currentKey: string | number
+  currentTime: TimeType[]
+  dates: DateType[]
+  times: TimeType[]
+  select?: (selectTimeData: TimeType[]) => void
+  pannelChange?: (pannelKey: string | number, selectTimeData: TimeType[]) => void
+  timeChange?: (time: string, selectTimeData: TimeType[]) => void
+}
+const defaultProps = {
+  className: '',
+  visible: false,
+  height: '20%',
+  multiple: false,
+  currentKey: 0,
+  currentTime: [],
+  title: '取件时间',
+  dates: [],
+  times: [],
+} as TimeSelectProps
+export const TimeSelect: FunctionComponent<
+  Partial<TimeSelectProps> & React.HTMLAttributes<HTMLDivElement>
+> = (props) => {
+  const { locale } = useConfig()
+  const {
+    children,
+    visible,
+    className,
+    height,
+    title,
+    currentKey,
+    currentTime,
+    dates,
+    times,
+    multiple,
+    select,
+    pannelChange,
+    timeChange,
+  } = {
+    ...defaultProps,
+    ...props,
+  }
+  const [activeKey, setActiveKey] = useState<string | number>(currentKey)
+  const [activeTime, setActiveTime] = useState<TimeType[]>(
+    currentTime.length
+      ? currentTime
+      : [
+          {
+            key: currentKey,
+            list: [],
+          },
+        ]
+  )
+  const [popVisible, setPopVisible] = useState(visible)
+  const popStyle = {
+    width: '100%',
+    height,
+  }
+  const b = bem('timeselect')
+
+  // popup 的关闭回调, 执行 select 函数更改外部 visible
+  const closeFun = () => {
+    select && select(activeTime)
+  }
+  // 选择配送时间回调
+  const handleSelectTime = (time: string) => {
+    let curTimeData = {} as TimeType
+    let curIndex: string | number = -1
+    for (let i = 0; i < activeTime.length; i++) {
+      if (String(activeTime[i].key) === String(activeKey)) {
+        curTimeData = activeTime[i]
+        curIndex = i
+        break
+      }
+    }
+    const curTimeIndex = curTimeData.list.findIndex((item: string) => String(item) === String(time))
+    if (curTimeIndex === -1) {
+      curTimeData.list.push(time)
+    } else {
+      curTimeData.list.splice(curTimeIndex, 1)
+    }
+    const resultTimeData = [...activeTime]
+    resultTimeData.splice(curIndex, 1, curTimeData)
+    setActiveTime(resultTimeData)
+    timeChange && timeChange(time, resultTimeData)
+  }
+  // 选择日期的回调
+  const handleChange = (pannelKey: string | number) => {
+    const resultTimeData: TimeType[] = [...activeTime]
+    if (String(pannelKey) !== String(activeKey)) {
+      setActiveKey?.(pannelKey)
+      if (multiple) {
+        const curTimeDataIndex = activeTime.findIndex(
+          (item: TimeType) => String(item.key) === String(pannelKey)
+        )
+        if (curTimeDataIndex === -1) {
+          resultTimeData.push({
+            key: pannelKey,
+            list: [],
+          } as TimeType)
+          setActiveTime(resultTimeData)
+        }
+      } else {
+        setActiveTime([
+          {
+            key: pannelKey,
+            list: [],
+          } as TimeType,
+        ])
+      }
+    }
+    pannelChange && pannelChange(pannelKey, resultTimeData)
+  }
+  // 选中的日期增加 active 类名
+  const getTimePannelClass = (dataItem: DateType) => {
+    if (String(dataItem['pannel-key']) === String(activeKey)) {
+      return 'nut-timepannel-active'
+    }
+    return ''
+  }
+  // 根据外部传入 visible 进行组件的显隐展示
+  useEffect(() => {
+    setPopVisible(visible)
+  }, [visible])
+  return (
+    <>
+      <Popup
+        closeable
+        round
+        visible={popVisible}
+        position="bottom"
+        style={popStyle}
+        onClose={() => {
+          closeFun()
+        }}
+      >
+        <div className={`${b()} ${className || ''}`}>
+          <div className="nut-timeselect__title">{title}</div>
+          <div className="nut-timeselect__content">
+            <div className="nut-timeselect__content-left">
+              {dates.map((dataItem: DateType, index: number) => (
+                <TimePannel
+                  date={dataItem.date}
+                  className={getTimePannelClass(dataItem)}
+                  key={String(dataItem['pannel-key'] || index)}
+                  curKey={String(dataItem['pannel-key'] || index)}
+                  change={handleChange}
+                />
+              ))}
+            </div>
+            <TimeDetail
+              times={times}
+              currentKey={String(activeKey)}
+              currentTime={activeTime}
+              select={handleSelectTime}
+            />
+          </div>
+        </div>
+      </Popup>
+    </>
+  )
+}
+
+TimeSelect.defaultProps = defaultProps
+TimeSelect.displayName = 'NutTimeSelect'
