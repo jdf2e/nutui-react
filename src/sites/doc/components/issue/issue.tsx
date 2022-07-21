6bd6e03b70d8b50d882f9b58eba227cf3ea7d998
oasis-cloud <suanbanren@foxmail.com>
2022-07-21 15:46:36 +0800

    chore: 文档语言增加issue跳转

--- src/sites/doc/components/issue/issue.tsx
+++ src/sites/doc/components/issue/issue.tsx
@@ -1,8 +1,32 @@
 import './issue.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Icon from '@/packages/icon'
+import { useLocation } from 'react-router-dom'
+import { nav } from '@/config.json'
 
 export function Issue() {
+  let location = useLocation()
+
+  const getComponentName = () => {
+    const s = window.location.hash.split('/')
+    const cname = s[s.length - 1].toLowerCase()
+    const component: any = {}
+    nav.forEach((item: any) => {
+      item.packages.forEach((sItem: any) => {
+        if (sItem.name.toLowerCase() == cname) {
+          component.name = sItem.name
+          component.cName = sItem.cName
+          return
+        }
+      })
+    })
+    return component
+  }
+  useEffect(() => {
+    const componentName = getComponentName()
+    setComponentName(componentName)
+  }, [location])
+  const [componentName, setComponentName] = useState({ name: '', cName: '' })
   return (
     <>
       <div className="doc-content-issue">
@@ -24,7 +48,7 @@ export function Issue() {
         </a>
         <a
           className="issue-item"
-          href="'https://github.com/jdf2e/nutui-react/issues?q=is:issue+is:closed+' + component"
+          href={`https://github.com/jdf2e/nutui-react/issues?q=is:issue+is:closed+${componentName.name}`}
           target="_blank"
         >
           <Icon name="checklist"></Icon>
