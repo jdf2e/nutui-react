edabf2bbd027009be5d7f594793370ce3145bb86
yangjinjun3 <475473228@qq.com>
2022-07-30 12:42:13 +0800

    feat: add Table

---
+++ src/packages/table/demo.tsx
@@ -0,0 +1,233 @@
+import React, { useState } from 'react'
+import Button from '../button'
+import Toast from '../toast'
+import Icon from '../icon'
+import { Table } from './table'
+import { TableColumnProps } from './types'
+
+const TableDemo = () => {
+  const [columns1, setColumns1] = useState<Array<TableColumnProps>>([
+    {
+      title: '姓名',
+      key: 'name',
+    },
+    {
+      title: '性别',
+      key: 'sex',
+      render: (record: any) => {
+        return (
+          <span style={{ color: record.sex === '女' ? 'blue' : 'green' }}>
+            {record.sex}
+          </span>
+        )
+      },
+    },
+    {
+      title: '学历',
+      key: 'record',
+    },
+  ])
+  const [columns2, setColumns2] = useState<Array<TableColumnProps>>([
+    {
+      title: '姓名',
+      key: 'name',
+      align: 'center',
+    },
+    {
+      title: '性别',
+      key: 'sex',
+    },
+    {
+      title: '学历',
+      key: 'record',
+    },
+  ])
+
+  const [columns4, setColumns4] = useState<Array<TableColumnProps>>([
+    {
+      title: '姓名',
+      key: 'name',
+      align: 'center',
+    },
+    {
+      title: '性别',
+      key: 'sex',
+    },
+    {
+      title: '学历',
+      key: 'record',
+    },
+    {
+      title: '操作',
+      key: 'render',
+    },
+  ])
+
+  const [columns5, setColumns5] = useState<Array<TableColumnProps>>([
+    {
+      title: '姓名',
+      key: 'name',
+      align: 'center',
+      sorter: true,
+    },
+    {
+      title: '性别',
+      key: 'sex',
+    },
+    {
+      title: '学历',
+      key: 'record',
+    },
+    {
+      title: '年龄',
+      key: 'age',
+      sorter: (row1: any, row2: any) => {
+        return row1.age - row2.age
+      },
+    },
+  ])
+
+  const [data1, setData1] = useState([
+    {
+      name: 'Tom',
+      sex: '男',
+      record: '小学',
+    },
+    {
+      name: 'Lucy',
+      sex: '女',
+      record: '本科',
+    },
+    {
+      name: 'Jack',
+      sex: '男',
+      record: '高中',
+    },
+  ])
+
+  const [data2, setData2] = useState([])
+  const [data3, setData3] = useState([] as any)
+  const [data4, setData4] = useState([
+    {
+      name: 'Tom',
+      sex: '男',
+      record: '小学',
+      render: () => {
+        return (
+          <Button
+            onClick={() => Toast.text('hello')}
+            size="small"
+            type="primary"
+          >
+            <div>Hello</div>
+          </Button>
+        )
+      },
+    },
+    {
+      name: 'Lucy',
+      sex: '女',
+      record: '本科',
+      render: () => {
+        return <Icon name="dongdong" size="14px" />
+      },
+    },
+    {
+      name: 'Jack',
+      sex: '男',
+      record: '高中',
+      render: () => {
+        return (
+          <Button
+            type="success"
+            size="small"
+            onClick={() => window.open('https://www.jd.com')}
+          >
+            <div>跳转到京东</div>
+          </Button>
+        )
+      },
+    },
+  ])
+
+  const [data5, setData5] = useState([
+    {
+      name: 'Tom',
+      sex: '男',
+      record: '小学',
+      age: 10,
+    },
+    {
+      name: 'Lucy',
+      sex: '女',
+      record: '本科',
+      age: 30,
+    },
+    {
+      name: 'Jack',
+      sex: '男',
+      record: '高中',
+      age: 4,
+    },
+  ])
+
+  setTimeout(() => {
+    setData3(data1)
+  }, 5000)
+
+  const handleSorter = (item: TableColumnProps, data: Array<any>) => {
+    Toast.text(`${JSON.stringify(item)}`)
+    setData5([...data])
+  }
+
+  return (
+    <>
+      <div className="demo">
+        <h2>基本用法</h2>
+        <Table columns={columns1} data={data1} style={{ background: '#fff' }} />
+        <h2>是否显示边框，文字对齐</h2>
+        <Table
+          columns={columns2}
+          data={data1}
+          bordered={false}
+          style={{ background: '#fff' }}
+        />
+        <h2>显示总结栏</h2>
+        <Table
+          columns={columns1}
+          data={data1}
+          summary="这是总结栏"
+          style={{ background: '#fff' }}
+        />
+        <h2>条纹、明暗交替</h2>
+        <Table
+          columns={columns1}
+          data={data1}
+          style={{ background: '#fff' }}
+          striped
+        />
+        <h2>无数据默认展示，支持自定义</h2>
+        <Table columns={columns1} data={data2} style={{ background: '#fff' }} />
+        <Table
+          columns={columns1}
+          data={data2}
+          style={{ background: '#fff' }}
+          noData="这里是自定义展示"
+        />
+        <h2>自定义单元格</h2>
+        <Table columns={columns4} data={data4} style={{ background: '#fff' }} />
+        <h2>支持异步渲染(5s之后看效果)</h2>
+        <Table columns={columns1} data={data3} style={{ background: '#fff' }} />
+        <h2>支持排序</h2>
+        <Table
+          columns={columns5}
+          data={data5}
+          onSorter={handleSorter}
+          style={{ background: '#fff' }}
+        />
+      </div>
+    </>
+  )
+}
+
+export default TableDemo
---
+++ src/packages/table/table.tsx
@@ -0,0 +1,155 @@
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import './table.scss'
+import classNames from 'classnames'
+import bem from '@/utils/bem'
+import Icon from '../icon'
+import { TableProps, TableColumnProps } from './types'
+
+const defaultProps = {
+  className: '',
+  style: {},
+  columns: [],
+  data: [],
+  bordered: true,
+  striped: false,
+  noData: '无数据',
+} as TableProps
+export const Table: FunctionComponent<
+  Partial<TableProps> & React.HTMLAttributes<HTMLDivElement>
+> = (props) => {
+  const {
+    children,
+    className,
+    style,
+    columns,
+    data,
+    bordered,
+    summary,
+    striped,
+    noData,
+    onSorter,
+    ...rest
+  } = {
+    ...defaultProps,
+    ...props,
+  }
+
+  const [curData, setCurData] = useState(data)
+
+  useEffect(() => {
+    if (data && String(data) !== String(curData)) {
+      setCurData(data)
+    }
+  }, [data])
+
+  const b = bem('table')
+  const classes = classNames({})
+  const cls = classNames(b(), classes, className)
+
+  const handleSorterClick = (item: TableColumnProps) => {
+    if (item.sorter) {
+      onSorter && onSorter(item, curData)
+
+      if (typeof item.sorter === 'function') {
+        setCurData(curData.sort(item.sorter as (a: any, b: any) => number))
+      } else {
+        setCurData(item.sorter === 'default' ? curData.sort() : curData)
+      }
+    }
+  }
+
+  const cellClasses = (item: TableColumnProps) => {
+    return {
+      'nut-table__main__head__tr--border': props.bordered,
+      [`nut-table__main__head__tr--align${item.align ? item.align : ''}`]: true,
+    }
+  }
+
+  const getColumnItem = (value: string): TableColumnProps => {
+    return columns.filter((item: TableColumnProps) => item.key === value)[0]
+  }
+
+  const renderHeadCells = () => {
+    return columns.map((item, index) => {
+      return (
+        <span
+          className={classNames(
+            'nut-table__main__head__tr__th',
+            cellClasses(item)
+          )}
+          key={item.key}
+          onClick={() => handleSorterClick(item)}
+        >
+          {item.title}
+          {item.sorter && <Icon name="down-arrow" size="12px" />}
+        </span>
+      )
+    })
+  }
+
+  const sortDataItem = () => {
+    return columns.map((columns: any) => {
+      return [columns.key, columns.render]
+    })
+  }
+
+  const renderBodyTds = (item: any) => {
+    return sortDataItem().map(([value, render], index) => {
+      return (
+        <span
+          className={classNames(
+            'nut-table__main__body__tr__td',
+            cellClasses(getColumnItem(value))
+          )}
+          key={value}
+        >
+          {typeof item[value] === 'function' || typeof render === 'function' ? (
+            <div>{render ? render(item) : item[value](item)}</div>
+          ) : (
+            item[value]
+          )}
+        </span>
+      )
+    })
+  }
+
+  const renderBoyTrs = () => {
+    return curData.map((item, index) => {
+      return (
+        <div className="nut-table__main__body__tr" key={index}>
+          {renderBodyTds(item)}
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div className={cls} style={style} {...rest}>
+      <div
+        className={classNames('nut-table__main', {
+          'nut-table__main--striped': striped,
+        })}
+      >
+        <div className="nut-table__main__head">
+          <div className="nut-table__main__head__tr">{renderHeadCells()}</div>
+        </div>
+        <div className="nut-table__main__body">{renderBoyTrs()}</div>
+      </div>
+      {summary && (
+        <div className="nut-table__summary">
+          <span className="nut-table__summary__text">{summary}</span>
+        </div>
+      )}
+      {curData.length === 0 && (
+        <div className="nut-table__nodata">
+          <div className="nut-table__nodata">
+            <div className="nut-table__nodata__text">{noData}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+Table.defaultProps = defaultProps
+Table.displayName = 'NutTable'
