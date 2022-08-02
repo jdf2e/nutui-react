43a7bbdc9b8c525f327ce2f4f750a9c050da621d
拧巴的猫 <353833373@qq.com>
2022-08-02 16:22:25 +0800

    feat: animatingnumbers 增加单元测试、文档和demo国际化支持、主题定制支持 (#189)

--- src/packages/animatingnumbers/demo.tsx
+++ src/packages/animatingnumbers/demo.tsx
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { AnimatingNumbers } from './animatingnumbers'
+import { useTranslate } from '../../sites/assets/locale'
 
+interface T {
+  basic: string
+  custom: string
+}
 const AnimatingNumbersDemo = () => {
+  const [translated] = useTranslate<T>({
+    'zh-CN': {
+      basic: '基本用法',
+      custom: '自定义样式，动态修改数据（需要指定最大位数）',
+    },
+    'zh-TW': {
+      basic: '基本用法',
+      custom: '自定義樣式，動態修改數據（需要指定最大位數）',
+    },
+    'en-US': {
+      basic: 'Basic Usage',
+      custom:
+        'Custom styles to dynamically modify data (maximum number of bits required)',
+    },
+  })
   const [endNumber, setEndNumer] = useState('1570.99')
   useEffect(() => {
     const timer = setInterval(() => {
@@ -18,9 +38,9 @@ const AnimatingNumbersDemo = () => {
   return (
     <>
       <div className="demo">
-        <h2>CountUp-基础用法</h2>
+        <h2>CountUp-{translated.basic}</h2>
         <AnimatingNumbers.CountUp endNumber="678.94" />
-        <h2>CountUp-自定义样式，动态修改数据（需要指定最大位数）</h2>
+        <h2>CountUp-{translated.custom}</h2>
         <AnimatingNumbers.CountUp
           endNumber={endNumber}
           easeSpeed={1.2}
