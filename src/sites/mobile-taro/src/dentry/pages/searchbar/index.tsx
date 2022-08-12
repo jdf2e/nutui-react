1a8ad0f27d1c18a8b708fecb161b2afa11675229
hanyuxinting <hanyuxinting@sina.cn>
2022-08-12 10:48:16 +0800

    feat:  searchbar 需调整样式。

---
+++ src/sites/mobile-taro/src/dentry/pages/searchbar/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { useConfig } from '@/packages/configprovider'
+import { SearchBar, Icon } from '@/packages/nutui.react.taro'
+
+const SearchBarDemo = () => {
+  const { locale } = useConfig()
+
+  const [value, setValue] = useState('')
+  const change = (val: string, e: Event) => {
+    setValue(val)
+  }
+  return (
+    <>
+      <div className="demo">
+        <h2>{locale.searchbar.title1}</h2>
+        <SearchBar placeholder={locale.searchbar.basePlaceholder} />
+        <h2>{locale.searchbar.title2}</h2>
+        <SearchBar shape="round" maxLength={5} />
+        <h2>{locale.searchbar.title3}</h2>
+        <SearchBar
+          background="linear-gradient(to right, #9866F0, #EB4D50)"
+          inputBackground="#999"
+          align="right"
+        />
+        <h2>{locale.searchbar.title4}</h2>
+        <SearchBar
+          label={locale.searchbar.text}
+          actionText={locale.searchbar.test}
+        />
+        <h2>{locale.searchbar.title5}</h2>
+        <SearchBar
+          leftoutIcon={<Icon name="heart-fill1" size="14" />}
+          rightoutIcon={<Icon name="star-fill" size="14" />}
+        />
+        <h2>{locale.searchbar.title6}</h2>
+        <SearchBar
+          onChange={(val: string, e: Event) => change(val, e)}
+          maxLength={10}
+        />
+        <span className="val-text">value：{value}</span>
+      </div>
+    </>
+  )
+}
+
+export default SearchBarDemo
