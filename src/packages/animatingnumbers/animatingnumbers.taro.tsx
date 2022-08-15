60cf2314f8105d768636c6b9fa455cfc9464363d
hanyuxinting <hanyuxinting@sina.cn>
2022-08-15 14:19:23 +0800

    feat: 处理动态数字。但未完成适配。

--- src/packages/animatingnumbers/animatingnumbers.taro.tsx
+++ src/packages/animatingnumbers/animatingnumbers.taro.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { CountUp } from './countup'
+import { CountUp } from './countup.taro'
 
 export interface AnimatingNumbersProps {}
 const defaultProps = {} as AnimatingNumbersProps
