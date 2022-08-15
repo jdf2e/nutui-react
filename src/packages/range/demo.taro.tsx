b0305d84f135623ec04130889d959177dd54850e
hanyuxinting <hanyuxinting@sina.cn>
2022-08-15 14:23:04 +0800

    feat: 同步main。将已有的组件，全部构建taro demo版本。

--- src/packages/range/demo.taro.tsx
+++ src/packages/range/demo.taro.tsx
@@ -1,20 +1,104 @@
 import React, { useState } from 'react'
+import { useTranslate } from '@/sites/assets/locale/taro'
 import { Range, Cell, Toast } from '@/packages/nutui.react.taro'
+import '@/packages/avatar/demo.scss'
+
+interface T {
+  title: string
+  title1: string
+  title2: string
+  title3: string
+  title4: string
+  title5: string
+  title6: string
+  title7: string
+  title8: string
+  title9: string
+  title10: string
+}
 
 const RangeDemo = () => {
+  const [translated] = useTranslate<T>({
+    'zh-CN': {
+      title: '基础用法',
+      title1: '双滑块',
+      title2: '指定范围',
+      title3: '设置步长',
+      title4: '隐藏范围',
+      title5: '隐藏标签',
+      title6: '禁用',
+      title7: '自定义样式',
+      title8: '自定义按钮',
+      title9: '垂直方向',
+      title10: '刻度标记',
+    },
+    'en-US': {
+      title: 'Basic Usage',
+      title1: 'Dual thumb',
+      title2: 'Range',
+      title3: 'Step Size',
+      title4: 'Hidden Range',
+      title5: 'Hidden Tag',
+      title6: 'Disabled',
+      title7: 'Custom Style',
+      title8: 'Custom Button',
+      title9: 'Vertical',
+      title10: 'Marks',
+    },
+  })
   const cellStyle = {
     padding: '40px 18px',
   }
-  const [value1, SetValue1] = useState(40)
+  const verticalStyle = {
+    height: '180px',
+    padding: '10px',
+  }
+  const [value0, SetValue0] = useState([30, 60])
+  const [value1, SetValue1] = useState(30)
   const [value2, SetValue2] = useState(60)
-  const change = (value: number, name?: string) => {
+  const [value3, SetValue3] = useState(20)
+  const [value4, SetValue4] = useState([20, 80])
+  const [value5, SetValue5] = useState(60)
+  const [value6, SetValue6] = useState([20, 80])
+  const [value7, SetValue7] = useState(60)
+  const [value8, SetValue8] = useState([20, 80])
+  const [marks, SetMarks] = useState({
+    0: 0,
+    20: 20,
+    40: 40,
+    60: 60,
+    80: 80,
+    100: 100,
+  })
+  const change = (value: any, name?: string) => {
     Toast.text(`当前值：${value}`)
     switch (name) {
+      case 'value0':
+        SetValue0(value)
+        break
       case 'value1':
         SetValue1(value)
         break
       case 'value2':
         SetValue2(value)
+        break
+      case 'value3':
+        SetValue3(value)
+        break
+      case 'value4':
+        SetValue4(value)
+        break
+      case 'value5':
+        SetValue5(value)
+        break
+      case 'value6':
+        SetValue6(value)
+        break
+      case 'value7':
+        SetValue7(value)
+        break
+      case 'value8':
+        SetValue8(value)
         break
       default:
         break
@@ -24,21 +108,21 @@ const RangeDemo = () => {
   return (
     <>
       <div className="demo">
-        <h2>基础用法</h2>
+        <h2>{translated.title}</h2>
         <Cell style={cellStyle}>
           <Range modelValue={40} />
         </Cell>
-        <h2>双滑块</h2>
+        <h2>{translated.title1}</h2>
         <Cell style={cellStyle}>
           <Range
             range
-            modelValue={[30, 60]}
+            modelValue={value0}
             change={(value: any) => {
-              change(value)
+              change(value, 'value0')
             }}
           />
         </Cell>
-        <h2>指定范围</h2>
+        <h2>{translated.title2}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={100}
@@ -49,7 +133,7 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>设置步长</h2>
+        <h2>{translated.title3}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={value1}
@@ -59,7 +143,7 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>隐藏范围</h2>
+        <h2>{translated.title4}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={30}
@@ -69,7 +153,7 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>隐藏标签</h2>
+        <h2>{translated.title5}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={20}
@@ -79,7 +163,7 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>禁用</h2>
+        <h2>{translated.title6}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={50}
@@ -89,7 +173,7 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>自定义样式</h2>
+        <h2>{translated.title7}</h2>
         <Cell style={cellStyle}>
           <Range
             className="test-range"
@@ -103,13 +187,77 @@ const RangeDemo = () => {
             }}
           />
         </Cell>
-        <h2>自定义按钮</h2>
+        <h2>{translated.title8}</h2>
         <Cell style={cellStyle}>
           <Range
             modelValue={value2}
-            button={<div className="custom-button">{value2}</div>}
+            button={<div className="range-custom-button">{value2}</div>}
             change={(value: number) => {
               change(value, 'value2')
+            }}
+          />
+        </Cell>
+        <h2>{translated.title9}</h2>
+        <Cell style={verticalStyle}>
+          <div style={{ width: '150px' }}>
+            <Range
+              modelValue={value3}
+              vertical
+              change={(value: number) => {
+                change(value, 'value3')
+              }}
+            />
+          </div>
+          <div style={{ width: '150px' }}>
+            <Range
+              modelValue={value4}
+              vertical
+              range
+              change={(value: number) => {
+                change(value, 'value4')
+              }}
+            />
+          </div>
+        </Cell>
+        <h2>{translated.title10}</h2>
+        <Cell style={cellStyle}>
+          <Range
+            modelValue={value5}
+            hiddenRange
+            marks={marks}
+            change={(value: number) => {
+              change(value, 'value5')
+            }}
+          />
+        </Cell>
+        <Cell style={cellStyle}>
+          <Range
+            modelValue={value6}
+            marks={marks}
+            range
+            change={(value: number) => {
+              change(value, 'value6')
+            }}
+          />
+        </Cell>
+
+        <Cell style={verticalStyle}>
+          <Range
+            modelValue={value7}
+            vertical
+            hiddenRange
+            marks={marks}
+            change={(value: number) => {
+              change(value, 'value7')
+            }}
+          />
+          <Range
+            modelValue={value8}
+            vertical
+            marks={marks}
+            range
+            change={(value: number) => {
+              change(value, 'value8')
             }}
           />
         </Cell>
---
+++ src/sites/mobile-taro/src/dentry/pages/picker/index.tsx
@@ -0,0 +1,377 @@
+import React, { useState } from 'react'
+import { Cell, Picker } from '@/packages/nutui.react.taro'
+
+interface PickerOption {
+  text: string | number
+  value: string | number
+  disabled?: string
+  children?: PickerOption[]
+  className?: string | number
+}
+
+const PickerDemo = () => {
+  const [isVisible1, setIsVisible1] = useState(false)
+  const [isVisible2, setIsVisible2] = useState(false)
+  const [isVisible3, setIsVisible3] = useState(false)
+  const [isVisible4, setIsVisible4] = useState(false)
+  const [isVisible5, setIsVisible5] = useState(false)
+  const [isVisible6, setIsVisible6] = useState(false)
+
+  const [cityCustmer, setCityCustmer] = useState('')
+  const [baseDesc, setBaseDesc] = useState('')
+  const [baseDefault, setbaseDefault] = useState('')
+  const [mutilDesc, setMutilDesc] = useState('')
+  const [asyncDesc, setasyncDesc] = useState('')
+  const [tileDesc, settileDesc] = useState('')
+
+  const [defaultValue, setDefaultValue] = useState([2])
+
+  const listData1 = [
+    [
+      {
+        value: 1,
+        text: '南京市',
+      },
+      {
+        value: 2,
+        text: '无锡市',
+      },
+      {
+        value: 3,
+        text: '海北藏族自治区',
+      },
+      {
+        value: 4,
+        text: '北京市',
+      },
+      {
+        value: 5,
+        text: '连云港市',
+      },
+      {
+        value: 6,
+        text: '浙江市',
+      },
+      {
+        value: 7,
+        text: '江苏市',
+      },
+      {
+        value: 8,
+        text: '大庆市',
+      },
+      {
+        value: 9,
+        text: '绥化市',
+      },
+      {
+        value: 10,
+        text: '潍坊市',
+      },
+      {
+        value: 11,
+        text: '请按市',
+      },
+      {
+        value: 12,
+        text: '乌鲁木齐市',
+      },
+    ],
+  ]
+
+  const listData2 = [
+    // 第一列
+    [
+      { text: '周一', value: 'Monday' },
+      { text: '周二', value: 'Tuesday' },
+      { text: '周三', value: 'Wednesday' },
+      { text: '周四', value: 'Thursday' },
+      { text: '周五', value: 'Friday' },
+    ],
+    // 第二列
+    [
+      { text: '上午', value: 'Morning' },
+      { text: '下午', value: 'Afternoon' },
+      { text: '晚上', value: 'Evening' },
+    ],
+  ]
+
+  const [custmerCityData, setCustmerCityData] = useState([
+    {
+      value: 1,
+      text: '北京',
+      children: [
+        {
+          value: 1,
+          text: '朝阳区',
+        },
+        {
+          value: 2,
+          text: '海淀区',
+        },
+        {
+          value: 3,
+          text: '大兴区',
+        },
+        {
+          value: 4,
+          text: '东城区',
+        },
+        {
+          value: 5,
+          text: '西城区',
+        },
+        {
+          value: 6,
+          text: '丰台区',
+        },
+      ],
+    },
+    {
+      value: 2,
+      text: '上海',
+      children: [
+        {
+          value: 1,
+          text: '黄浦区',
+        },
+        {
+          value: 2,
+          text: '长宁区',
+        },
+        {
+          value: 3,
+          text: '普陀区',
+        },
+        {
+          value: 4,
+          text: '杨浦区',
+        },
+        {
+          value: 5,
+          text: '浦东新区',
+        },
+      ],
+    },
+  ])
+  // 动态获取
+  const [asyncData, setAsyncData] = useState([
+    {
+      value: 1,
+      text: '北京',
+      children: [
+        {
+          value: 1,
+          text: '朝阳区',
+        },
+        {
+          value: 2,
+          text: '海淀区',
+        },
+        {
+          value: 3,
+          text: '大兴区',
+        },
+        {
+          value: 4,
+          text: '东城区',
+        },
+        {
+          value: 5,
+          text: '西城区',
+        },
+        {
+          value: 6,
+          text: '丰台区',
+        },
+      ],
+    },
+    {
+      value: 2,
+      text: '上海',
+      children: [],
+    },
+  ])
+
+  const setChooseValueCustmer = (
+    values: (string | number)[],
+    options: PickerOption[]
+  ) => {
+    console.log('多级联动确定', values, options)
+    const str = options.map((item) => item.text).join('-')
+    setCityCustmer(str)
+  }
+
+  const setAsyncConfirm = (
+    values: (string | number)[],
+    options: PickerOption[]
+  ) => {
+    console.log('异步获取确定', values, options)
+    const str = options.map((item) => item.text).join('-')
+    setasyncDesc(str)
+  }
+
+  const updateChooseValueCustmer = (
+    columnIndex: number,
+    values: (string | number)[],
+    options: PickerOption[]
+  ) => {
+    console.log('异步获取change', columnIndex, values, options)
+    if (columnIndex === 0 && values[0] === 2) {
+      setTimeout(() => {
+        if (asyncData[1].children.length === 0) {
+          asyncData[1].children = [
+            { value: 1, text: '黄浦区' },
+            { value: 2, text: '长宁区' },
+            { value: 3, text: '普陀区' },
+            { value: 4, text: '杨浦区' },
+            { value: 5, text: '浦东新区' },
+          ]
+
+          setAsyncData([...asyncData])
+        }
+      }, 100)
+    }
+  }
+
+  // 切换选择项
+  const changePicker = (columnIndex: number, values: any, options: any[]) => {
+    console.log('picker选择change', columnIndex, values, options)
+  }
+  // 确定选择
+  const confirmPicker = (
+    type: string,
+    values: (string | number)[],
+    options: PickerOption[]
+  ) => {
+    console.log('picker选择确定', values, options)
+    let desc = ''
+    options.forEach((option: any) => {
+      desc += option.text
+    })
+    if (type === 'base') {
+      setBaseDesc(desc)
+    }
+    if (type === 'mutil') {
+      setMutilDesc(desc)
+    }
+
+    if (type === 'default') {
+      setbaseDefault(desc)
+      setDefaultValue([options[0].value as number])
+    }
+
+    if (type === 'tile') {
+      settileDesc(desc)
+    }
+  }
+  return (
+    <>
+      <div className="demo">
+        <h2>基础用法</h2>
+        <Cell
+          title="请选择城市"
+          desc={baseDesc}
+          onClick={() => setIsVisible1(!isVisible1)}
+        />
+        <Picker
+          title="请选择城市"
+          isVisible={isVisible1}
+          listData={listData1}
+          onConfirm={(values, list) => confirmPicker('base', values, list)}
+          onClose={() => setIsVisible1(false)}
+          onChange={changePicker}
+        />
+
+        <h2>默认选中项</h2>
+        <Cell
+          title="请选择城市"
+          desc={baseDefault}
+          onClick={() => setIsVisible4(!isVisible4)}
+        />
+        <Picker
+          isVisible={isVisible4}
+          listData={listData1}
+          onConfirm={(values, list) => confirmPicker('default', values, list)}
+          defaultValueData={defaultValue}
+          onClose={() => setIsVisible4(false)}
+          onChange={changePicker}
+        />
+
+        <h2>多列用法</h2>
+        <Cell
+          title="多列用法"
+          desc={mutilDesc}
+          onClick={() => setIsVisible2(!isVisible2)}
+        />
+        <Picker
+          isVisible={isVisible2}
+          listData={listData2}
+          onClose={() => setIsVisible2(false)}
+          defaultValueData={['Wednesday']}
+          onChange={changePicker}
+          onConfirm={(values, list) => confirmPicker('mutil', values, list)}
+        />
+        <h2>平铺展示</h2>
+        <Cell
+          title="请选择城市"
+          desc={tileDesc}
+          onClick={() => setIsVisible6(!isVisible6)}
+        />
+        <Picker
+          isVisible={isVisible6}
+          listData={listData1}
+          onConfirm={(values, list) => confirmPicker('tile', values, list)}
+          defaultValueData={defaultValue}
+          threeDimensional={false}
+          onClose={() => setIsVisible6(false)}
+          onChange={changePicker}
+        />
+
+        <h2>多级联动</h2>
+        <Cell
+          title="多级联动"
+          desc={cityCustmer}
+          onClick={() => setIsVisible3(!isVisible3)}
+        />
+
+        <Picker
+          isVisible={isVisible3}
+          listData={custmerCityData}
+          onClose={() => setIsVisible3(false)}
+          onConfirm={(values, list: PickerOption[]) =>
+            setChooseValueCustmer(values, list)
+          }
+          onChange={(
+            columnIndex: number,
+            value: (string | number)[],
+            options: PickerOption[]
+          ) => console.log('多级联动', columnIndex, value, options)}
+        />
+
+        <h2>动态获取</h2>
+        <Cell
+          title="请选择城市"
+          desc={asyncDesc}
+          onClick={() => setIsVisible5(!isVisible5)}
+        />
+
+        <Picker
+          isVisible={isVisible5}
+          listData={asyncData}
+          onClose={() => setIsVisible5(false)}
+          onConfirm={(values, list: PickerOption[]) =>
+            setAsyncConfirm(values, list)
+          }
+          onChange={(
+            columnIndex: number,
+            value: (string | number)[],
+            options: PickerOption[]
+          ) => updateChooseValueCustmer(columnIndex, value, options)}
+        />
+      </div>
+    </>
+  )
+}
+
+export default PickerDemo
