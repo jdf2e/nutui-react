eb59a50c5183c54b9c6e64851af66e458d62ad79
Ymm0008 <being0008@163.com>
2022-08-22 14:17:24 +0800

    fix: 国际化文档优化

--- src/packages/swipe/demo.tsx
+++ src/packages/swipe/demo.tsx
@@ -1,24 +1,136 @@
 import React, { useRef } from 'react'
 import { SwipeInstance, Swipe } from './swipe'
-import { useConfig } from '@/packages/configprovider'
+import { useTranslate } from '../../sites/assets/locale'
 import Cell from '@/packages/cell'
 import Button from '@/packages/button'
 import Toast from '@/packages/toast'
 import Dialog from '@/packages/dialog'
 import InputNumber from '@/packages/inputnumber'
 
+type TSwipeDemo = {
+  title1: string
+  title2: string
+  title3: string
+  title4: string
+  title5: string
+  click: string
+  sure: string
+  del: string
+  choose: string
+  event: string
+  goods: string
+  collect: string
+  open: string
+  close: string
+  tips: string
+  cart: string
+  leftDel: string
+  disabled: string
+  chooseTips: string
+  deleteTips: string
+}
+
 const SwipeDemo = () => {
-  const { locale } = useConfig()
+  const [translated] = useTranslate<TSwipeDemo>({
+    'zh-CN': {
+      title1: '基础用法',
+      title2: '禁用滑动',
+      title3: '事件监听',
+      title4: '非同步控制',
+      title5: '自定义內容',
+      click: '点击',
+      sure: '确定',
+      del: '删除',
+      choose: '选择',
+      event: '事件',
+      goods: '商品',
+      collect: '收藏',
+      open: '打开',
+      close: '关闭',
+      tips: '提示',
+      cart: '加入购物车',
+      leftDel: '左滑删除',
+      disabled: '禁用滑动',
+      chooseTips: '确定选择吗？',
+      deleteTips: '确定删除吗？',
+    },
+    'zh-TW': {
+      title1: '基礎用法',
+      title2: '禁用滑動',
+      title3: '事件監聽',
+      title4: '非同步控制',
+      title5: '自定義內容',
+      click: '點擊',
+      sure: '確定',
+      del: '刪除',
+      choose: '選擇',
+      event: '事件',
+      goods: '商品',
+      collect: '收藏',
+      open: '打開',
+      close: '關閉',
+      tips: '提示',
+      cart: '加入購物車',
+      leftDel: '左滑刪除',
+      disabled: '禁用滑動',
+      chooseTips: '確定選擇嗎？ ',
+      deleteTips: '確定刪除嗎？ ',
+    },
+    'en-US': {
+      title1: 'basic usage',
+      title2: 'Disable sliding',
+      title3: 'Event monitoring',
+      title4: 'Asynchronous control',
+      title5: 'Custom content',
+      click: 'click',
+      sure: 'ok',
+      del: 'delete',
+      choose: 'select',
+      event: 'event',
+      goods: 'goods',
+      collect: 'collect',
+      open: 'open',
+      close: 'close',
+      tips: 'tips',
+      cart: 'add to shopping cart',
+      leftDel: 'left slide delete',
+      disabled: 'Disable sliding',
+      chooseTips: 'are you sure to choose?',
+      deleteTips: 'are you sure to delete?',
+    },
+    'id-ID': {
+      title1: 'penggunaan dasar',
+      title2: 'Lumpuhkan sliding',
+      title3: 'Monitor waktu',
+      title4: 'kontrol asinkron',
+      title5: 'isi suai',
+      click: 'klik',
+      sure: 'OK',
+      del: 'Hapus',
+      choose: 'pilih',
+      event: 'peristiwa',
+      goods: 'barang',
+      collect: 'kumpulkan',
+      open: 'buka',
+      close: 'tutup',
+      tips: 'tip',
+      cart: 'tambah ke kereta belanja',
+      leftDel: 'padam slide kiri',
+      disabled: 'Lumpuhkan sliding',
+      chooseTips: 'Apakah kamu benar-benar memilih?',
+      deleteTips: 'Apakah Anda yakin untuk menghapus?',
+    },
+  })
 
   const refDom = useRef<SwipeInstance>(null)
   const handleChange = () => {
-    Toast.text(locale.swipe.click)
+    Toast.text(translated.click)
   }
   const beforeClose = (postion: string) => {
     Dialog.alert({
-      title: locale.swipe.tips,
+      title: translated.tips,
       content:
-        postion === 'left' ? locale.swipe.chooseTips : locale.swipe.deleteTips,
+        postion === 'left' ? translated.chooseTips : translated.deleteTips,
       onOk: () => {
         refDom.current && refDom.current.close()
       },
@@ -28,75 +140,75 @@ const SwipeDemo = () => {
   return (
     <>
       <div className="demo">
-        <h2>{locale.swipe.title1}</h2>
+        <h2>{translated.title1}</h2>
         <Swipe
           rightAction={
             <Button type="primary" shape="square">
-              {locale.swipe.del}
+              {translated.del}
             </Button>
           }
         >
-          <Cell title={locale.swipe.leftDel} roundRadius={0} />
+          <Cell title={translated.leftDel} roundRadius={0} />
         </Swipe>
-        <h2>{locale.swipe.title2}</h2>
+        <h2>{translated.title2}</h2>
         <Swipe
           rightAction={
             <Button shape="square" type="danger">
-              {locale.swipe.del}
+              {translated.del}
             </Button>
           }
           disabled
         >
-          <Cell title={locale.swipe.disabled} roundRadius={0} />
+          <Cell title={translated.disabled} roundRadius={0} />
         </Swipe>
-        <h2>{locale.swipe.title3}</h2>
+        <h2>{translated.title3}</h2>
         <Swipe
           leftAction={
             <Button shape="square" type="success">
-              {locale.swipe.choose}
+              {translated.choose}
             </Button>
           }
           rightAction={
             <>
               <Button shape="square" type="danger">
-                {locale.swipe.del}
+                {translated.del}
               </Button>
               <Button shape="square" type="info">
-                {locale.swipe.collect}
+                {translated.collect}
               </Button>
             </>
           }
           onActionClick={handleChange}
-          onOpen={() => Toast.text(locale.swipe.open)}
-          onClose={() => Toast.text(locale.swipe.close)}
+          onOpen={() => Toast.text(translated.open)}
+          onClose={() => Toast.text(translated.close)}
         >
-          <Cell title={locale.swipe.event} />
+          <Cell title={translated.event} />
         </Swipe>
-        <h2>{locale.swipe.title4}</h2>
+        <h2>{translated.title4}</h2>
         <Swipe
           ref={refDom}
           beforeClose={beforeClose}
           leftAction={
             <Button shape="square" type="success">
-              {locale.swipe.choose}
+              {translated.choose}
             </Button>
           }
           rightAction={
             <>
               <Button shape="square" type="danger">
-                {locale.swipe.del}
+                {translated.del}
               </Button>
             </>
           }
         >
-          <Cell title={locale.swipe.event} />
+          <Cell title={translated.event} />
         </Swipe>
-        <h2>{locale.swipe.title5}</h2>
+        <h2>{translated.title5}</h2>
         <Swipe
           rightAction={
             <>
               <Button shape="square" type="danger">
-                {locale.swipe.cart}
+                {translated.cart}
               </Button>
             </>
           }
@@ -109,7 +221,7 @@ const SwipeDemo = () => {
                 width: '100%',
               }}
             >
-              <span>{locale.swipe.goods}</span>
+              <span>{translated.goods}</span>
               <InputNumber style={{ float: 'right' }} />
             </div>
           </Cell>
