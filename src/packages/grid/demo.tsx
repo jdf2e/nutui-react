0c1e0e8376a70b1fb55e185b1f203d3e59134e30
yangxiaolu3 <962468124@qq.com>
2022-08-29 17:39:17 +0800

    fix: grid组件demo引入toast文件修改

--- src/packages/grid/demo.tsx
+++ src/packages/grid/demo.tsx
@@ -3,7 +3,7 @@ import Avatar from '../avatar'
 import { Grid } from './grid'
 import GridItem from '../griditem'
 import { useTranslate } from '../../sites/assets/locale'
-import Toast from '../toast/index.taro'
+import Toast from '../toast'
 
 interface T {
   basic: string
@@ -54,7 +54,6 @@ const GridDemo = () => {
   })
 
   const handleClick = () => {
-    // console.log('点击了第几个')
     Toast.text('点击了第几个')
   }
   return (
