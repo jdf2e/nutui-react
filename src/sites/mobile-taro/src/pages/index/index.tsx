7bba7877dc9c496bb03a751f6f080680e54674d2
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2022-09-21 10:10:07 +0800

    Feat: 适配 Taro h5。处理样式兼容问题。 (#307)
    * chore: 升级Taro版本。
    
    * chore: 调整@tarojs/cli
    
    * feat: 适配Taro h5,主要是样式处理

--- src/sites/mobile-taro/src/pages/index/index.tsx
+++ src/sites/mobile-taro/src/pages/index/index.tsx
@@ -30,19 +30,20 @@ const Index = () => {
 
   return (
     <>
-      <div className="index">
-        <div className="index-header">
+      <div className="nut-demo-index">
+        <div className="nut-demo-index-header">
           <img
             src={`https://img14.360buyimg.com/imagetools/jfs/t1/117879/25/28831/6279/6329723bE66715a2f/5f099b8feca9e8cc.png`}
             alt=""
             srcSet=""
+            className="nut-demo-img"
           />
-          <div className="info">
-            <h1>NutUI React</h1>
+          <div className="nut-demo-info">
+            <h1 className="nut-demo-title">NutUI React</h1>
             <p>京东风格的轻量级小程序组件库 React 版</p>
           </div>
         </div>
-        <div className="index-components">
+        <div className="nut-demo-index-components">
           {navs.map((nav) => (
             <ol key={nav.name}>
               <li>{nav.name}</li>
