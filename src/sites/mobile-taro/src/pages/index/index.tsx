169d4a7913bc502378923d200f8fa0f628e7ef72
hanyuxinting <hanyuxinting@sina.cn>
2022-09-21 11:57:34 +0800

    feat: 改样式配置项,删除冗余代码

--- src/sites/mobile-taro/src/pages/index/index.tsx
+++ src/sites/mobile-taro/src/pages/index/index.tsx
@@ -30,20 +30,19 @@ const Index = () => {
 
   return (
     <>
-      <div className="nut-demo-index">
-        <div className="nut-demo-index-header">
+      <div className="index">
+        <div className="index-header">
           <img
             src={`https://img14.360buyimg.com/imagetools/jfs/t1/117879/25/28831/6279/6329723bE66715a2f/5f099b8feca9e8cc.png`}
             alt=""
             srcSet=""
-            className="nut-demo-img"
           />
-          <div className="nut-demo-info">
-            <h1 className="nut-demo-title">NutUI React</h1>
+          <div className="info">
+            <h1>NutUI React</h1>
             <p>京东风格的轻量级小程序组件库 React 版</p>
           </div>
         </div>
-        <div className="nut-demo-index-components">
+        <div className="index-components">
           {navs.map((nav) => (
             <ol key={nav.name}>
               <li>{nav.name}</li>
