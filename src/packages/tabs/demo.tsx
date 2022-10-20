f48902c75ec9abe14b633cd8e533c73b1ca016f6
雷雨 <leiyu0932@163.com>
2022-10-20 19:14:44 +0800

    feat: tabs组件支持css粘性布局 #345 (#346)

--- src/packages/tabs/demo.tsx
+++ src/packages/tabs/demo.tsx
@@ -15,6 +15,7 @@ interface T {
   title7: string
   title8: string
   title9: string
+  title10: string
   custom1: string
   custom2: string
   custom3: string
@@ -33,6 +34,7 @@ const TabsDemo = () => {
       title7: '标签栏字体尺寸 large normal small',
       title8: '自定义标签栏',
       title9: 'Tabpane 自动高度',
+      title10: 'Css 粘性布局',
       custom1: '自定义 1',
       custom2: '自定义 2',
       custom3: '自定义 3',
@@ -48,6 +50,7 @@ const TabsDemo = () => {
       title7: 'tab bar font size large normal small',
       title8: 'custom tab bar',
       title9: 'Tabpane auto height',
+      title10: 'Css Sticky',
       custom1: 'custom 1',
       custom2: 'custom 2',
       custom3: 'custom 3',
@@ -133,6 +136,40 @@ const TabsDemo = () => {
           <TabPane title="Tab 2" pane-key="1" disabled>
             {' '}
             Tab 2{' '}
+          </TabPane>
+          <TabPane title="Tab 3" pane-key="2">
+            {' '}
+            Tab 3{' '}
+          </TabPane>
+        </Tabs>
+        <h2>{translated.title10}</h2>
+        <Tabs
+          value={tab2value}
+          tabStyle={{ position: 'sticky', top: '0px', zIndex: 1 }}
+          onChange={({ paneKey }) => {
+            setTab2value(paneKey)
+          }}
+        >
+          <TabPane title="Tab 1" pane-key="0">
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+            <p>Tab 1</p>
+          </TabPane>
+          <TabPane title="Tab 2" pane-key="1">
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
+            <p>Tab 2</p>
           </TabPane>
           <TabPane title="Tab 3" pane-key="2">
             {' '}
