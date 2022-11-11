50ca2956215882c1fac32168701df14a60c8b597
cuicuiworld <407060373@qq.com>
2022-11-11 16:48:10 +0800

    fix: 修复TabBar组件路由跳转失败问题

--- src/packages/tabbaritem/tabbaritem.taro.tsx
+++ src/packages/tabbaritem/tabbaritem.taro.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import Taro from '@tarojs/taro'
 
 import bem from '@/utils/bem'
 import Icon from '@/packages/icon/index.taro'
@@ -65,7 +65,6 @@ export const TabbarItem: FunctionComponent<Partial<TabbarItemProps>> = (
   }
   const b = bem('tabbar-item')
   const bIcon = bem('tabbar-item__icon-box')
-  const history = useHistory()
 
   useEffect(() => {
     if (active && href) {
@@ -73,9 +72,11 @@ export const TabbarItem: FunctionComponent<Partial<TabbarItemProps>> = (
       return
     }
     if (active && to) {
-      history.push(to)
+      Taro.navigateTo({
+        url: to,
+      })
     }
-  }, [active, history, href, to])
+  }, [active, href, to])
 
   return (
     <div
