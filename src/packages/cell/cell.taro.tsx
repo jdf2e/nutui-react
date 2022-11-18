6086e7b9eead40a4c1555c958bebce24bb6a02ab
oasis <835865303@qq.com>
2022-11-18 17:45:00 +0800

    fix: cell 组件在 taro 包中的跳转改为 Taro 路由的支持

--- src/packages/cell/cell.taro.tsx
+++ src/packages/cell/cell.taro.tsx
@@ -79,21 +79,9 @@ export const Cell: FunctionComponent<
   const b = bem('cell')
   const history = useHistory()
   const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    click(event)
     onClick(event)
-    if (to && history) {
-      history[replace ? 'replace' : 'push'](to)
-    } else if (url) {
-      if (
-        url.startsWith('https://') ||
-        url.startsWith('http://') ||
-        url.startsWith('//')
-      ) {
-        replace ? window.location.replace(url) : (window.location.href = url)
-      } else {
-        Taro.navigateTo({ url })
-      }
-    }
+    const link = to || url
+    replace ? Taro.redirectTo({ url: link }) : Taro.navigateTo({ url: link })
   }
 
   const baseStyle = {
