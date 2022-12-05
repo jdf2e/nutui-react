38ab934cfde784fdb1b0ed8e1b1e962642e0bb52
oasis-cloud <suanbanren@foxmail.com>
2022-12-05 09:41:31 +0800

    fix: 本地启动环境增加样式

--- src/sites/mobile/main.tsx
+++ src/sites/mobile/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
+import '@/styles/theme-default.scss'
 import '@/sites/assets/styles/reset.scss'
 
 import('../../packages/nutui.react.scss')
