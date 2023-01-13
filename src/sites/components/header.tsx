eba6c578bbd1f5a85d601c0e80fb467aa8dda1b8
oasis-cloud <suanbanren@foxmail.com>
2023-01-13 09:57:38 +0800

    chore: taro h5 demo 外壳修改

--- src/sites/components/header.tsx
+++ src/sites/components/header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Taro from '@tarojs/taro'
 import './header.scss'
 import config from '../../config.json'
+import Icon from '@/packages/icon'
 
 const Header = () => {
   const navigateTo = () => {
@@ -13,7 +14,6 @@ const Header = () => {
   const compName = () => {
     let allComps: any = []
     let hashCompName = location.hash.split('pages/')[1].replace('/index', '')
-    console.log(hashCompName)
     config.nav.map((item: any) => {
       allComps = [...allComps, ...item.packages]
     })
@@ -30,7 +30,8 @@ const Header = () => {
       {Taro.getEnv() === 'WEB' ? (
         <div className="applets-demo-header">
           <div className="back" onClick={navigateTo}>
-            <span className="home-icon"></span>
+            {/*<span className="home-icon"></span>*/}
+            <Icon name="left"></Icon>
           </div>
           <div className="applets-icon">
             <img src="https://img13.360buyimg.com/imagetools/jfs/t1/67106/30/23857/9375/63b4df85Fce5fd959/35265019206515fe.png" />
