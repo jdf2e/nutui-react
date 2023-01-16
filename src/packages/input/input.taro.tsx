6738654e4c530b1da1667736ff23bd2c52e0a909
yewenwen3 <357595032@qq.com>
2023-01-16 09:49:32 +0800

    fix: 修复input组件onChange事件循环问题；修复错误提示样式问题；

--- src/packages/input/input.taro.tsx
+++ src/packages/input/input.taro.tsx
@@ -380,101 +380,103 @@ export const Input: FunctionComponent<
             </div>
           ) : null}
           <div className="nut-input-value">
-            <div
-              className="nut-input-inner"
-              onClick={(e) => {
-                handleClickInput(e)
-              }}
-            >
-              {type === 'textarea' ? (
-                <textarea
-                  name={name}
-                  className="input-text"
-                  ref={inputRef}
-                  style={{
-                    textAlign: inputAlign,
-                    height: `${Number(rows) * 24}px`,
-                  }}
-                  maxLength={maxlength}
-                  placeholder={placeholder || locale.placeholder}
-                  disabled={disabled}
-                  readOnly={readonly}
-                  value={inputValue}
-                  autoFocus={autofocus}
-                  onBlur={(e: any) => {
-                    handleBlur(e)
-                  }}
-                  onFocus={(e: any) => {
-                    handleFocus(e)
-                  }}
-                  onInput={(e: any) => {
-                    handleInput(e)
-                  }}
-                />
-              ) : (
-                <input
-                  {...rest}
-                  name={name}
-                  className="input-text"
-                  ref={inputRef}
-                  style={{ textAlign: inputAlign }}
-                  type={inputType(type)}
-                  maxLength={maxlength}
-                  placeholder={placeholder || locale.placeholder}
-                  disabled={disabled}
-                  readOnly={readonly}
-                  value={inputValue}
-                  autoFocus={autofocus}
-                  onBlur={(e: any) => {
-                    handleBlur(e)
-                  }}
-                  onFocus={(e: any) => {
-                    handleFocus(e)
-                  }}
-                  onInput={(e: any) => {
-                    handleInput(e)
-                  }}
-                />
-              )}
-              {clearable && !readonly && active && inputValue.length > 0 ? (
-                <Icon
-                  classPrefix={iconClassPrefix}
-                  fontClassName={iconFontClassName}
-                  className="nut-input-clear"
-                  name={clearIcon}
-                  size={clearSize}
-                  onClick={(e) => {
-                    handleClear(e)
-                  }}
-                />
-              ) : null}
-            </div>
-            {rightIcon && rightIcon.length > 0 ? (
+            <div className="nut-input-main-con">
               <div
-                className="nut-input-right-icon"
+                className="nut-input-inner"
                 onClick={(e) => {
-                  handleClickRightIcon(e)
+                  handleClickInput(e)
                 }}
               >
-                <Icon
-                  classPrefix={iconClassPrefix}
-                  fontClassName={iconFontClassName}
-                  name={rightIcon}
-                  size={rightIconSize}
-                />
+                {type === 'textarea' ? (
+                  <textarea
+                    name={name}
+                    className="input-text"
+                    ref={inputRef}
+                    style={{
+                      textAlign: inputAlign,
+                      height: `${Number(rows) * 24}px`,
+                    }}
+                    maxLength={maxlength}
+                    placeholder={placeholder || locale.placeholder}
+                    disabled={disabled}
+                    readOnly={readonly}
+                    value={inputValue}
+                    autoFocus={autofocus}
+                    onBlur={(e: any) => {
+                      handleBlur(e)
+                    }}
+                    onFocus={(e: any) => {
+                      handleFocus(e)
+                    }}
+                    onInput={(e: any) => {
+                      handleInput(e)
+                    }}
+                  />
+                ) : (
+                  <input
+                    {...rest}
+                    name={name}
+                    className="input-text"
+                    ref={inputRef}
+                    style={{ textAlign: inputAlign }}
+                    type={inputType(type)}
+                    maxLength={maxlength}
+                    placeholder={placeholder || locale.placeholder}
+                    disabled={disabled}
+                    readOnly={readonly}
+                    value={inputValue}
+                    autoFocus={autofocus}
+                    onBlur={(e: any) => {
+                      handleBlur(e)
+                    }}
+                    onFocus={(e: any) => {
+                      handleFocus(e)
+                    }}
+                    onInput={(e: any) => {
+                      handleInput(e)
+                    }}
+                  />
+                )}
+                {clearable && !readonly && active && inputValue.length > 0 ? (
+                  <Icon
+                    classPrefix={iconClassPrefix}
+                    fontClassName={iconFontClassName}
+                    className="nut-input-clear"
+                    name={clearIcon}
+                    size={clearSize}
+                    onClick={(e) => {
+                      handleClear(e)
+                    }}
+                  />
+                ) : null}
               </div>
-            ) : null}
-            {slotButton ? (
-              <div className="nut-input-button">{slotButton}</div>
-            ) : null}
-            {showWordLimit && maxlength ? (
-              <div className="nut-input-word-limit">
-                <span className="nut-input-word-num">
-                  {inputValue ? inputValue.length : 0}
-                </span>
-                /{maxlength}
-              </div>
-            ) : null}
+              {rightIcon && rightIcon.length > 0 ? (
+                <div
+                  className="nut-input-right-icon"
+                  onClick={(e) => {
+                    handleClickRightIcon(e)
+                  }}
+                >
+                  <Icon
+                    classPrefix={iconClassPrefix}
+                    fontClassName={iconFontClassName}
+                    name={rightIcon}
+                    size={rightIconSize}
+                  />
+                </div>
+              ) : null}
+              {slotButton ? (
+                <div className="nut-input-button">{slotButton}</div>
+              ) : null}
+              {showWordLimit && maxlength ? (
+                <div className="nut-input-word-limit">
+                  <span className="nut-input-word-num">
+                    {inputValue ? inputValue.length : 0}
+                  </span>
+                  /{maxlength}
+                </div>
+              ) : null}
+            </div>
             {errorMessage ? (
               <div
                 className="nut-input-error-message"
