8d62adcef87a7feae8706317c6bdd03d0b541096
oasis-cloud <suanbanren@foxmail.com>
2023-02-15 09:46:59 +0800

    chore: 本地开发切换到模拟器后，刷新页面跳转到demo (#668)

--- src/sites/doc/main.tsx
+++ src/sites/doc/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import { isMobile } from '@/sites/assets/util'
 import '@/sites/assets/styles/reset.scss'
 import '@/styles/theme-default.scss'
 import '@/sites/assets/styles/md-style.scss'
 import App from './App'
+
+if (isMobile) {
+  let url = location.hash
+    .replace('/zh-CN/', '')
+    .replace('/en-US/', '')
+    .replace('component/', '')
+  location.replace('demo.html' + url)
+}
 
 const rootElement = document.querySelector('#doc')
 
