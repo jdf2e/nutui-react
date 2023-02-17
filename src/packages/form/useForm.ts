f91a6058f3023b8c34cf82417800e7042adcc639
junjun666 <3476078473@qq.com>
2023-02-17 19:58:41 +0800

    feat: 新增form-item的validator校验和重置提示状态功能

--- src/packages/form/useForm.ts
+++ src/packages/form/useForm.ts
@@ -116,7 +116,13 @@ class FormStore {
     }
   }
 
-  resetFields = () => {}
+  resetFields = () => {
+    this.errList.length = 0
+
+    this.fieldEntities.forEach((enetity: FieldEntity) => {
+      enetity.onStoreChange()
+    })
+  }
 
   getForm = () => {
     return {
@@ -124,6 +130,7 @@ class FormStore {
       registerField: this.registerField,
       getFieldValue: this.getFieldValue,
       setFieldsValue: this.setFieldsValue,
+      resetFields: this.resetFields,
       submit: this.submit,
       store: this.store,
       errList: this.errList,
