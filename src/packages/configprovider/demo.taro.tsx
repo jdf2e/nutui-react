79f3e1ebbea77fba1c7e8c957e32c87221e48ac7
eiinu <44915689+eiinu@users.noreply.github.com>
2023-02-20 17:39:07 +0800

    feat(configprovider): demo 增加国际化 (#706)

--- src/packages/configprovider/demo.taro.tsx
+++ src/packages/configprovider/demo.taro.tsx
@@ -9,6 +9,7 @@ import {
 } from '@/packages/nutui.react.taro'
 import enUS from '@/locales/en-US'
 import Header from '@/sites/components/header'
+import { useTranslate } from '@/sites/assets/locale/taro'
 import Taro from '@tarojs/taro'
 
 const darkTheme = {
@@ -17,19 +18,42 @@ const darkTheme = {
   nutuiBrandColorEnd: 'green',
 }
 const ConfigProviderDemo = () => {
+  const [translated] = useTranslate({
+    'zh-CN': {
+      title1: 'Textarea 默认',
+      title2: 'Textarea 英文',
+      defaultTheme: '默认主题',
+      customTheme: '定制主题',
+      submit: '提交',
+    },
+    'zh-TW': {
+      title1: '默認用法',
+      title2: 'Textarea 英文',
+      defaultTheme: '默認主題',
+      customTheme: '定制主題',
+      submit: '提交',
+    },
+    'en-US': {
+      title1: 'Textarea default',
+      title2: 'Textarea en-US',
+      customTheme: 'Custom Theme',
+      defaultTheme: 'Default Theme',
+      submit: 'Submit',
+    },
+  })
   return (
     <>
       <Header />
       <div className={`demo ${Taro.getEnv() === 'WEB' ? 'web' : ''}`}>
-        <h2>Textarea 默认</h2>
+        <h2>{translated.title1}</h2>
         <ConfigProvider>
           <TextArea disabled limitshow maxlength="20" />
         </ConfigProvider>
-        <h2>Textarea 英文</h2>
+        <h2>{translated.title2}</h2>
         <ConfigProvider locale={enUS}>
           <TextArea disabled limitshow maxlength="20" />
         </ConfigProvider>
-        <h2>默认主题</h2>
+        <h2>{translated.defaultTheme}</h2>
         <ConfigProvider>
           <CellGroup>
             <Cell>
@@ -37,12 +61,12 @@ const ConfigProviderDemo = () => {
             </Cell>
             <Cell>
               <Button type="primary" size="large">
-                提交
+                {translated.submit}
               </Button>
             </Cell>
           </CellGroup>
         </ConfigProvider>
-        <h2>定制主题</h2>
+        <h2>{translated.customTheme}</h2>
         <ConfigProvider theme={darkTheme}>
           <CellGroup>
             <Cell>
@@ -50,7 +74,7 @@ const ConfigProviderDemo = () => {
             </Cell>
             <Cell>
               <Button type="primary" size="large">
-                提交
+                {translated.submit}
               </Button>
             </Cell>
           </CellGroup>
