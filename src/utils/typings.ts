1896ea64955c1bf7cccf9bd73d4ccbe156a6a4fc
oasis-cloud <suanbanren@foxmail.com>
2023-03-14 18:56:03 +0800

    chore: 修改基础类型

--- src/utils/typings.ts
+++ src/utils/typings.ts
@@ -3,15 +3,7 @@ import type { CSSProperties, ReactNode } from 'react'
 export interface BasicComponent {
   className?: string
   style?: CSSProperties
-  iconFontClassName?: string
-  iconClassPrefix?: string
   children?: ReactNode
 }
 
-export const ComponentDefaults = {
-  // className: '',
-  // style: {},
-  // children: undefined,
-  iconClassPrefix: 'nut-icon',
-  iconFontClassName: 'nutui-iconfont',
-}
+export const ComponentDefaults = {}
