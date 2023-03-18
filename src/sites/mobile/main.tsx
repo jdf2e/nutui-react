232b463e66d5a29549ff9442014284d94b7d0220
imswk <syfzxx@vip.qq.com>
2023-03-18 18:41:14 +0800

    feat: 文档Demo示例touch兼容

--- src/sites/mobile/main.tsx
+++ src/sites/mobile/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
+import '@/utils/touchEmulator'
 import '@/sites/assets/styles/reset.scss'
 import '@/styles/font/iconfont.css'
 
