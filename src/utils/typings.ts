3e58bcb5797791c81882f0490048eae279b47c80
junjun666 <3476078473@qq.com>
2023-03-21 18:41:10 +0800

    chore: update pnpm

--- src/utils/typings.ts
+++ src/utils/typings.ts
@@ -3,7 +3,15 @@ import type { CSSProperties, ReactNode } from 'react'
 export interface BasicComponent {
   className?: string
   style?: CSSProperties
+  iconFontClassName?: string
+  iconClassPrefix?: string
   children?: ReactNode
 }
 
-export const ComponentDefaults = {}
+export const ComponentDefaults = {
+  // className: '',
+  // style: {},
+  // children: undefined,
+  iconClassPrefix: 'nut-icon',
+  iconFontClassName: 'nutui-iconfont',
+}
