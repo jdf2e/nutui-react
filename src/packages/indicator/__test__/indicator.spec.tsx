86206c8eaaa5c9732b08660c5e411634155ca918
hanyuxinting <hanyuxinting@sina.cn>
2023-04-17 13:46:03 +0800

    feat: 更新单元测试。

--- src/packages/indicator/__test__/indicator.spec.tsx
+++ src/packages/indicator/__test__/indicator.spec.tsx
@@ -3,10 +3,7 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import { Indicator } from '../indicator'
-import Button from '@/packages/button'
 import Cell from '@/packages/cell'
-import Row from '@/packages/row'
-import Col from '@/packages/col'
 
 test('should match snapshot', () => {
   const { asFragment } = render(<Indicator total={3} current={3} />)
@@ -14,26 +11,33 @@ test('should match snapshot', () => {
 })
 
 test('should be shown when passing size and current', () => {
-  const { container, getByTestId } = render(
+  const { container } = render(
     <Cell>
-      <Row>
-        <Col span="12">
-          <Button size="small" type="primary">
-            主要按钮
-          </Button>
-        </Col>
-        <Col span="12">
-          <Indicator
-            data-testid="indicator"
-            className="aa"
-            total={6}
-            current={5}
-          />
-        </Col>
-      </Row>
+      <Indicator total={3} current={3} />
     </Cell>
   )
-  expect(getByTestId('indicator')).toHaveClass('aa')
+  expect(container.querySelectorAll('.nut-indicator__dot')).toHaveLength(3)
+  expect(container.querySelectorAll('.nut-indicator__active')).toHaveLength(1)
+})
+
+test('should be shown when custom node', () => {
+  const { container } = render(
+    <Cell>
+      <Indicator total={6} current={5}>
+        <div className="number">{5}</div>
+      </Indicator>
+    </Cell>
+  )
   expect(container.querySelectorAll('.nut-indicator__dot')).toHaveLength(5)
-  expect(container.querySelectorAll('.nut-indicator__number')).toHaveLength(1)
+})
+
+test('should be shown when custom node', () => {
+  const { container } = render(
+    <Cell>
+      <Indicator total={6} current={5}>
+        <div className="number">{5}</div>
+      </Indicator>
+    </Cell>
+  )
+  expect(container.querySelectorAll('.nut-indicator__dot')).toHaveLength(5)
 })
