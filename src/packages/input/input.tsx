1a5c1b7e2dcc45b860c27433180072cf0c093c73
oasis-cloud <suanbanren@foxmail.com>
2023-04-25 18:21:46 +0800

    refactor: input h5

--- src/packages/input/input.tsx
+++ src/packages/input/input.tsx
@@ -1,10 +1,7 @@
 import React, {
   FunctionComponent,
-  useEffect,
-  useState,
   useCallback,
   useRef,
-  MouseEvent,
   HTMLInputTypeAttribute,
   forwardRef,
   useImperativeHandle,
@@ -15,8 +12,8 @@ import { useConfig } from '@/packages/configprovider'
 import { BasicComponent, ComponentDefaults } from '@/utils/typings'
 import { usePropsValue } from '@/utils/use-props-value'
 
-export type InputAlignType = 'left' | 'center' | 'right' // text-align
-export type InputFormatTrigger = 'onChange' | 'onBlur' // onChange: 在输入时执行格式化 ; onBlur: 在失焦时执行格式化
+export type InputAlignType = 'left' | 'center' | 'right'
+export type InputFormatTrigger = 'onChange' | 'onBlur'
 export type InputType = HTMLInputTypeAttribute
 
 export type InputRule = {
@@ -38,7 +35,6 @@ export interface InputProps extends BasicComponent {
   clearable: boolean
   clearIcon: React.ReactNode
   formatTrigger: InputFormatTrigger
-  rules: Array<InputRule>
   autofocus: boolean
   formatter?: (value: string) => void
   onChange?: (value: string) => void
@@ -61,7 +57,6 @@ const defaultProps = {
   clearable: false,
   clearIcon: null,
   formatTrigger: 'onChange',
-  rules: [] as InputRule[],
   autofocus: false,
 } as InputProps
 
@@ -74,10 +69,8 @@ export const Input: FunctionComponent<
 > = forwardRef((props, ref) => {
   const { locale } = useConfig()
   const {
-    children,
     type,
     name,
-    defaultValue,
     placeholder,
     align,
     disabled,
@@ -86,136 +79,85 @@ export const Input: FunctionComponent<
     clearable,
     clearIcon,
     formatTrigger,
-    rules,
     autofocus,
     style,
     className,
     onChange,
-    onBlur,
     onFocus,
     onClear,
     formatter,
     onClick,
-    ...rest
   } = {
     ...defaultProps,
     ...props,
   }
-
   const [value, setValue] = usePropsValue<string>({
     value: props.value,
     defaultValue: props.defaultValue,
     finalValue: '',
     onChange,
   })
-
-  const inputPlaceholder = placeholder || locale.placeholder
-
-  const [inputValue, SetInputValue] = useState('')
-  const [active, SetActive] = useState(false)
-  const [classes, setClasses] = useState('')
-  const getModelValue = () => String(inputValue ?? '')
-  const inputRef = useRef<any>(null)
-  const state = {
-    focused: false,
-    validateFailed: false, // 校验失败
-    validateMessage: '', // 校验信息
-  }
-
-  // useEffect(() => {
-  //   setClasses(inputClass)
-  //   SetInputValue(defaultValue)
-  // }, [defaultValue])
-
-  // useEffect(() => {
-  //   if (inputValue) {
-  //     updateValue(getModelValue())
-  //     resetValidation()
-  //   }
-  // }, [inputValue])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const composingRef = useRef(false)
 
   useImperativeHandle(ref, () => {
-    return inputRef.current
+    return {
+      clear: () => {
+        setValue('')
+      },
+      focus: () => {
+        inputRef.current?.focus()
+      },
+      blur: () => {
+        inputRef.current?.blur()
+      },
+      get nativeElement() {
+        return inputRef.current
+      },
+    }
   })
 
   const inputClass = useCallback(() => {
-    const prefixCls = 'nut-input'
-    return [prefixCls, `${disabled ? `${prefixCls}-disabled` : ''}`]
+    const classPrefix = 'nut-input'
+    return [classPrefix, `${disabled ? `${classPrefix}-disabled` : ''}`]
       .filter(Boolean)
       .join(' ')
   }, [disabled])
 
-  // 样式状态重置
-  useEffect(() => {
-    setClasses(inputClass)
-  }, [disabled, required, error, border, slotButton, rightIcon, center])
-
   const updateValue = (
     value: any,
-    trigger: InputFormatTrigger = 'onChange',
-    event?: any
+    trigger: InputFormatTrigger = 'onChange'
   ) => {
     let val = value
 
-    if (type === 'digit' || type === 'tel') {
+    if (type === 'digit') {
       val = formatNumber(val, false, false)
     }
     if (type === 'number') {
       val = formatNumber(val, true, true)
     }
-    if (type === 'tel' && !formatter) {
-      const regTel =
-        /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
-      const regNumber = /[^-0-9]/g
-      val =
-        !regTel.test(val) && val.length > 11
-          ? val.substring(0, 11)
-          : val.replace(regNumber, '')
-    }
-
     if (formatter && trigger === formatTrigger) {
       val = formatter(val)
     }
-
-    if (inputRef?.current?.value !== val) {
-      inputRef.current.value = val
+    setValue(val)
+    const eventHandler = props[trigger]
+    if (eventHandler && typeof eventHandler === 'function') {
+      eventHandler(val)
     }
-    SetInputValue(val)
   }
 
   const handleFocus = (event: Event) => {
     const val: any = (event.target as any).value
-    SetActive(true)
-    onFocus && onFocus(val, event)
+    onFocus && onFocus(val)
   }
 
-  const handleInput = (event: Event) => {
-    let val: any = (event.target as any).value
-
-    if (maxLength && val.length > Number(maxLength)) {
-      val = val.slice(0, Number(maxLength))
-    }
-    updateValue(val, 'onChange')
-    onChange && onChange(val, event)
+  const handleInput = (value: string) => {
+    updateValue(value, 'onChange')
   }
 
   const handleBlur = (event: Event) => {
-    setTimeout(() => {
-      SetActive(false)
-    }, 200)
-    let val: any = (event.target as any).value
-    if (maxLength && val.length > Number(maxLength)) {
-      val = val.slice(0, Number(maxLength))
-    }
+    const val: any = (event.target as any).value
     updateValue(val, 'onBlur')
-    onBlur && onBlur(val, event)
-  }
-
-  const resetValidation = () => {
-    if (state.validateFailed) {
-      state.validateFailed = false
-      state.validateMessage = ''
-    }
   }
 
   const inputType = (type: string) => {
@@ -228,31 +170,25 @@ export const Input: FunctionComponent<
     return type
   }
 
-  const handleClear = (event: Event) => {
-    updateValue('')
-    onClear && onClear('', event)
-  }
-
   return (
     <div
-      className={`${classes}  ${className || ''}`}
+      className={`${inputClass()}  ${className || ''}`}
       style={style}
       onClick={(e) => {
-        onClick && onClick(e)
+        onClick && onClick('a')
       }}
-      {...rest}
     >
       <input
         name={name}
-        className="input-text"
+        className="nut-input-native"
         ref={inputRef}
         style={{ textAlign: align }}
         type={inputType(type)}
         maxLength={maxLength}
-        placeholder={inputPlaceholder}
+        placeholder={placeholder || locale.placeholder}
         disabled={disabled}
         readOnly={readonly}
-        value={inputValue}
+        value={value}
         autoFocus={autofocus}
         onBlur={(e: any) => {
           handleBlur(e)
@@ -260,10 +196,30 @@ export const Input: FunctionComponent<
         onFocus={(e: any) => {
           handleFocus(e)
         }}
-        onInput={(e: any) => {
-          handleInput(e)
+        onChange={(e: any) => {
+          handleInput(e.currentTarget.value)
+        }}
+        onCompositionStart={(e) => {
+          composingRef.current = true
+          props.onCompositionStart?.(e)
+        }}
+        onCompositionEnd={(e) => {
+          composingRef.current = false
+          props.onCompositionEnd?.(e)
         }}
       />
+      {clearable && value.length > 0 ? (
+        <span
+          style={{ display: 'flex', alignItems: 'center' }}
+          onClick={() => {
+            setValue('')
+            inputRef.current?.focus()
+            onClear && onClear('')
+          }}
+        >
+          {clearIcon || <MaskClose className="nut-input-clear" />}
+        </span>
+      ) : null}
     </div>
   )
 })
