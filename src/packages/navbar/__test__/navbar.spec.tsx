3fb63ae6f8a563e1762f0bfd356b6f9eabbd0201
拧巴的猫 <353833373@qq.com>
2023-04-25 08:50:28 +0800

    refactor(navbar): 组件2.0 (#890)
    * feat(navbar): 组件2.0
    
    * refactor: navbar 2.0
    
    * refactor: navbar pr 修改
    
    * refactor: navbar pr 修改
    
    * refactor: navbar pr 修改

--- src/packages/navbar/__test__/navbar.spec.tsx
+++ src/packages/navbar/__test__/navbar.spec.tsx
@@ -2,114 +2,71 @@ import * as React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-import { Cart2 } from '@nutui/icons-react'
 import { NavBar } from '../navbar'
 
 test('should render left slot correctly', () => {
-  const { container } = render(
-    <NavBar title="订单详情" leftShow leftText="返回">
-      <span slot="left">left</span>
-    </NavBar>
-  )
+  const { container } = render(<NavBar left={<span>left</span>} />)
   expect(
     container.querySelectorAll('.nut-navbar__left')[0].innerHTML
-  ).toContain('<span slot="left">left</span>')
+  ).toContain('<span>left</span>')
 })
 
 test('should render right slot correctly', () => {
-  const { container } = render(
-    <NavBar title="订单详情" leftShow leftText="返回">
-      <span slot="right">right</span>
-    </NavBar>
-  )
+  const { container } = render(<NavBar right={<span>right</span>} />)
   expect(
     container.querySelectorAll('.nut-navbar__right')[0].innerHTML
-  ).toContain('<span slot="right">right</span>')
+  ).toContain('<span>right</span>')
 })
 
 test('should render title slot correctly', () => {
   const { container } = render(
-    <NavBar title="订单详情" leftShow leftText="返回">
-      <span slot="content">content</span>
+    <NavBar>
+      <span>content</span>
     </NavBar>
   )
   expect(
     container.querySelectorAll('.nut-navbar__title')[0].innerHTML
-  ).toContain('<span slot="content">content</span>')
+  ).toContain('<span>content</span>')
 })
 
 test('should left-text', () => {
-  const { container } = render(
-    <NavBar title="订单详情" leftShow leftText="back" />
+  const { container } = render(<NavBar left="back">订单详情</NavBar>)
+  expect(container.querySelectorAll('.nut-navbar__left')[0].innerHTML).toBe(
+    'back'
   )
-  expect(
-    container.querySelectorAll('.nut-navbar__left .nut-navbar__text')[0]
-      .innerHTML
-  ).toBe('back')
 })
 
 test('should description', () => {
-  const { container } = render(
-    <NavBar title="订单详情" description="description" />
+  const { container } = render(<NavBar right="description">订单详情</NavBar>)
+  expect(container.querySelectorAll('.nut-navbar__right')[0].innerHTML).toBe(
+    'description'
   )
-  expect(
-    container.querySelectorAll('.nut-navbar__right .nut-navbar__text')[0]
-      .innerHTML
-  ).toBe('description')
 })
 
 test('should render placeholder element when using placeholder prop', () => {
-  const { container } = render(<NavBar title="订单详情" fixed placeholder />)
+  const { container } = render(
+    <NavBar fixed placeholder>
+      订单详情
+    </NavBar>
+  )
   expect(
     container.querySelectorAll('.nut-navbar--placeholder')[0].innerHTML
   ).toMatchSnapshot()
 })
 
-test('should emit click-left event when clicking left text', () => {
+test('should emit click-back event when clicking back text', () => {
   const onClickBack = jest.fn()
   const { container } = render(
-    <NavBar title="订单详情" onClickBack={onClickBack} />
-  )
-
-  fireEvent.click(container.querySelectorAll('.nut-navbar__left')[0])
-  expect(onClickBack).toBeCalled()
-})
-
-test('should emit click-right event when clicking right text', () => {
-  const onClickRight = jest.fn()
-  const { container } = render(
-    <NavBar title="订单详情" onClickRight={onClickRight} />
-  )
-
-  fireEvent.click(container.querySelectorAll('.nut-navbar__right')[0])
-  expect(onClickRight).toBeCalled()
-})
-
-test('should emit click-title event when clicking title content', () => {
-  const onClickTitle = jest.fn()
-  const { container } = render(
-    <NavBar title="订单详情" onClickTitle={onClickTitle} />
-  )
-
-  fireEvent.click(container.querySelectorAll('.nut-navbar__title .title')[0])
-  expect(onClickTitle).toBeCalled()
-})
-
-test('should emit click-icon event when clicking title icon', () => {
-  const onClickIcon = jest.fn()
-  const { container } = render(
-    <NavBar title="订单详情" onClickIcon={onClickIcon}>
-      <i slot="titleIcon">
-        <Cart2 />
-      </i>
+    <NavBar back="返回" onClickBack={onClickBack}>
+      订单详情
     </NavBar>
   )
 
-  fireEvent.click(container.querySelectorAll('.nut-navbar__title div')[1])
-  expect(onClickIcon).toBeCalled()
+  fireEvent.click(container.querySelectorAll('.nut-navbar__left__back')[0])
+  expect(onClickBack).toBeCalled()
 })
 
 test('should change z-index when using z-index prop', () => {
-  const { container } = render(<NavBar title="订单详情" zIndex="100" />)
+  const { container } = render(<NavBar zIndex="100">订单详情</NavBar>)
   expect((container.firstChild as HTMLDivElement).style.zIndex).toBe('100')
 })
