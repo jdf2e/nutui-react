39eda21f9c5e22b096e23d96426d417fce2ec17a
oasis-cloud <suanbanren@foxmail.com>
2023-04-28 09:33:56 +0800

    refactor: input test case

--- src/packages/input/__test__/input.spec.tsx
+++ src/packages/input/__test__/input.spec.tsx
@@ -7,22 +7,28 @@ test('input props test', () => {
   const { container } = render(
     <Input name="text" placeholder="请输入文字" defaultValue="初始文本" />
   )
-  expect(container.querySelector('.input-text')).toHaveAttribute(
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
     'placeholder',
     '请输入文字'
   )
-  expect(container.querySelector('.input-text')).toHaveAttribute(
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
     'value',
     '初始文本'
   )
-  expect(container.querySelector('.input-text')).toHaveAttribute('name', 'text')
-  expect(container.querySelector('.input-text')).toHaveAttribute('type', 'text')
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+    'name',
+    'text'
+  )
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+    'type',
+    'text'
+  )
   expect(container).toMatchSnapshot()
 })
 
 test('password test', () => {
   const { container } = render(<Input placeholder="文本" type="password" />)
-  expect(container.querySelector('.input-text')).toHaveAttribute(
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
     'type',
     'password'
   )
@@ -30,23 +36,27 @@ test('password test', () => {
 
 test('readOnly test', () => {
   const { container } = render(<Input placeholder="文本" readOnly />)
-  expect(container.querySelector('.input-text')).toHaveAttribute('readOnly')
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+    'readOnly'
+  )
 })
 
 test('disabled test', () => {
   const { container } = render(<Input placeholder="文本" disabled />)
-  expect(container.querySelector('.input-text')).toHaveAttribute('disabled')
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+    'disabled'
+  )
 })
 
 test('textarea test', () => {
   const { container } = render(
-    <Input name="textarea" placeholder="留言" type="textarea" maxLength={50} />
+    <Input name="textarea" placeholder="留言" maxLength={50} />
   )
-  expect(container.querySelector('.input-text')).toHaveAttribute(
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
     'name',
     'textarea'
   )
-  expect(container.querySelector('.input-text')).toHaveAttribute(
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
     'maxlength',
     '50'
   )
@@ -57,26 +67,28 @@ test('clearable and clear event test', () => {
   const { container } = render(
     <Input defaultValue="清除文本" clearable onClear={handleClear} />
   )
-  const inputEl = container.querySelector('.nut-input-inner') as Element
-  expect(container.querySelector('.input-text')).toHaveAttribute(
-    'value',
-    '清除文本'
-  )
+  const inputEl = container.querySelector('.nut-input-native') as HTMLElement
+  expect(inputEl).toHaveAttribute('value', '清除文本')
   fireEvent.focus(inputEl)
   setTimeout(() => {
     expect(inputEl.querySelector('.nut-input-clear')).toHaveClass(
-      'nut-icon-mask-close'
+      'nut-icon-MaskClose'
     )
     const clearBtn = inputEl.querySelector('.nut-input-clear') as Element
     fireEvent.click(clearBtn)
     expect(handleClear).toBeCalled()
-    expect(container.querySelector('.input-text')).toHaveAttribute('value', '')
+    expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+      'value',
+      ''
+    )
   }, 300)
 })
 
 test('disabled test', () => {
   const { container } = render(<Input placeholder="文本" disabled />)
-  expect(container.querySelector('.input-text')).toHaveAttribute('disabled')
+  expect(container.querySelector('.nut-input-native')).toHaveAttribute(
+    'disabled'
+  )
 })
 
 test('clearable and clear event test', () => {
@@ -94,18 +106,15 @@ test('clearable and clear event test', () => {
       onClick={handleClick}
     />
   )
-  const inputEl = container.querySelector('.input-text') as Element
-  expect(container.querySelector('.input-text')).toHaveAttribute(
-    'value',
-    '文本'
-  )
+  const inputEl = container.querySelector('.nut-input-native') as Element
+  expect(inputEl).toHaveAttribute('value', '文本')
   fireEvent.click(inputEl)
   expect(handleClick).toBeCalled()
   fireEvent.change(inputEl, { target: { value: '文字改变' } })
   setTimeout(() => {
     expect(handleFocus).toBeCalled()
     expect(handleChange).toBeCalled()
-    expect(container.querySelector('.input-text')).toHaveAttribute(
+    expect(container.querySelector('.nut-input-native')).toHaveAttribute(
       'value',
       '文字改变'
     )
