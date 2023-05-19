e9ada7346501d895b2bca0524578e98133d7da35
hanyuxinting <hanyuxinting@sina.cn>
2023-05-19 17:31:38 +0800

    fix: test

--- src/packages/noticebar/__test__/noticebar.spec.tsx
+++ src/packages/noticebar/__test__/noticebar.spec.tsx
@@ -75,7 +75,7 @@ test('wrap test', () => {
   const text =
     'NutUI 是京东风格的移动端组件库，使用 Vue 语言来编写可以在 H5，小程序平台上的应用，帮助研发人员提升开发效率，改善开发体验。'
   const { container } = render(<NoticeBar content={text} wrap />)
-  expect(container.querySelector('.nut-noticebar-page')).toHaveClass('wrap')
+  expect(container.querySelector('.nut-noticebar-page')).toHaveClass('wrapable')
 })
 
 test('vertical test', () => {
