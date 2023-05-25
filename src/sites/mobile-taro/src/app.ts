8c37f6a875606f23e244a212ce3d6bbf0e30596a
oasis-cloud <suanbanren@foxmail.com>
2023-05-25 16:54:24 +0800

    docs: 站点中的 demo 增加 touch-emulator

--- src/sites/mobile-taro/src/app.ts
+++ src/sites/mobile-taro/src/app.ts
@@ -2,6 +2,9 @@ import { Component } from 'react'
 import '@/sites/assets/styles/reset.scss'
 import NutUI from '@/packages/nutui.react.taro.scss'
 import './app.scss'
+import Taro from '@tarojs/taro'
+
+Taro.getEnv() === 'WEB' ? import('@/sites/assets/util/touch-emulator.js') : null
 
 console.log(NutUI)
 class App extends Component {
