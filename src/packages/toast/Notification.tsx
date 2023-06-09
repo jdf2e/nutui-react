05deb61b29c40fefd0ab1986cf137651e2e78557
Eiinu <eiinu@qq.com>
2023-06-09 12:55:08 +0800

    fix(toast): 修复连续调用 show 方法时无法销毁的问题

--- src/packages/toast/Notification.tsx
+++ src/packages/toast/Notification.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import classNames from 'classnames'
+import { Check, Loading, Failure, Issue } from '@nutui/icons-react'
 import { render, unmount } from '@/utils/render'
 import Overlay from '@/packages/overlay/index'
-import { Check, Loading, Failure, Issue } from '@nutui/icons-react'
 import { BasicComponent } from '@/utils/typings'
 
 export interface NotificationProps extends BasicComponent {
@@ -18,10 +18,15 @@ export interface NotificationProps extends BasicComponent {
   contentStyle?: React.CSSProperties
   onClose: () => void
 }
-
+export interface NotificationState {
+  show: boolean
+}
 const classPrefix = 'nut-toast'
 
-export default class Notification extends React.PureComponent<NotificationProps> {
+export default class Notification extends React.PureComponent<
+  NotificationProps,
+  NotificationState
+> {
   static newInstance: (properties: NotificationProps, callback: any) => void
 
   private closeTimer: number | undefined
@@ -31,12 +36,21 @@ export default class Notification extends React.PureComponent<NotificationProps>
     this.close = this.close.bind(this)
     this.startCloseTimer = this.startCloseTimer.bind(this)
     this.clearCloseTimer = this.clearCloseTimer.bind(this)
-    this.close = this.close.bind(this)
     this.clickCover = this.clickCover.bind(this)
+    this.state = {
+      show: true,
+    }
   }
 
   close() {
+    this.setState({
+      show: false,
+    })
     this.clearCloseTimer()
+    if (this.props.id) {
+      const element = document.getElementById(this.props.id)
+      element && element.parentNode && element.parentNode.removeChild(element)
+    }
     this.props.onClose()
   }
 
@@ -109,6 +123,8 @@ export default class Notification extends React.PureComponent<NotificationProps>
       contentStyle,
     } = this.props
 
+    const { show } = this.state
+
     const classes = classNames({
       'nut-toast-has-icon': icon,
       [`nut-toast-${size}`]: true,
@@ -116,11 +132,10 @@ export default class Notification extends React.PureComponent<NotificationProps>
     return (
       <>
         <Overlay
-          visible={true}
+          visible={show}
           style={style}
           className={`${classPrefix}__overlay-default ${className}`}
           onClick={() => {
-            console.log('onclick')
             this.clickCover()
           }}
           closeOnOverlayClick={closeOnOverlayClick}
@@ -145,19 +160,17 @@ export default class Notification extends React.PureComponent<NotificationProps>
 
 Notification.newInstance = (properties, callback) => {
   const element = document.createElement('div')
-  if (properties.id) {
-    element.setAttribute('id', `${properties.id}`)
-  }
+  const id = properties.id ? properties.id : `${new Date().getTime()}`
+  element.id = id
+  properties.id = id
   document.body.appendChild(element)
 
   let called = false
-
   function ref(instance: any) {
     if (called) {
       return
     }
     called = true
-
     callback({
       component: instance,
       destroy() {
