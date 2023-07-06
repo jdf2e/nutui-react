a37c1b847988553da69cff9ae14595579d366e6c
junjun666 <35680677+junjun666@users.noreply.github.com>
2023-07-06 17:49:33 +0800

    fix: circleprogress color 属性的类型优化 (#1191)

--- src/packages/circleprogress/circleprogress.tsx
+++ src/packages/circleprogress/circleprogress.tsx
@@ -12,7 +12,7 @@ export interface CircleProgressProps extends BasicComponent {
   strokeWidth?: string | number
   radius?: number | string
   strokeLinecap?: 'butt' | 'round' | 'square' | 'inherit'
-  color?: any
+  color?: Record<string, string> | string
   background?: string
   clockwise?: boolean
 }
@@ -82,7 +82,7 @@ export const CircleProgress: FunctionComponent<
     const colorArr = Object.keys(color).sort(
       (a, b) => parseFloat(a) - parseFloat(b)
     )
-    const stopArr: any[] = []
+    const stopArr: Record<string, string>[] = []
     colorArr.forEach((item) => {
       const obj = {
         key: '',
@@ -100,7 +100,7 @@ export const CircleProgress: FunctionComponent<
       <svg viewBox="0 0 100 100">
         <defs>
           <linearGradient id={refRandomId} x1="100%" y1="0%" x2="0%" y2="0%">
-            {stop()?.map((item: any, index) => {
+            {stop()?.map((item: Record<string, string>, index) => {
               return (
                 <stop key={index} offset={item.key} stopColor={item.value} />
               )
