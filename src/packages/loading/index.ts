3008106f3f0569c3cbbac0945373b0f5c966be9d
mike8625 <mike8625@163.com>
2023-07-31 15:29:48 +0800

    fix: 修正一个导出类型的路径bug

--- src/packages/loading/index.ts
+++ src/packages/loading/index.ts
@@ -1,4 +1,4 @@
 import { Loading } from './loading'
 
-export type { LoadingType, DirectionType } from './loading.taro'
+export type { LoadingType, DirectionType } from './loading'
 export default Loading
