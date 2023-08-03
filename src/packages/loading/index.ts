1f421bbaaa1748f1070876e17d912fc183ff9695
oasis-cloud <suanbanren@foxmail.com>
2023-08-03 11:51:05 +0800

    fix: loading 组件导出 props 类型

--- src/packages/loading/index.ts
+++ src/packages/loading/index.ts
@@ -1,4 +1,4 @@
 import { Loading } from './loading'
 
-export type { LoadingType, DirectionType } from './loading'
+export type { LoadingProps, LoadingType, DirectionType } from './loading'
 export default Loading
