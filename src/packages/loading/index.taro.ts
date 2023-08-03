4ed35d8638cdca561bd50349dd3a9777897e7fbc
oasis-cloud <suanbanren@foxmail.com>
2023-08-03 15:17:23 +0800

    fix: loading 组件导出 props 类型 (#1278)

--- src/packages/loading/index.taro.ts
+++ src/packages/loading/index.taro.ts
@@ -1,4 +1,4 @@
 import { Loading } from './loading.taro'
 
-export type { LoadingType, DirectionType } from './loading.taro'
+export type { LoadingProps, LoadingType, DirectionType } from './loading.taro'
 export default Loading
