f2e7736b2b6c1ba9ce7c72a8cd1ef899bf17508d
oasis-cloud <12181600+oasis-cloud@users.noreply.github.com>
2023-08-30 13:58:03 +0800

    chore: 模板配置修改 (#1352)

--- packages/nutui-templates/vite-demo/vite.config.ts
+++ packages/nutui-templates/vite-demo/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
