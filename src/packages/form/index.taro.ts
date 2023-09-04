b8f7e82342650d5fc36f4d6730e2032ad2521919
oasis-cloud <suanbanren@foxmail.com>
2023-09-04 15:54:47 +0800

    fix: useFrom 在类组件下报错，可以采用 ref 的方式使用 FromInstance

--- src/packages/form/index.taro.ts
+++ src/packages/form/index.taro.ts
@@ -1,4 +1,7 @@
-import { Form } from './form.taro'
+import React from 'react'
+import { Form, FormProps } from './form.taro'
+import { FormItem } from '../formitem/formitem.taro'
+import { FormInstance } from './types'
 
 export type {
   FormItemRuleWithoutValidator,
@@ -7,4 +10,15 @@ export type {
   FormInstance,
   FieldEntity,
 } from './types'
-export default Form
+
+type CompoundedComponent = React.ForwardRefExoticComponent<
+  Partial<FormProps> &
+    Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> &
+    React.RefAttributes<FormInstance>
+> & {
+  Item: typeof FormItem
+}
+
+const InnerForm = Form as CompoundedComponent
+
+export default InnerForm
