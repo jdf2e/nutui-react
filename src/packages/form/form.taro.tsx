4b0b1deba650b0ee319051284e1eb8891d303643
oasis-cloud <12181600+oasis-cloud@users.noreply.github.com>
2023-09-06 15:12:37 +0800

    fix: useForm 在类组件下报错，可以采用 ref 的方式使用 FormInstance (#1383)
    * fix: useFrom 在类组件下报错，可以采用 ref 的方式使用 FromInstance
    
    * fix: useFrom 在类组件下报错，可以采用 ref 的方式使用 FromInstance
    
    * fix: export useForm
    
    * chore: prettier 2.3
    
    * chore: prettier 2.3

--- src/packages/form/form.taro.tsx
+++ src/packages/form/form.taro.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent, ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 import classNames from 'classnames'
 import { BasicComponent, ComponentDefaults } from '@/utils/typings'
 import Cell from '@/packages/cell/index.taro'
 import { Context } from './context'
 import { useForm } from './useform.taro'
-import { FormItem } from '../formitem/formitem.taro'
+import { FormInstance } from '@/packages/form/types'
 
 export interface FormProps extends BasicComponent {
   footer: ReactNode
@@ -33,76 +33,74 @@ const PositionInfo: any = {
   right: 'form-layout-right',
 }
 
-export const Form: FunctionComponent<
-  Partial<FormProps> & React.HTMLAttributes<HTMLFormElement>
-> & { Item: typeof FormItem } & { useForm: typeof useForm } = (props) => {
-  const classPrefix = 'nut-form'
-  const {
-    className,
-    style,
-    footer,
-    children,
-    initialValues,
-    divider,
-    onFinish,
-    onFinishFailed,
-    labelPosition,
-    starPosition,
-    form,
-  } = {
-    ...defaultProps,
-    ...props,
-  }
+export const Form = React.forwardRef<FormInstance, Partial<FormProps>>(
+  (props, ref) => {
+    const classPrefix = 'nut-form'
+    const {
+      className,
+      style,
+      footer,
+      children,
+      initialValues,
+      divider,
+      onFinish,
+      onFinishFailed,
+      labelPosition,
+      starPosition,
+      form,
+    } = {
+      ...defaultProps,
+      ...props,
+    }
 
-  let formInstance
-  if (form !== undefined) {
-    formInstance = form
-  } else {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    ;[formInstance] = useForm()
-  }
-  formInstance.starPosition = starPosition
-  const { setCallback, submit, resetFields, setInitialValues } = formInstance
-  // 设置校验后的回调，给组件的使用者暴露的接口
-  setCallback({
-    onFinish,
-    onFinishFailed,
-  })
-  // 初始化 initialValues 和 store
-  const mountRef = React.useRef<boolean>(false)
-  setInitialValues(initialValues, !mountRef.current)
-  if (!mountRef.current) {
-    mountRef.current = true
-  }
+    let formInstance
+    if (form !== undefined) {
+      formInstance = form
+    } else {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      ;[formInstance] = useForm()
+    }
+    formInstance.starPosition = starPosition
+    const { setCallback, submit, resetFields, setInitialValues } = formInstance
+    // 设置校验后的回调，给组件的使用者暴露的接口
+    setCallback({
+      onFinish,
+      onFinishFailed,
+    })
+    // 初始化 initialValues 和 store
+    const mountRef = React.useRef<boolean>(false)
+    setInitialValues(initialValues, !mountRef.current)
+    if (!mountRef.current) {
+      mountRef.current = true
+    }
 
-  return (
-    <form
-      className={classNames(
-        classPrefix,
-        className,
-        PositionInfo[labelPosition]
-      )}
-      style={style}
-      onSubmit={(e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        submit()
-      }}
-      onReset={(e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        resetFields()
-      }}
-    >
-      <Cell.Group divider={divider}>
-        <Context.Provider value={formInstance}>{children}</Context.Provider>
-        {footer ? <Cell>{footer}</Cell> : null}
-      </Cell.Group>
-    </form>
-  )
-}
+    return (
+      <form
+        className={classNames(
+          classPrefix,
+          className,
+          PositionInfo[labelPosition]
+        )}
+        style={style}
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          submit()
+        }}
+        onReset={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          resetFields()
+        }}
+      >
+        <Cell.Group divider={divider}>
+          <Context.Provider value={formInstance}>{children}</Context.Provider>
+          {footer ? <Cell>{footer}</Cell> : null}
+        </Cell.Group>
+      </form>
+    )
+  }
+)
 
 Form.defaultProps = defaultProps
 Form.displayName = 'NutForm'
-Form.Item = FormItem
-Form.useForm = useForm
--- src/packages/form/index.ts
+++ src/packages/form/index.ts
@@ -1,4 +1,8 @@
-import { Form } from './form'
+import React from 'react'
+import { Form, FormProps } from './form'
+import { FormItem } from '../formitem/formitem'
+import { FormInstance } from './types'
+import { useForm } from '@/packages/form/useform'
 
 export type {
   FormItemRuleWithoutValidator,
@@ -7,4 +11,19 @@ export type {
   FormInstance,
   FieldEntity,
 } from './types'
-export default Form
+
+type CompoundedComponent = React.ForwardRefExoticComponent<
+  Partial<FormProps> &
+    Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> &
+    React.RefAttributes<FormInstance>
+> & {
+  Item: typeof FormItem
+  useForm: typeof useForm
+}
+
+const InnerForm = Form as CompoundedComponent
+
+InnerForm.Item = FormItem
+InnerForm.useForm = useForm
+
+export default InnerForm
