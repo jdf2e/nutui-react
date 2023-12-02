ef52b78123743de6b39b601731eb5ee2b372f7e3
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2023-12-02 19:58:59 +0800

    style(tag): 适配v12 (#1733)

--- src/packages/tag/demo.taro.tsx
+++ src/packages/tag/demo.taro.tsx
@@ -86,7 +86,7 @@ const TagDemo = () => {
             extra={
               <Tag
                 closeable
-                closeIcon={<Failure width={12} height={12} />}
+                closeIcon={<Failure width={8} height={8} />}
                 onClose={() => Taro.showToast({ title: 'Tag closed' })}
                 type="primary"
               >
