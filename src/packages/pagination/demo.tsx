6b4b91bddc3ff0d110cb410763fb12f441bbd495
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2023-12-04 10:56:04 +0800

    feat(pagination): add lite mode and css variable (#1743)

--- src/packages/pagination/demo.tsx
+++ src/packages/pagination/demo.tsx
@@ -7,6 +7,7 @@ import { useTranslate } from '../../sites/assets/locale'
 interface T {
   basic: string
   simple: string
+  lite: string
   ellipse: string
   custom: string
   uncontrolled: string
@@ -17,6 +18,7 @@ const PaginationDemo = () => {
     'zh-CN': {
       basic: '基础用法',
       simple: '简单模式',
+      lite: '极简模式',
       ellipse: '显示省略号',
       custom: '自定义按钮',
       uncontrolled: '非受控方式',
@@ -24,6 +26,7 @@ const PaginationDemo = () => {
     'zh-TW': {
       basic: '基礎用法',
       simple: '簡單模式',
+      lite: '极简模式',
       ellipse: '顯示省略號',
       custom: '自定義按鈕',
       uncontrolled: '非受控方式',
@@ -31,6 +34,7 @@ const PaginationDemo = () => {
     'en-US': {
       basic: 'Basic usage',
       simple: 'Simple mode',
+      lite: 'lite Mode',
       ellipse: 'Show ellipsis',
       custom: 'Custom button',
       uncontrolled: 'Uncontrolled mode',
@@ -81,6 +85,16 @@ const PaginationDemo = () => {
           total={12}
           pageSize={1}
           mode="simple"
+          onChange={pageChange2}
+        />
+      </Cell>
+      <h2>{translated.lite}</h2>
+      <Cell>
+        <Pagination
+          value={currentPage2}
+          total={12}
+          pageSize={1}
+          mode="lite"
           onChange={pageChange2}
         />
       </Cell>
