de196d7d76ed26e24d615452919b8024f8697d5a
oasis-cloud <suanbanren@foxmail.com>
2023-12-08 11:29:05 +0800

    fix(popup): 在 taro 中只给overlay 增加 catchmove，popup 内容不应该增加 catchmove

--- src/packages/popup/popup.taro.tsx
+++ src/packages/popup/popup.taro.tsx
@@ -248,7 +248,6 @@ export const Popup: FunctionComponent<
           style={popStyles}
           className={popClassName}
           onClick={onHandleClick}
-          catchMove={lockScroll}
         >
           {renderTitle()}
           {showChildren ? children : ''}
