c31352f357ccb417d1b29e513b4e4badf6655fcc
oasis-cloud <suanbanren@foxmail.com>
2024-01-03 10:28:43 +0800

    feat(range): marks 类型优化

--- src/packages/range/index.taro.ts
+++ src/packages/range/index.taro.ts
@@ -1,4 +1,5 @@
 import { Range } from './range.taro'
 
-export type { RangeProps, RangeValue } from './range.taro'
+export type { RangeMark, RangeValue } from './types'
+export type { RangeProps } from './range.taro'
 export default Range
