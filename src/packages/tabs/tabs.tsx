27e0d7224648790cb6b3c2a09ea31cd73c159706
songchenglin3 <353833373@qq.com>
2024-01-17 13:56:40 +0800

    fix(tabs): 纵向图标未对齐问题

--- src/packages/tabs/tabs.tsx
+++ src/packages/tabs/tabs.tsx
@@ -207,7 +207,9 @@ export const Tabs: FunctionComponent<Partial<TabsProps>> & {
                 >
                   {activeType === 'line' && (
                     <div
-                      className={`${classPrefix}-titles-item-line`}
+                      className={classNames(`${classPrefix}-titles-item-line`, {
+                        [`${classPrefix}-titles-item-line-${direction}`]: true,
+                      })}
                       style={tabsActiveStyle}
                     />
                   )}
