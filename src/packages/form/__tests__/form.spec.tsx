db6f961253f913589ec503cb1f48e664f4ad9028
oasis-cloud <suanbanren@foxmail.com>
2024-02-28 19:58:59 +0800

    fix: test

--- src/packages/form/__tests__/form.spec.tsx
+++ src/packages/form/__tests__/form.spec.tsx
@@ -22,8 +22,8 @@ test('form set initialValues', () => {
   )
 })
 
-test('form validateTrigger', () => {
-  const { container } = render(
+test('form validateTrigger', async () => {
+  const { container, rerender } = render(
     <Form>
       <Form.Item
         label="字段A"
@@ -40,7 +40,28 @@ test('form validateTrigger', () => {
   if (input) {
     fireEvent.focus(input)
     fireEvent.blur(input)
-    waitFor(() => {
+    await waitFor(() => {
+      const errorMessage = container.querySelector('.nut-form-item-body-tips')
+      expect(errorMessage).toBeTruthy()
+    })
+  }
+  rerender(
+    <Form initialValues={{ username: 'NutUI-React' }}>
+      <Form.Item
+        label="字段A"
+        name="username"
+        required
+        validateTrigger={['onBlur']}
+        rules={[{ required: true, message: '请输入字段A' }]}
+      >
+        <Input placeholder="请输入字段A" type="text" />
+      </Form.Item>
+    </Form>
+  )
+  if (input) {
+    fireEvent.focus(input)
+    fireEvent.blur(input)
+    await waitFor(() => {
       const errorMessage = container.querySelector('.nut-form-item-body-tips')
       expect(errorMessage).toBeTruthy()
     })
