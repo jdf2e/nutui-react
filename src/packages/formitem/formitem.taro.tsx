cf1591198b854bdff646e8bcf58e149889dcaebd
oasis <12181600+oasis-cloud@users.noreply.github.com>
2024-03-01 16:02:18 +0800

    fix(form): 组件更新时，标签结构变化，会导致失去焦点 (#1973)

--- src/packages/formitem/formitem.taro.tsx
+++ src/packages/formitem/formitem.taro.tsx
@@ -216,14 +216,15 @@ export class FormItem extends React.Component<
         ) : null}
         <div className="nut-cell-value nut-form-item-body">
           <div className="nut-form-item-body-slots">{childNode}</div>
-          {item && item.length > 0 && (
-            <div
-              className="nut-form-item-body-tips"
-              style={{ textAlign: errorMessageAlign }}
-            >
-              {item[0].message}
-            </div>
-          )}
+          <div
+            className="nut-form-item-body-tips"
+            style={{
+              textAlign: errorMessageAlign,
+              display: item?.length ? 'initial' : 'none',
+            }}
+          >
+            {item?.[0]?.message}
+          </div>
         </div>
       </Cell>
     )
