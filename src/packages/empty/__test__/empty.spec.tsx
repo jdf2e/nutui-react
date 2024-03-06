b3a82ae5453ac10396c3eeb92b31742c76ac5adb
hanyuxinting <hanyuxinting@sina.cn>
2024-03-06 11:03:50 +0800

    test(Empty): 图片变更

--- src/packages/empty/__test__/empty.spec.tsx
+++ src/packages/empty/__test__/empty.spec.tsx
@@ -19,7 +19,7 @@ test('should render image props correctly', () => {
   const { container } = render(<Empty status="empty" />)
   expect(container.querySelector('.img')).toHaveAttribute(
     'src',
-    'https://storage.360buyimg.com/imgtools/44f3cc10c4-0cf9a7e0-c0ac-11ee-8375-193101bb1a46.png'
+    'https://static-ftcms.jd.com/p/files/61a9e3183985005b3958672b.png'
   )
 })
 test('should render actions correctly', () => {
@@ -42,7 +42,7 @@ test('should render custom image correctly', () => {
       description="店铺为空"
       image={
         <img
-          src="https://storage.360buyimg.com/imgtools/44f3cc10c4-0cf9a7e0-c0ac-11ee-8375-193101bb1a46.png"
+          src="https://static-ftcms.jd.com/p/files/61a9e3313985005b3958672e.png"
           alt=""
           data-testid="testImg"
         />
@@ -51,6 +51,6 @@ test('should render custom image correctly', () => {
   )
   expect(getByTestId('testImg')).toHaveAttribute(
     'src',
-    'https://storage.360buyimg.com/imgtools/44f3cc10c4-0cf9a7e0-c0ac-11ee-8375-193101bb1a46.png'
+    'https://static-ftcms.jd.com/p/files/61a9e3313985005b3958672e.png'
   )
 })
