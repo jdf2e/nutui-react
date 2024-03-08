917cc37ccfc4df88a5c53013b49535ca067067f9
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2024-03-08 09:52:27 +0800

    feat(swipe): 增加自定义高度的demo (#1996)
    * feat(Swipe): 增加自定义高度的demo
    
    * docs(Swipe): add docs

--- src/packages/swipe/demo.taro.tsx
+++ src/packages/swipe/demo.taro.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { Del } from '@nutui/icons-react-taro'
 import { useTranslate } from '@/sites/assets/locale/taro'
 import {
   Button,
@@ -22,9 +23,12 @@ type TSwipeDemo = {
   title7: string
   openOrClose: string
   title8: string
+  title9: string
   click: string
   sure: string
   del: string
+  alwaysbuy: string
+  liked: string
   choose: string
   event: string
   goods: string
@@ -52,13 +56,16 @@ const SwipeDemo = () => {
       openOrClose: '点击下方按钮打开或关闭',
       title7: '点击关闭',
       title8: '阻止父元素滚动',
+      title9: '卡片场景',
       click: '点击',
       sure: '确定',
       del: '删除',
+      alwaysbuy: '设置常购',
+      liked: '看相似',
       choose: '选择',
       event: '事件',
       goods: '商品',
-      collect: '收藏',
+      collect: '移入收藏',
       open: '打开',
       close: '关闭',
       closeLeft: '点击右侧按钮关闭',
@@ -79,9 +86,12 @@ const SwipeDemo = () => {
       openOrClose: '點擊下方按鈕打開或關閉',
       title7: '点击关闭',
       title8: '阻止父元素滾動',
+      title9: '卡片場景',
       click: '點擊',
       sure: '確定',
       del: '刪除',
+      alwaysbuy: '设置常购',
+      liked: '看相似',
       choose: '選擇',
       event: '事件',
       goods: '商品',
@@ -106,9 +116,12 @@ const SwipeDemo = () => {
       openOrClose: 'Click the button below',
       title7: 'Click to close',
       title8: 'Prevent parent scrolling',
+      title9: 'Card mode',
       click: 'click',
       sure: 'ok',
       del: 'delete',
+      alwaysbuy: 'always buy',
+      liked: 'liked',
       choose: 'select',
       event: 'event',
       goods: 'goods',
@@ -149,6 +162,24 @@ const SwipeDemo = () => {
   const closeRef = useRef(null)
   const openRef = useRef(null)
   const [shouldCatchMove, setShouldCatchMove] = useState(false)
+
+  const divNode = (text: string, style: any, hasIcon: boolean = true) => {
+    return (
+      <div
+        style={{
+          width: '60px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          ...style,
+        }}
+      >
+        {hasIcon ? <Del style={{ marginBottom: '8px' }} /> : null}
+        <>{text}</>
+      </div>
+    )
+  }
   return (
     <>
       <Header />
@@ -159,6 +190,95 @@ const SwipeDemo = () => {
             <Button type="primary" shape="square">
               {translated.del}
             </Button>
+          }
+        >
+          <Cell title={translated.leftDel} radius={0} />
+        </Swipe>
+
+        <h2>{translated.title9}</h2>
+        <Swipe
+          style={{ height: '104px' }}
+          rightAction={
+            <div
+              style={{
+                height: 'inherit',
+                width: '240px',
+                display: 'flex',
+                fontSize: '12px',
+              }}
+            >
+              <>
+                {divNode(translated.alwaysbuy, {
+                  background: '#F8F8F8',
+                  color: '#1A1A1A',
+                })}
+                {divNode(translated.collect, {
+                  background: '#ffcc00',
+                  color: '#FFF',
+                })}
+                {divNode(translated.liked, {
+                  background: '#FF860D',
+                  color: '#FFF',
+                })}
+                {divNode(translated.del, {
+                  background: '#FA2C19',
+                  color: '#FFF',
+                })}
+              </>
+            </div>
+          }
+        >
+          <Cell title={translated.leftDel} radius={0} />
+        </Swipe>
+
+        <br />
+
+        <Swipe
+          style={{ height: '104px' }}
+          rightAction={
+            <div
+              style={{
+                height: 'inherit',
+                width: '240px',
+                display: 'flex',
+                fontSize: '12px',
+              }}
+            >
+              <>
+                {divNode(
+                  translated.alwaysbuy,
+                  {
+                    background: '#F8F8F8',
+                    color: '#1A1A1A',
+                  },
+                  false
+                )}
+                {divNode(
+                  translated.collect,
+                  {
+                    background: '#ffcc00',
+                    color: '#FFF',
+                  },
+                  false
+                )}
+                {divNode(
+                  translated.liked,
+                  {
+                    background: '#FF860D',
+                    color: '#FFF',
+                  },
+                  false
+                )}
+                {divNode(
+                  translated.del,
+                  {
+                    background: '#FA2C19',
+                    color: '#FFF',
+                  },
+                  false
+                )}
+              </>
+            </div>
           }
         >
           <Cell title={translated.leftDel} radius={0} />
