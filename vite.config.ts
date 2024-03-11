63c510ae1e411486b0e4f98b650225e5dd4a9564
oasis <12181600+oasis-cloud@users.noreply.github.com>
2024-03-11 09:49:01 +0800

    fix: 构建站点主题变量未成功引入 (#2002)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react'
 import path from 'path'
 import atImport from 'postcss-import'
@@ -8,13 +8,13 @@ const projectID = process.env.VITE_APP_PROJECT_ID || ''
 
 const { resolve } = path
 
-let fileStr = `@import "@/styles/mixins/index.scss";@import "@/styles/variables.scss";@import "@/sites/assets/styles/variables.scss";\n`
+let fileStr = `@import "@/styles/variables.scss";@import "@/sites/assets/styles/variables.scss";@import '@/styles/theme-default.scss';\n`
 if (projectID) {
-  fileStr = `@import "@/styles/mixins/index.scss";@import '@/styles/variables-${projectID}.scss';\n@import "@/sites/assets/styles/variables.scss";\n`
+  fileStr = `@import '@/styles/variables-${projectID}.scss';\n@import "@/sites/assets/styles/variables.scss";\n@import '@/styles/font-${projectID}/iconfont.css';\n@import '@/styles/theme-${projectID}.scss';\n`
 }
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => {
+export default defineConfig(async (): Promise<UserConfig> => {
   const mdx = await import('@mdx-js/rollup')
   const remarkGfm = await import('remark-gfm')
   const remarkDirective = await import('remark-directive')
@@ -23,9 +23,6 @@ export default defineConfig(async () => {
       host: '0.0.0.0',
     },
     base: '/react/',
-    define: {
-      __PROJECTID__: JSON.stringify(`${projectID}` ? `-${projectID}` : ''),
-    },
     resolve: {
       alias: [
         { find: '@', replacement: resolve(__dirname, './src') },
@@ -64,13 +61,13 @@ export default defineConfig(async () => {
       {
         name: 'test',
         apply: 'serve',
-        async load(id) {
+        async load(id: string) {
           if (id.endsWith('.scss')) {
             // 移除 @import 语句
             const filePath = resolve(process.cwd(), id)
             const scssCode = await readFileSync(filePath, 'utf-8')
             const modifiedCode = scssCode.replace(
-              /@import\s+['"](\.{2}?\/)[^'"]+['"];/g,
+              /@import\s+['"](\.{2}?\/)[^'".]+(.s?css)['"];/g,
               ''
             )
             return modifiedCode
