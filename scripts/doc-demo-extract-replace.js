82536b3ff8a46a750718fc6963d94b8303edca3a
huxiyang3 <1872591453@qq.com>
2024-03-12 18:04:40 +0800

    fix: 完善script

--- scripts/doc-demo-extract-replace.js
+++ scripts/doc-demo-extract-replace.js
@@ -1,10 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-
-const extractH5Demos = () => {
-  const markdownFilePath = path.join(__dirname, 'doc.md')
+const args = process.argv.slice(2)
+console.log(args)
+const extractH5Demos = (i) => {
+  const markdownFilePath = path.join(
+    __dirname,
+    `../src/packages/${args[i]}/doc.md`
+  )
   let markdownContent = fs.readFileSync(markdownFilePath, 'utf-8')
-  const outputDirectory = path.join(__dirname, 'demos/h5')
+  const outputDirectory = path.join(
+    __dirname,
+    `../src/packages/${args[i]}/demos/h5`
+  )
   const tsxRegex = /:::demo\r\n\r\n```tsx\r\n([\s\S]*?)```\r\n\r\n:::/g
   let match
   if (!fs.existsSync(outputDirectory)) {
@@ -23,17 +30,25 @@ const extractH5Demos = () => {
       `export default Demo${counter}`
     )
     const fileName = `/demos/h5/demo${counter}.tsx`
-    fs.writeFileSync(path.join(__dirname, fileName), codeContent.trim())
-
+    fs.writeFileSync(
+      path.join(__dirname, `../src/packages/${args[i]}/${fileName}`),
+      codeContent.trim()
+    )
     counter++
   }
   fs.writeFileSync(markdownFilePath, markdownContent)
   console.log('======H5 demos have been extracted successfully======')
 }
-const extractTaroDemos = () => {
-  const markdownFilePath = path.join(__dirname, 'doc.taro.md')
+const extractTaroDemos = (i) => {
+  const markdownFilePath = path.join(
+    __dirname,
+    `../src/packages/${args[i]}/doc.md`
+  )
   let markdownContent = fs.readFileSync(markdownFilePath, 'utf-8')
-  const outputDirectory = path.join(__dirname, 'demos/taro')
+  const outputDirectory = path.join(
+    __dirname,
+    `../src/packages/${args[i]}/demos/taro`
+  )
   const tsxRegex = /:::demo\r\n\r\n```tsx\r\n([\s\S]*?)```\r\n\r\n:::/g
   let match
   if (!fs.existsSync(outputDirectory)) {
@@ -52,7 +67,10 @@ const extractTaroDemos = () => {
       `export default Demo${counter}`
     )
     const fileName = `/demos/taro/demo${counter}.tsx`
-    fs.writeFileSync(path.join(__dirname, fileName), codeContent.trim())
+    fs.writeFileSync(
+      path.join(__dirname, `../src/packages/${args[i]}/${fileName}`),
+      codeContent.trim()
+    )
 
     counter++
   }
@@ -60,12 +78,12 @@ const extractTaroDemos = () => {
   console.log('======Taro demos have been extracted successfully======')
 }
 
-const replaceAllDocs = () => {
+const replaceAllDocs = (i) => {
   const filePaths = [
-    path.join(__dirname, 'doc.md'),
-    path.join(__dirname, 'doc.en-US.md'),
-    path.join(__dirname, 'doc.taro.md'),
-    path.join(__dirname, 'doc.zh-TW.md'),
+    path.join(__dirname, `../src/packages/${args[i]}/doc.md`),
+    path.join(__dirname, `../src/packages/${args[i]}/doc.en-US.md`),
+    path.join(__dirname, `../src/packages/${args[i]}/doc.taro.md`),
+    path.join(__dirname, `../src/packages/${args[i]}/doc.zh-TW.md`),
   ]
   filePaths.forEach((path, index) => {
     let markdownContent = fs.readFileSync(path, 'utf-8')
@@ -88,6 +106,8 @@ const replaceAllDocs = () => {
   })
   console.log('======All docs have replaced successfully======')
 }
-extractH5Demos()
-extractTaroDemos()
-replaceAllDocs()
+for (let i = 0; i < args.length; i++) {
+  extractH5Demos(i)
+  extractTaroDemos(i)
+  replaceAllDocs(i)
+}
