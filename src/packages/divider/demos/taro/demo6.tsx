1c6f7a3ed908dea18081be3b4dcca684c760328d
huxiyang3 <1872591453@qq.com>
2024-03-13 15:16:32 +0800

    fix(divider): demo拆解与规范

---
+++ src/packages/divider/demos/taro/demo6.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Divider } from '@nutui/nutui-react-taro'
+
+const Demo6 = () => {
+  return (
+    <>
+      <div>
+        文本
+        <Divider direction="vertical" />
+        <a href="#" style={{ color: '#1989fa' }}>
+          链接
+        </a>
+        <Divider direction="vertical" />
+        <a href="#" style={{ color: '#1989fa' }}>
+          链接
+        </a>
+      </div>
+    </>
+  )
+}
+export default Demo6
