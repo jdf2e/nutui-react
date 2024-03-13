7e986f2c77af59eaf6b01f5faddec5c460ce1a77
oasis-cloud <suanbanren@foxmail.com>
2024-03-13 15:33:27 +0800

    chore: 增加注入 scss 的插件

--- packages/nutui-taro-demo/plugins/inject-scss.js
+++ packages/nutui-taro-demo/plugins/inject-scss.js
@@ -1,18 +1,29 @@
 const { readFileSync } = require('node:fs')
 const path = require('path')
+const configJson = require('../../../src/config.json')
+
 const logPrefix = 'inject-scss-for-harmony'
+const components = configJson.nav.map(nav => nav.packages.map(package => package.name.toLowerCase())).flat()
+
 function injectScss() {
   return {
     enforce: 'pre',
     name: logPrefix,
     async load(id) {
-      if (id.indexOf('button.taro') > -1) {
+      let find = -1
+      components.forEach((component, index) => {
+        if (id.indexOf('/' + component + '.taro') > -1 && component !== 'icon') {
+          find = index
+          return
+        }
+      })
+      if (find !== -1) {
         const filePath = path.resolve(process.cwd(), id)
         const code = await readFileSync(filePath, 'utf-8')
-        const modifiedCode = 'import "./button.scss"\n' + code
+        const modifiedCode = `import "./${components[find]}.scss"\n` + code
         return modifiedCode
       }
-    }
+    },
   }
 }
 
