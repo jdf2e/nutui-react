0054751fbd68a242367e7b9206680ceb6d62767c
huxiyang3 <1872591453@qq.com>
2024-03-14 16:05:49 +0800

    fix(space): demo拆解与规范

---
+++ src/packages/space/demos/taro/demo2.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Space, Button } from '@nutui/nutui-react-taro'
+
+const Demo2 = () => {
+  return (
+    <Space wrap>
+      <Button>按钮1</Button>
+      <Button>按钮2</Button>
+      <Button>按钮3</Button>
+      <Button>按钮4</Button>
+      <Button>按钮5</Button>
+      <Button>按钮6</Button>
+    </Space>
+  )
+}
+export default Demo2
