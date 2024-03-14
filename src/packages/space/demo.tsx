f848fcbf4fe5da7ea6957c8af512e5082c0c96d8
Alex.huxiyang <99181718+Alex-huxiyang@users.noreply.github.com>
2024-03-14 17:18:01 +0800

    fix(space): demo拆解与规范 (#2021)

--- src/packages/space/demo.tsx
+++ src/packages/space/demo.tsx
@@ -1,39 +1,20 @@
 import React from 'react'
-import { Space } from './space'
-import Button from '@/packages/button'
 import Cell from '@/packages/cell'
-import ConfigProvider from '@/packages/configprovider'
 import { useTranslate } from '@/sites/assets/locale'
+import Demo1 from './demos/h5/demo1'
+import Demo2 from './demos/h5/demo2'
+import Demo3 from './demos/h5/demo3'
+import Demo4 from './demos/h5/demo4'
+import Demo5 from './demos/h5/demo5'
+import Demo6 from './demos/h5/demo6'
 
-interface Button {
-  button1: string
-  button2: string
-  button3: string
-  button4: string
-  button5: string
-  button6: string
-}
-interface T extends Button {
-  basic: string
-  wrap: string
-  direction: string
-  spaceGap: string
-  mainAxisAlign: string
-  crossAxisAlign: string
-}
 const SpaceDemo = () => {
-  const [translated] = useTranslate<T>({
+  const [translated] = useTranslate({
     'zh-CN': {
       basic: '基本用法',
       wrap: '换行',
       direction: '垂直',
       spaceGap: '间距大小',
-      button1: '按钮1',
-      button2: '按钮2',
-      button3: '按钮3',
-      button4: '按钮4',
-      button5: '按钮5',
-      button6: '按钮6',
       mainAxisAlign: '主轴对齐方式',
       crossAxisAlign: '交叉轴对齐方式',
     },
@@ -42,12 +23,6 @@ const SpaceDemo = () => {
       wrap: '換行',
       direction: '垂直',
       spaceGap: '間距大小',
-      button1: '按钮1',
-      button2: '按钮2',
-      button3: '按钮3',
-      button4: '按钮4',
-      button5: '按钮5',
-      button6: '按钮6',
       mainAxisAlign: '主軸對齊方式',
       crossAxisAlign: '交叉軸對齊方式',
     },
@@ -56,12 +31,6 @@ const SpaceDemo = () => {
       wrap: 'wrap',
       direction: 'Direction',
       spaceGap: 'SpaceGap',
-      button1: 'button1',
-      button2: 'button2',
-      button3: 'button3',
-      button4: 'button4',
-      button5: 'button5',
-      button6: 'button6',
       mainAxisAlign: 'MainAxis Alignment',
       crossAxisAlign: 'CrossAxis Alignment',
     },
@@ -72,92 +41,27 @@ const SpaceDemo = () => {
       <div className="demo">
         <h2>{translated.basic}</h2>
         <Cell>
-          <Space>
-            <Button>{translated.button1}</Button>
-            <Button>{translated.button2}</Button>
-            <Button>{translated.button3}</Button>
-          </Space>
+          <Demo1 />
         </Cell>
-
         <h2>{translated.wrap}</h2>
         <Cell>
-          <Space wrap>
-            <Button>{translated.button1}</Button>
-            <Button>{translated.button2}</Button>
-            <Button>{translated.button3}</Button>
-            <Button>{translated.button4}</Button>
-            <Button>{translated.button5}</Button>
-            <Button>{translated.button6}</Button>
-          </Space>
+          <Demo2 />
         </Cell>
-
         <h2>{translated.direction}</h2>
         <Cell>
-          <Space direction="vertical">
-            <Button>{translated.button1}</Button>
-            <Button>{translated.button2}</Button>
-            <Button>{translated.button3}</Button>
-          </Space>
+          <Demo3 />
         </Cell>
-
         <h2>{translated.spaceGap}</h2>
         <Cell>
-          <ConfigProvider
-            theme={{
-              nutuiSpaceGap: '20px',
-            }}
-          >
-            <Space>
-              <Button>{translated.button1}</Button>
-              <Button>{translated.button2}</Button>
-              <Button>{translated.button3}</Button>
-            </Space>
-          </ConfigProvider>
+          <Demo4 />
         </Cell>
-
         <h2>{translated.mainAxisAlign}</h2>
-
         <Cell style={{ display: 'block' }}>
-          <Space justify="start" wrap>
-            <Button>{translated.button1}</Button>
-            <div>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button2}
-              </Button>
-              <Button block>{translated.button2}</Button>
-            </div>
-            <div>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button3}
-              </Button>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button3}
-              </Button>
-              <Button block>{translated.button3}</Button>
-            </div>
-          </Space>
+          <Demo5 />
         </Cell>
-
         <h2>{translated.crossAxisAlign}</h2>
         <Cell>
-          <Space align="end" wrap>
-            <Button>{translated.button1}</Button>
-            <div>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button2}
-              </Button>
-              <Button block>{translated.button2}</Button>
-            </div>
-            <div>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button3}
-              </Button>
-              <Button block style={{ marginBottom: 5 }}>
-                {translated.button3}
-              </Button>
-              <Button block>{translated.button3}</Button>
-            </div>
-          </Space>
+          <Demo6 />
         </Cell>
       </div>
     </>
