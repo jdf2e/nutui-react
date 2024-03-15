4c3e3e5bd8300373c81165cf3eb7150e735a1b95
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2024-03-15 20:32:28 +0800

    feat(pulltorefresh): 增加 type 属性，支持深色背景设置 (#2044)
    * feat(PullToRefresh): 增加反白模式
    
    * feat: 提取css变量
    
    * docs: 修改doc
    
    * fix: 导出typs
    
    * fix: 增加doc
    
    ---------
    
    Co-authored-by: xiaoyatong <hanyuxingint@sina.cn>

---
+++ src/packages/pulltorefresh/demos/taro/demo3.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { PullToRefresh, Toast } from '@nutui/nutui-react-taro'
+
+const Demo1 = () => {
+  const [list] = useState([1, 2, 3, 4, 5, 6, 7])
+  const [show, SetShow] = useState(false)
+  const [toastMsg, SetToastMsg] = useState('')
+  const toastShow = (msg: any) => {
+    SetToastMsg(msg)
+    SetShow(true)
+  }
+  return (
+    <>
+      <PullToRefresh
+        type="primary"
+        onRefresh={() =>
+          new Promise((resolve) => {
+            toastShow('😊')
+            resolve('done')
+          })
+        }
+      >
+        {list.map((item) => (
+          <div
+            style={{
+              textAlign: 'center',
+              height: '50px',
+              lineHeight: '50px',
+            }}
+            key={item}
+          >
+            {item}
+          </div>
+        ))}
+      </PullToRefresh>
+      <Toast
+        type="text"
+        visible={show}
+        content={toastMsg}
+        onClose={() => {
+          SetShow(false)
+        }}
+      />
+    </>
+  )
+}
+
+export default Demo1
