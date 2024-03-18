7d3ec45ef7b86f3080fdda1d0cc24d2e0a0a91c8
eiinu <eiinu@qq.com>
2024-03-18 18:44:09 +0800

    test(noticebar): update

--- src/packages/noticebar/__test__/noticebar.spec.tsx
+++ src/packages/noticebar/__test__/noticebar.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import { render, fireEvent, waitFor, act } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import NoticeBar from '@/packages/noticebar'
 
@@ -17,12 +17,12 @@ test('noticebar base test', () => {
   expect(container).toMatchSnapshot()
 })
 
-test('scrollable test', () => {
+test('scrollable test', async () => {
   const text =
     'NutUI 是京东风格的移动端组件库，使用 Vue 语言来编写可以在 H5，小程序平台上的应用，帮助研发人员提升开发效率，改善开发体验。'
 
   const { container } = render(<NoticeBar content={text} scrollable={false} />)
-  setTimeout(() => {
+  await waitFor(() => {
     expect(
       container.querySelector('.nut-noticebar-box-wrap-content')
     ).toHaveClass('nut-ellipsis')
@@ -32,12 +32,12 @@ test('scrollable test', () => {
       'style',
       'animation-delay: 1s; animation-duration: 0s; transform: translateX(0);'
     )
-  }, 300)
+  })
 
   expect(container).toMatchSnapshot()
 })
 
-test('closeable & rightIcon test', () => {
+test('closeable & rightIcon test', async () => {
   const text =
     'NutUI 是京东风格的移动端组件库，使用 Vue 语言来编写可以在 H5，小程序平台上的应用，帮助研发人员提升开发效率，改善开发体验。'
   const handleClick = vi.fn()
@@ -49,34 +49,27 @@ test('closeable & rightIcon test', () => {
       rightIcon="circle-close"
     />
   )
-  setTimeout(() => {
-    expect(container.querySelector('.nut-noticebar-box-right-icon')).toBeTruthy
-    expect(container.querySelector('.nutui-iconfont')).toHaveClass(
-      'nut-icon-circle-close'
-    )
-    fireEvent.click(container)
+  const icon = container.querySelector('.nut-noticebar-box-right-icon')
+  expect(icon).toBeTruthy
+  expect(icon?.innerHTML).toBe('circle-close')
+  await act(() => {
+    fireEvent.click(icon as Element)
     expect(handleClick).toBeCalled()
-  }, 300)
+  })
 })
 
-test('customer leftIcon test', () => {
+test('customer leftIcon test', async () => {
   const { container } = render(
-    <NoticeBar leftIcon="https://img13.360buyimg.com/imagetools/jfs/t1/72082/2/3006/1197/5d130c8dE1c71bcd6/e48a3b60804c9775.png">
+    <NoticeBar leftIcon="circle-close">
       <a href="https://www.jd.com">京东商城</a>
     </NoticeBar>
   )
-  setTimeout(() => {
-    expect(container.querySelector('.nut-noticebar-box-left-icon')).toBeTruthy
-    expect(
-      container.querySelector('.nut-noticebar-box-left-icon')
-    ).toHaveAttribute(
-      'style',
-      'background-image: url("https://img13.360buyimg.com/imagetools/jfs/t1/72082/2/3006/1197/5d130c8dE1c71bcd6/e48a3b60804c9775.png");'
-    )
-    expect(
-      container.querySelector('.nut-noticebar-box-wrap-content')?.innerHTML
-    ).toBe('<a href="https://www.jd.com">京东商城</a>')
-  }, 300)
+  const icon = container.querySelector('.nut-noticebar-box-left-icon')
+  expect(icon).toBeTruthy
+  expect(icon?.innerHTML).toBe('circle-close')
+  expect(
+    container.querySelector('.nut-noticebar-box-wrap-content')?.innerHTML
+  ).toBe('<a href="https://www.jd.com">京东商城</a>')
 })
 
 test('wrap test', () => {
@@ -155,6 +148,8 @@ test('event test', async () => {
   )
 
   const box = container.querySelectorAll('.nut-noticebar-box')[0]
-  fireEvent.click(box)
-  await waitFor(() => expect(handleClick).toBeCalled())
+  await act(() => {
+    fireEvent.click(box)
+    expect(handleClick).toBeCalled()
+  })
 })
