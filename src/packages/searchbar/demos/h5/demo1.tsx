19318974fa5908825991f36b465f77379b1a24f9
Joyfully <584974245@qq.com>
2024-03-19 09:43:06 +0800

    fix(SearchBar): demo格式化

--- src/packages/searchbar/demos/h5/demo1.tsx
+++ src/packages/searchbar/demos/h5/demo1.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react'
 import { SearchBar } from '../../searchbar'
 
 const Demo1 = () => {
-  return <>
-    <SearchBar backable placeholder="上京东，购好物" />
-  </>
+  return (
+    <>
+      <SearchBar backable placeholder="上京东，购好物" />
+    </>
+  )
 }
-export default Demo1;
+export default Demo1
--- src/packages/searchbar/demos/taro/demo5.tsx
+++ src/packages/searchbar/demos/taro/demo5.tsx
@@ -1,43 +1,44 @@
-import React from "react";
-import { SearchBar } from '@nutui/nutui-react-taro';
+import React from 'react'
+import { SearchBar } from '@nutui/nutui-react-taro'
 import {
   ArrowLeft,
   Photograph,
   More,
   Close,
-  Star
+  Star,
 } from '@nutui/icons-react-taro'
 
 const Demo5 = () => {
-  return <>
-    <SearchBar
-      left={
-        <>
-          <ArrowLeft size={20} />
-          <Close size={20} />
-        </>
-      }
-      right={
-        <>
-          <Star
-            size={20}
-            style={{
-              color: 'var(--nutui-color-primary)',
+  return (
+    <>
+      <SearchBar
+        left={
+          <>
+            <ArrowLeft size={20} />
+            <Close size={20} />
+          </>
+        }
+        right={
+          <>
+            <Star
+              size={20}
+              style={{
+                color: 'var(--nutui-color-primary)',
+              }}
+            />
+            <More size={20} />
+          </>
+        }
+        rightIn={
+          <Photograph
+            size={16}
+            onClick={() => {
+              console.log('Photograph right in')
             }}
           />
-          <More size={20} />
-        </>
-      }
-      rightIn={
-        <Photograph
-          size={16}
-          onClick={() => {
-            console.log('Photograph right in')
-          }}
-        />
-      }
-    />
-
-  </>
+        }
+      />
+    </>
+  )
 }
-export default Demo5;
+export default Demo5
