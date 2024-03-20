55fd3f3efbea8621c4d99ddc3c22178ed4e2fb19
Alex.huxiyang <1872591453@qq.com>
2024-03-20 18:25:50 +0800

    fix(circleprogress): demo拆解与规范

---
+++ src/packages/circleprogress/demos/h5/demo1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CircleProgress } from '@nutui/nutui-react'
+
+const Demo1 = () => {
+  return (
+    <>
+      <CircleProgress percent={20} />
+      <CircleProgress percent={60}>60%</CircleProgress>
+    </>
+  )
+}
+export default Demo1
---
+++ src/packages/circleprogress/demos/taro/demo2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CircleProgress } from '@nutui/nutui-react-taro'
+
+const Demo2 = () => {
+  return (
+    <>
+      <CircleProgress percent={50} strokeWidth={10} />
+    </>
+  )
+}
+export default Demo2
