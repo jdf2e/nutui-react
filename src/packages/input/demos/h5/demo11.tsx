410eb008a25c5847543506b403fd5eb59192ac40
yeyu1998 <53027240+yeyu98@users.noreply.github.com>
2024-03-20 19:02:15 +0800

    fix(input): demo拆解与规范 (#2035)
    * fix(input): 拆解demo文件&format
    
    * fix(input): input h5 demo拆解
    
    * fix(input): 移除注释
    
    * docs(input): 文档引入demo
    
    * fix(input): taro demo拆解&文档引入
    
    * fix(input): taro toast 传参问题
    
    * fix(input): 移除demo中未使用的多语言文案
    
    * fix: demos
    
    ---------
    
    Co-authored-by: yeyu98 <763626908@qq.com>
    Co-authored-by: xiaohu <zhouyi@yx.com>
    Co-authored-by: xiaoyatong <hanyuxingint@sina.cn>

---
+++ src/packages/input/demos/h5/demo11.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from '@nutui/nutui-react'
+
+const Demo11 = () => {
+  return (
+    <>
+      <Input align="left" placeholder="文本内容对齐" />
+      <Input align="right" placeholder="文本内容对齐" />
+    </>
+  )
+}
+export default Demo11
