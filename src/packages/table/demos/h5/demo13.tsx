d94385d469dbef87837b3e5d441b80f21143932c
Alex.huxiyang <99181718+Alex-huxiyang@users.noreply.github.com>
2024-03-21 14:14:26 +0800

    fix(table): demo拆解与规范 (#2074)
    * fix: save
    
    * fix(table): demo拆解与规范
    
    * fix: pr-fix
    
    * fix: pr-fix
    
    * fix: pr-fix
    
    * fix: pr-fix
    
    * fix: pr-fix
    
    * fix: test failed

---
+++ src/packages/table/demos/h5/demo13.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { Table } from '@nutui/nutui-react'
+
+interface TableColumnProps {
+  key: string
+  title?: string
+  align?: string
+  sorter?: ((a: any, b: any) => number) | boolean | string
+  render?: (rowData: any, rowIndex: number) => string | React.ReactNode
+  fixed?: 'left' | 'right'
+  width?: number
+}
+const Demo13 = () => {
+  const [data6, setData6] = useState([
+    {
+      name: 'Tom',
+      sex: '男',
+      record: '小学',
+      birthday: '2010-01-01',
+      age: 10,
+    },
+    {
+      name: 'Lucy',
+      sex: '女',
+      record: '本科',
+      birthday: '2000-01-01',
+      age: 30,
+    },
+    {
+      name: 'Jack',
+      sex: '男',
+      record: '高中',
+      birthday: '2020-01-01',
+      age: 4,
+    },
+    {
+      name: 'Sara',
+      sex: '女',
+      record: '高中',
+      birthday: '2020-01-01',
+      age: 6,
+    },
+    {
+      name: 'Frank',
+      sex: '男',
+      record: '幼儿园',
+      birthday: '2020-01-01',
+      age: 3,
+    },
+  ])
+
+  const [columnsStickRight, setColumnsStickRight] = useState<
+    Array<TableColumnProps>
+  >([
+    {
+      title: '姓名',
+      key: 'name',
+      align: 'center',
+      width: 100,
+    },
+    {
+      title: '性别',
+      key: 'sex',
+      width: 60,
+    },
+    {
+      title: '学历',
+      key: 'record',
+      width: 100,
+    },
+    {
+      title: '生日',
+      key: 'birthday',
+      width: 100,
+    },
+    {
+      title: '年龄',
+      key: 'age',
+      fixed: 'right',
+      width: 60,
+    },
+  ])
+
+  return <Table columns={columnsStickRight} data={data6} />
+}
+export default Demo13
---
+++ src/packages/table/demos/h5/demo7.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { Table, Button, Toast } from '@nutui/nutui-react'
+import { Star } from '@nutui/icons-react'
+
+const Demo7 = () => {
+  const [columns4, setColumns4] = useState([
+    {
+      title: '姓名',
+      key: 'name',
+      align: 'center',
+    },
+    {
+      title: '性别',
+      key: 'sex',
+    },
+    {
+      title: '学历',
+      key: 'record',
+    },
+    {
+      title: '操作',
+      key: 'render',
+    },
+  ])
+
+  const [data4, setData4] = useState([
+    {
+      name: 'Tom',
+      sex: '男',
+      record: '小学',
+      render: () => {
+        return (
+          <Button
+            onClick={() => Toast.show('hello')}
+            size="small"
+            type="primary"
+          >
+            <div>Hello</div>
+          </Button>
+        )
+      },
+    },
+    {
+      name: 'Lucy',
+      sex: '女',
+      record: '本科',
+      render: () => {
+        return <Star height="14px" width="14px" />
+      },
+    },
+    {
+      name: 'Jack',
+      sex: '男',
+      record: '高中',
+      render: () => {
+        return (
+          <Button
+            type="success"
+            size="small"
+            onClick={() => window.open('https://www.jd.com')}
+          >
+            <div>跳转到京东</div>
+          </Button>
+        )
+      },
+    },
+  ])
+
+  return <Table columns={columns4} data={data4} />
+}
+export default Demo7
