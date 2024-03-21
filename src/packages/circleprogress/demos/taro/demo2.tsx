3847b01b851b75b66e59d05acd71142527c8c41a
Alex.huxiyang <1872591453@qq.com>
2024-03-21 13:56:17 +0800

    fix: pr-fix

--- src/packages/circleprogress/demos/taro/demo2.tsx
+++ src/packages/circleprogress/demos/taro/demo2.tsx
@@ -4,7 +4,8 @@ import { CircleProgress } from '@nutui/nutui-react-taro'
 const Demo2 = () => {
   return (
     <>
-      <CircleProgress percent={50} strokeWidth={10} />
+      <CircleProgress percent={50} strokeWidth={2} />
+      <CircleProgress percent={60} strokeWidth={10} background="#e5e9f2" />
     </>
   )
 }
