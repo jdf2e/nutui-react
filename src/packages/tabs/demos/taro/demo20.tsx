abfd9028b6e5bfd22c30ccfdff1e0c84d1514d97
yeyu1998 <53027240+yeyu98@users.noreply.github.com>
2024-03-21 13:48:27 +0800

    fix(tabs): demo拆解与规范 (#2066)
    * docs(tabs): 文档内容调整
    
    * fix(tabs): 文档格式调整
    
    * fix(tabs): tabs demo拆解
    
    * fix(tabs): tabs h5 demo拆解
    
    * fix(tabs): tabs h5 demo多语言文案
    
    * fix(tabs): taro tabs demo文件拆解
    
    * fix(tabs): demo 变量类型补充
    
    * chore: update
    
    ---------
    
    Co-authored-by: oasis-cloud <suanbanren@foxmail.com>

---
+++ src/packages/tabs/demos/taro/demo20.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Tabs } from '@nutui/nutui-react-taro'
+
+const Demo20 = () => {
+  const [tab8value, setTab8value] = useState<string | number>('0')
+  const [tab9value, setTab9value] = useState<string | number>('0')
+  return (
+    <>
+      <Tabs
+        value={tab8value}
+        onChange={(value) => {
+          setTab8value(value)
+        }}
+        direction="vertical"
+      >
+        <Tabs.TabPane title="Tab 1">
+          <Tabs
+            value={tab9value}
+            onChange={(value) => {
+              setTab9value(value)
+            }}
+            direction="horizontal"
+          >
+            <Tabs.TabPane title="Tab 1"> Tab 1 </Tabs.TabPane>
+            <Tabs.TabPane title="Tab 2"> Tab 2 </Tabs.TabPane>
+            <Tabs.TabPane title="Tab 3"> Tab 3 </Tabs.TabPane>
+          </Tabs>
+        </Tabs.TabPane>
+        <Tabs.TabPane title="Tab 2"> Tab 2 </Tabs.TabPane>
+        <Tabs.TabPane title="Tab 3"> Tab 3 </Tabs.TabPane>
+      </Tabs>
+    </>
+  )
+}
+export default Demo20
