e4fab593b2dbcac5e10265792bfd92f5a72c935c
eiinu <eiinu@qq.com>
2024-03-21 17:37:21 +0800

    fix(video): demo拆解与规范

---
+++ src/packages/video/demos/h5/demo1.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { Cell, Video } from '@nutui/nutui-react'
+
+const Demo1 = () => {
+  const [source, setSource] = useState({
+    src: 'https://storage.360buyimg.com/nutui/video/video_NutUI.mp4',
+    type: 'video/mp4',
+  })
+  const options = {
+    controls: true,
+  }
+  const play = (elm: HTMLVideoElement) => console.log('play', elm)
+  const pause = (elm: HTMLVideoElement) => console.log('pause', elm)
+  const playend = (elm: HTMLVideoElement) => console.log('playend', elm)
+  return (
+    <>
+      <Cell style={{ padding: '0' }}>
+        <Video
+          source={source}
+          options={options}
+          onPlay={play}
+          onPause={pause}
+          onPlayEnd={playend}
+        />
+      </Cell>
+    </>
+  )
+}
+export default Demo1
---
+++ src/packages/video/demos/taro/demo6.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Cell, Video } from '@nutui/nutui-react-taro'
+
+const Demo6 = () => {
+  const [source, setSource] = useState({
+    src: 'https://storage.360buyimg.com/nutui/video/video_NutUI.mp4',
+    type: 'video/mp4',
+  })
+  const options = {
+    controls: false,
+    autoplay: true,
+    disabled: true,
+    muted: true,
+    playsinline: true,
+    loop: true,
+  }
+  const play = (elm: any) => console.log('play', elm)
+  const pause = (elm: any) => console.log('pause', elm)
+  const playend = (elm: any) => console.log('playend', elm)
+  return (
+    <>
+      <Cell style={{ padding: '0' }}>
+        <Video
+          source={source}
+          options={options}
+          onPlay={play}
+          onPause={pause}
+          onPlayEnd={playend}
+          style={{ height: '163px' }}
+        />
+      </Cell>
+    </>
+  )
+}
+export default Demo6
