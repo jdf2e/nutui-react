749016849898588e01a36d86e2807b84041e46b1
Alex.huxiyang <1872591453@qq.com>
2024-03-22 09:20:26 +0800

    feat: 兼容taro下的lazy属性

--- src/packages/progress/demos/taro/demo8.tsx
+++ src/packages/progress/demos/taro/demo8.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { Progress } from '@nutui/nutui-react-taro'
 
 const Demo8 = () => {
-  return <Progress percent={30} delay={500} lazy />
+  return <Progress percent={30} delay={5000} lazy />
 }
 export default Demo8
