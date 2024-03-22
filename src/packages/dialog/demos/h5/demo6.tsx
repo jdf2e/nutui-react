35aebd7cd2fde67b7ac99a8cca8140af31698a40
songsong <353833373@qq.com>
2024-03-22 19:25:34 +0800

    fix(dialog): 关闭按钮默认在底部，24px白色图标 (#2118)
    * fix(dialog): 关闭按钮默认在底部，24px白色图标
    
    * fix: 修改关闭按钮默认尺寸是24

--- src/packages/dialog/demos/h5/demo6.tsx
+++ src/packages/dialog/demos/h5/demo6.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Cell, Dialog } from '@nutui/nutui-react'
-import { Close } from '@nutui/icons-react'
 
 const Demo6 = () => {
   const [visible1, setVisible1] = useState(false)
@@ -18,6 +17,10 @@ const Demo6 = () => {
         title="顶部带关闭按钮"
         visible={visible1}
         closeIcon
+        closeIconPosition="top-right"
+        style={{
+          '--nutui-dialog-close-color': '#8c8c8c',
+        }}
         onConfirm={() => setVisible1(false)}
         onCancel={() => setVisible1(false)}
       >
@@ -33,13 +36,9 @@ const Demo6 = () => {
         className="test-dialog"
         title="底部带关闭按钮"
         visible={visible2}
-        closeIcon={<Close width="24px" height="24px" />}
-        closeIconPosition="bottom"
         onConfirm={() => setVisible2(false)}
         onCancel={() => setVisible2(false)}
-        style={{
-          '--nutui-dialog-close-color': '#FFFFFF',
-        }}
+        closeIcon
       >
         支持函数调用和组件调用两种方式。
       </Dialog>
