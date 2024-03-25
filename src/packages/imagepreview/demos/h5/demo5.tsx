d97cc3f5a1125d66fa7fdb7362470b0736c3f8af
Alex.huxiyang <1872591453@qq.com>
2024-03-25 18:53:01 +0800

    fix(imageprevire): demo拆解与规范

---
+++ src/packages/imagepreview/demos/h5/demo5.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { ImagePreview, Cell } from '@nutui/nutui-react'
+
+const Demo5 = () => {
+  const images = [
+    {
+      src: '//fastly.jsdelivr.net/npm/@vant/assets/apple-4.jpeg',
+    },
+    {
+      src: '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png',
+    },
+    {
+      src: '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg',
+    },
+    {
+      src: '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg',
+    },
+  ]
+  const [showPreview, setShowPreview] = useState(false)
+  return (
+    <>
+      <ImagePreview
+        images={images}
+        visible={showPreview}
+        indicator
+        indicatorColor="red"
+        onClose={() => setShowPreview(false)}
+      />
+      <Cell title="设置轮播指示器及颜色" onClick={() => setShowPreview(true)} />
+    </>
+  )
+}
+export default Demo5
