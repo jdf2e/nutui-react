b7be8ca5d8929b0dff47536167aaa87fd4992b1f
Amylee9712 <80453866+Amylee9712@users.noreply.github.com>
2024-03-25 16:30:08 +0800

    fix(textarea): demo拆解与规范 (#2132)

---
+++ src/packages/textarea/demos/taro/demo1.tsx
@@ -0,0 +1,16 @@
+import { TextArea } from '@nutui/nutui-react-taro'
+import React from 'react'
+
+const Demo1 = () => {
+  return (
+    <TextArea
+      defaultValue="基础用法"
+      className="text-1"
+      style={{ fontSize: '12px' }}
+      onChange={(value) => console.log('change', value)}
+      onBlur={() => console.log('blur')}
+      onFocus={() => console.log('focus')}
+    />
+  )
+}
+export default Demo1
