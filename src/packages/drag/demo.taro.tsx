bffe0aaf79aa388985aefc3bd16d5f56173186ec
eiinu <eiinu@qq.com>
2024-03-25 14:38:26 +0800

    chore(tag): demo

--- src/packages/drag/demo.taro.tsx
+++ src/packages/drag/demo.taro.tsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import Taro from '@tarojs/taro'
-import { Drag, Button } from '@/packages/nutui.react.taro'
 import { useTranslate } from '@/sites/assets/locale/taro'
 import Header from '@/sites/components/header'
 
-interface T {
-  basic: string
-  dragBasic: string
-  direction: string
-  directionX: string
-  directionY: string
-  attract: string
-  attractText: string
-  limitBoundaries: string
-}
+import Demo1 from './demos/taro/demo1'
+import Demo2 from './demos/taro/demo2'
+import Demo3 from './demos/taro/demo3'
+import Demo4 from './demos/taro/demo4'
+
 const DragDemo = () => {
-  const [translated] = useTranslate<T>({
+  const [translated] = useTranslate({
     'zh-CN': {
       basic: '基础用法',
       dragBasic: '触摸移动',
@@ -51,82 +45,21 @@ const DragDemo = () => {
 
   const isTaroWeb = Taro.getEnv() === 'WEB'
 
-  const right = () => {
-    return screenWidth - 300 - 9
-  }
-  const bottom = () => {
-    return windowHeight - 501 - (isTaroWeb ? 57 : 0)
-  }
   return (
     <>
       <Header />
-      <div className={`demo ${isTaroWeb ? 'web' : ''}`}>
+      <div className={`demo full ${isTaroWeb ? 'web' : ''}`}>
         <h2>{translated.basic}</h2>
-        <Drag style={{ left: '8px' }}>
-          <Button type="primary">{translated.dragBasic}</Button>
-        </Drag>
-        <h2 style={{ top: '30px', position: 'relative' }}>
-          {translated.direction}
-        </h2>
-        <Drag
-          direction="x"
-          style={{
-            top: isTaroWeb ? '197px' : '140px',
-            left: '8px',
-          }}
-        >
-          <Button type="primary"> {translated.directionX}</Button>
-        </Drag>
-        <Drag
-          direction="y"
-          style={{
-            top: isTaroWeb ? '197px' : '140px',
-            right: '50px',
-          }}
-        >
-          <Button type="primary"> {translated.directionY}</Button>
-        </Drag>
-        <h2 style={{ top: '60px', position: 'relative' }}>
-          {translated.attract}
-        </h2>
-        <Drag
-          direction="x"
-          attract
-          style={{
-            top: isTaroWeb ? '277px' : '220px',
-            left: '8px',
-          }}
-        >
-          <Button type="primary">{translated.attractText}</Button>
-        </Drag>
-        <h2 style={{ top: '90px', position: 'relative' }}>
-          {translated.limitBoundaries}
-        </h2>
-        <div
-          className="drag-boundary"
-          style={{
-            position: 'absolute',
-            top: isTaroWeb ? '357px' : '300px',
-            left: '8px',
-            width: '300px',
-            height: '200px',
-            border: '1px solid var(--nutui-color-primary)',
-          }}
-        />
-        <Drag
-          boundary={{
-            top: isTaroWeb ? 358 : 301,
-            left: 9,
-            bottom: bottom(),
-            right: right(),
-          }}
-          style={{
-            top: isTaroWeb ? '417px' : '360px',
-            left: '50px',
-          }}
-        >
-          <Button type="primary">{translated.limitBoundaries}</Button>
-        </Drag>
+        <Demo1 />
+
+        <h2 style={{ paddingTop: '30px' }}>{translated.direction}</h2>
+        <Demo2 />
+
+        <h2 style={{ paddingTop: '30px' }}>{translated.attract}</h2>
+        <Demo3 />
+
+        <h2 style={{ paddingTop: '30px' }}>{translated.limitBoundaries}</h2>
+        <Demo4 />
       </div>
     </>
   )
