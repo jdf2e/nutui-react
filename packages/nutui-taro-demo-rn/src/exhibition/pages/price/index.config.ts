f806428ceca8395f26f741eb676b351f87bf6c25
songchenglin3 <353833373@qq.com>
2024-05-06 10:26:50 +0800

    fix: 修改demo入口文件格式

--- packages/nutui-taro-demo-rn/src/exhibition/pages/price/index.config.ts
+++ packages/nutui-taro-demo-rn/src/exhibition/pages/price/index.config.ts
@@ -1,3 +1,3 @@
 export default {
-        navigationBarTitleText: 'Price',
-     }
+  navigationBarTitleText: 'Price',
+}
--- packages/nutui-taro-demo-rn/src/exhibition/pages/price/index.tsx
+++ packages/nutui-taro-demo-rn/src/exhibition/pages/price/index.tsx
@@ -1,2 +1,3 @@
 import Demo from '@/packages/price/demo.taro';
-     export default Demo;
+
+export default Demo;
