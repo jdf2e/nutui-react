99a7310a8e741017cefbfed3de7a0f7a4fd02517
Alex.huxiyang <1872591453@qq.com>
2024-05-09 16:29:53 +0800

    chore(image): demo代码风格优化 (#2220)

--- src/packages/image/demos/taro/demo7.tsx
+++ src/packages/image/demos/taro/demo7.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import { Row, Col, Image } from '@nutui/nutui-react-taro'
+import { Image } from '@nutui/nutui-react-taro'
 
 const Demo7 = () => {
   const src =
     'https://storage.360buyimg.com/imgtools/e067cd5b69-07c864c0-dd02-11ed-8b2c-d7f58b17086a.png'
+  const modes = [
+    'top',
+    'bottom',
+    'center',
+    'left',
+    'right',
+    'top left',
+    'top right',
+    'bottom left',
+    'bottom right',
+  ]
   return (
     <>
-      <Row gutter={10}>
-        {[
-          'top',
-          'bottom',
-          'center',
-          'left',
-          'right',
-          'top left',
-          'top right',
-          'bottom left',
-          'bottom right',
-        ].map((mode) => {
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {modes.map((mode) => {
           return (
-            <Col span="8" key={mode}>
+            <div style={{ width: 100, height: 100 }} key={mode}>
               <Image src={src} mode={mode as any} width="80" height="80" />
-            </Col>
+            </div>
           )
         })}
-      </Row>
+      </div>
     </>
   )
 }
