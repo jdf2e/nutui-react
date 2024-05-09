635db384edd18f7eefccd9033c89ecee85ab6e79
songchenglin3 <353833373@qq.com>
2024-05-09 10:25:15 +0800

    fix: 鸿蒙行内样式适配

--- packages/nutui-taro-demo-rn/nutui-react/packages/countdown/demos/taro/demo8.tsx
+++ packages/nutui-taro-demo-rn/nutui-react/packages/countdown/demos/taro/demo8.tsx
@@ -1,26 +1,42 @@
 import React, { useRef, useState } from 'react'
 import { Cell, CountDown } from '@nutui/nutui-react-taro'
 import { View, Text } from '@tarojs/components'
+import Taro, { pxTransform } from '@tarojs/taro'
 import '../../demo.scss'
 
+const HARMONY =
+  Taro.getEnv() === Taro.ENV_TYPE.HARMONYHYBRID ||
+  Taro.getEnv() === Taro.ENV_TYPE.HARMONY
+
 const partItemStyle = {
-  flexShrink: 0,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: 20,
-  height: 25,
   backgroundColor: '#e8220e',
   color: '#fff',
-  fontSize: '14px',
-  borderRadius: 6,
-  overflow: 'hidden',
+  ...(HARMONY
+    ? {
+        width: pxTransform(20),
+        height: pxTransform(25),
+        fontSize: pxTransform(14),
+        borderRadius: pxTransform(6),
+      }
+    : {
+        width: 20,
+        height: 25,
+        fontSize: 14,
+        borderRadius: 6,
+      }),
 }
-const partItemSymbolStyle = {
-  marginLeft: 5,
-  marginRight: 5,
-  whiteSpace: 'nowrap',
-}
+const partItemSymbolStyle = HARMONY
+  ? {
+      marginLeft: pxTransform(5),
+      marginRight: pxTransform(5),
+    }
+  : {
+      marginLeft: 5,
+      marginRight: 5,
+    }
 const Demo8 = () => {
   const onUpdate = (v: any) => {
     setResetTime(v)
