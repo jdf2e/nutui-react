17162ed4ed37b36b91668b9a1f8925dfc9ec2e39
songchenglin3 <353833373@qq.com>
2024-05-16 09:30:56 +0800

    fix: 修改demo

--- src/packages/price/demos/h5/demo8.tsx
+++ src/packages/price/demos/h5/demo8.tsx
@@ -3,16 +3,11 @@ import { Price } from '@nutui/nutui-react'
 
 const Demo8 = () => {
   return (
-    <div
-      style={{
-        display: 'inline-flex',
-        alignItems: 'baseline',
-      }}
-    >
+    <>
       <Price price={1513.12} size="normal" thousands />
       <span>&nbsp;</span>
       <Price price={1513.88} thousands line />
-    </div>
+    </>
   )
 }
 export default Demo8
--- src/packages/price/demos/taro/demo8.tsx
+++ src/packages/price/demos/taro/demo8.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Price } from '@nutui/nutui-react-taro'
 import { Text, View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 
 const Demo8 = () => {
+  const harmonyAndRn = [
+    Taro.ENV_TYPE.RN,
+    Taro.ENV_TYPE.HARMONYHYBRID,
+    Taro.ENV_TYPE.HARMONY,
+  ].includes(Taro.getEnv())
   return (
     <View
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'baseline',
-      }}
+      style={
+        harmonyAndRn
+          ? {
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }
+          : {}
+      }
     >
       <Price price={1513.12} size="normal" thousands />
       <Text>&nbsp;</Text>
