9d59bf410fffc08cfe5a10cf904e7aec86891094
songchenglin3 <353833373@qq.com>
2024-05-22 14:26:45 +0800

    fix: 修改平台判断函数

--- src/utils/platform-taro.ts
+++ src/utils/platform-taro.ts
@@ -1,9 +1,5 @@
 import Taro from '@tarojs/taro'
 
 export const hommaryAndRn = () => {
-  return [
-    Taro.ENV_TYPE.RN,
-    Taro.ENV_TYPE.HARMONYHYBRID,
-    Taro.ENV_TYPE.HARMONY,
-  ].includes(Taro.getEnv())
+  return ['RN', 'HARMONY', 'HARMONYHYBRID'].includes(Taro.getEnv())
 }
