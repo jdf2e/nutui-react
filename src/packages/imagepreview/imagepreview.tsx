c89e2220e59d0faccae2111311805a21cd569ab5
Alex.huxiyang <1872591453@qq.com>
2024-05-24 14:59:56 +0800

    fix(imagepreview): 阻止冒泡，防止点击图片关闭 (#2281)
    * fix(imagePreview): 无法换页无法控制
    
    * fix(imagePreview): 无法换页无法控制

--- src/packages/imagepreview/imagepreview.tsx
+++ src/packages/imagepreview/imagepreview.tsx
@@ -254,6 +254,7 @@ export const ImagePreview: FunctionComponent<Partial<ImagePreviewProps>> = (
   }
 
   const closeOnImg = (e: React.MouseEvent<Element, MouseEvent>) => {
+    e.stopPropagation()
     // 点击内容区域的图片是否可以关闭弹层（视频区域由于nut-video做了限制，无法关闭弹层）
     if (closeOnContentClick) {
       onCloseInner(e)
