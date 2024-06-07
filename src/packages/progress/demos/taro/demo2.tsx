eec8bd92df8df3b8da66d5c5fb5d5f77f8764c4d
huxiyang <1872591453@qq.com>
2024-06-07 13:43:45 +0800

    chore: css端侧区分

--- src/packages/progress/demos/taro/demo2.tsx
+++ src/packages/progress/demos/taro/demo2.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Cell, Progress } from '@nutui/nutui-react-taro'
+import Taro from '@tarojs/taro'
 
 const Demo2 = () => {
   return (
     <Cell>
       <Progress
         percent={30}
-        color="#FF0F23"
-        background="#FFEAE8"
+        color={
+          ['HARMONY', 'RN'].includes(Taro.getEnv())
+            ? '#FF0F23'
+            : 'var(--nutui-color-primary)'
+        }
+        background={
+          ['HARMONY', 'RN'].includes(Taro.getEnv())
+            ? '#FFEAE8'
+            : 'var(--nutui-brand-1)'
+        }
         strokeWidth="15"
       />
     </Cell>
