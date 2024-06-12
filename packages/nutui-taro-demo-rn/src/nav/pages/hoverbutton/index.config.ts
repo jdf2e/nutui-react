30ebce076051e57989c3bd58930317ed67fd1ddc
liyixin5 <lyxuncle@gmail.com>
2024-06-12 10:43:30 +0800

    feat(hoverbutton): 补充 hoverbuttonitem rn 端的拷贝

--- packages/nutui-taro-demo-rn/src/nav/pages/hoverbutton/index.config.ts
+++ packages/nutui-taro-demo-rn/src/nav/pages/hoverbutton/index.config.ts
@@ -1,3 +1,3 @@
 export default {
-  navigationBarTitleText: 'HoverButton'
+  navigationBarTitleText: 'Hoverbutton'
 }
--- packages/nutui-taro-demo-rn/src/nav/pages/hoverbutton/index.tsx
+++ packages/nutui-taro-demo-rn/src/nav/pages/hoverbutton/index.tsx
@@ -1,3 +1,2 @@
 import Demo from '@/packages/hoverbutton/demo.taro';
-console.log(Demo)
 export default Demo;
