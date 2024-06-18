c49d6ca8eebce0e473559bd933d5657c871765ad
oasis <12181600+oasis-cloud@users.noreply.github.com>
2024-06-18 15:41:31 +0800

    Update px-transform.ts

--- src/utils/px-transform.ts
+++ src/utils/px-transform.ts
@@ -2,6 +2,6 @@ import { pxTransform as transform } from '@tarojs/taro'
 import { harmony, rn } from './platform-taro'
 
 export default function pxTransform(value: number) {
-  if (harmony() && rn()) return transform(value)
+  if (harmony() || rn()) return transform(value)
   return `${value}px`
 }
