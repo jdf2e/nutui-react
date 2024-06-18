d390973f0b37f564caf8c3f5b5b34c58abfc89fb
oasis <12181600+oasis-cloud@users.noreply.github.com>
2024-06-18 13:40:36 +0800

    feat(input): 适配鸿蒙、小程序、H5、RN; 鸿蒙下无法隐藏密码Icon (#2338)

--- packages/nutui-taro-demo-rn/scripts/taro/adapted.js
+++ packages/nutui-taro-demo-rn/scripts/taro/adapted.js
@@ -27,4 +27,5 @@ exports = module.exports = [
   'safearea',
   'hoverbuttonitem',
   'configprovider',
+  'input',
 ]
--- src/packages/input/demos/taro/demo13.tsx
+++ src/packages/input/demos/taro/demo13.tsx
@@ -1,32 +1,45 @@
 import React from 'react'
-import { Input, Button } from '@nutui/nutui-react-taro'
+import { Button, Input } from '@nutui/nutui-react-taro'
 import { Tips } from '@nutui/icons-react-taro'
+import { Text, View } from '@tarojs/components'
+import { harmonyAndRn } from '@/utils/platform-taro'
+import pxTransform from '@/utils/px-transform'
 
 const Demo13 = () => {
   return (
     <>
-      <div
+      <View
         style={{
           display: 'flex',
+          flexWrap: 'nowrap',
           alignItems: 'center',
-          background: '#fff',
-          padding: '0 10px',
+          flexDirection: 'row',
+          backgroundColor: '#ffffff',
         }}
       >
-        <Tips />
+        {!harmonyAndRn() ? (
+          <Tips style={{ marginLeft: pxTransform(10) }} />
+        ) : null}
         <Input
           placeholder="请输入短信验证码"
+          placeholderTextColor="#757575"
           style={{ '--nutui-input-padding': '10px' }}
         />
-        <div
-          className="right"
-          style={{ display: 'flex', alignItems: 'center' }}
+        <View
+          style={{
+            display: 'flex',
+            width: pxTransform(100),
+            marginRight: pxTransform(10),
+            alignItems: 'center',
+          }}
         >
-          <Button type="primary" size="small">
-            获取验证码
+          <Button type="primary" size="small" style={{ flexShrink: 1 }}>
+            <Text style={{ fontSize: pxTransform(12), color: '#ffffff' }}>
+              获取验证码
+            </Text>
           </Button>
-        </div>
-      </div>
+        </View>
+      </View>
     </>
   )
 }
