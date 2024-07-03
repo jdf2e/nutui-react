9a0c1f37f286ae1384de544d2670bde0a904da62
522109452 <522109452@qq.com>
2024-07-03 15:54:43 +0800

    fix: 日历组件滚动到顶部时 出现空白问题

--- src/packages/calendaritem/calendaritem.taro.tsx
+++ src/packages/calendaritem/calendaritem.taro.tsx
@@ -532,6 +532,7 @@ export const CalendarItem = React.forwardRef<
     const scrollTop = (e.target as HTMLElement).scrollTop
     Taro.getEnv() === 'WEB' && setScrollTop(scrollTop)
     let current = Math.floor(scrollTop / avgHeight)
+    if (current < 0) return
     if (!monthsData[current + 1]) return
     const nextTop = monthsData[current + 1].scrollTop
     const nextHeight = monthsData[current + 1].cssHeight
