f980240b89f9a1ded271ba94239e4df03dc00f3c
oasis <12181600+oasis-cloud@users.noreply.github.com>
2024-07-03 20:29:19 +0800

    fix(form): validator 中的 value 类型更新为 any (#2406)
    * fix(form): validator 中的 value 类型更新为 any
    
    * fix(form): validator 中的 value 类型更新为 any

--- src/packages/form/demos/taro/demo1.tsx
+++ src/packages/form/demos/taro/demo1.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {
-  Form,
   Button,
-  InputNumber,
+  Form,
   Input,
+  InputNumber,
   TextArea,
 } from '@nutui/nutui-react-taro'
+import { FormItemRuleWithoutValidator } from '@/packages/formitem/types'
 
 const Demo1 = () => {
   return (
@@ -27,6 +28,14 @@ const Demo1 = () => {
           rules={[
             { max: 5, message: '字段A不能超过5个字' },
             { required: true, message: '请输入字段A' },
+            {
+              validator: (
+                ruleCfg: FormItemRuleWithoutValidator,
+                value: string
+              ) => {
+                return value.length > 5
+              },
+            },
           ]}
         >
           <Input
