cf4e118d83a59872b51e6e69eadc090e8231a8a0
EC <lyxuncle@users.noreply.github.com>
2024-07-22 17:47:24 +0800

    Feat/backtop 适配 JDRN、鸿蒙端 (#2374)
    * feat(backtop): 适配 JDRN，增加 JDRN demo
    
    * feat(backtop): 适配鸿蒙端
    
    * feat(backtop): 优化代码，补充文档
    
    * chore(backtop): 更新组件名
    
    * chore(backtop): 简化代码
    
    * chore(backtop): 优化代码
    
    * fix: merge
    
    ---------
    
    Co-authored-by: xiaoyatong <hanyuxingint@sina.cn>

--- src/packages/backtop/backtop.taro.tsx
+++ src/packages/backtop/backtop.taro.tsx
@@ -1,16 +1,34 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, {
+  FunctionComponent,
+  useCallback,
+  useState,
+  useMemo,
+  useEffect,
+  useRef,
+} from 'react'
 import type { MouseEvent } from 'react'
-import { usePageScroll, pageScrollTo } from '@tarojs/taro'
+import {
+  usePageScroll,
+  pageScrollTo,
+  PageScrollObject,
+  getSystemInfo,
+} from '@tarojs/taro'
 import { View, ITouchEvent } from '@tarojs/components'
 import { Top } from '@nutui/icons-react-taro'
 import classNames from 'classnames'
 import { BasicComponent, ComponentDefaults } from '@/utils/typings'
 import { useRtl } from '@/packages/configprovider/index.taro'
+import { harmonyAndRn, rn } from '@/utils/platform-taro'
+import pxTransform from '@/utils/px-transform'
 
 export interface BackTopProps extends BasicComponent {
   threshold: number
   zIndex: number
   duration: number
+  /**
+   * 容器滚动时的回调参数，主要用于 rn、鸿蒙端
+   */
+  scrollRes?: PageScrollObject
   onClick?: (event: React.MouseEvent<Element, MouseEvent> | ITouchEvent) => void
 }
 
@@ -21,11 +39,22 @@ const defaultProps = {
   duration: 1000,
 } as BackTopProps
 
+const isNative = harmonyAndRn()
+
 export const BackTop: FunctionComponent<
   Partial<BackTopProps> & Omit<React.HTMLAttributes<HTMLDivElement>, 'onClick'>
 > = (props) => {
   const rtl = useRtl()
-  const { children, threshold, zIndex, className, duration, style, onClick } = {
+  const {
+    children,
+    threshold,
+    zIndex,
+    className,
+    duration,
+    style,
+    scrollRes,
+    onClick,
+  } = {
     ...defaultProps,
     ...props,
   }
@@ -33,40 +62,72 @@ export const BackTop: FunctionComponent<
   const classPrefix = 'nut-backtop'
 
   const [backTop, SetBackTop] = useState(false)
+  const [isTouchStart, setTouchStart] = useState(false)
+  const systemInfo = useRef<getSystemInfo.Result>({})
+
+  useEffect(() => {
+    getSystemInfo().then((res) => {
+      systemInfo.current = res
+    })
+  }, [])
+
+  const handleActiveStart = useCallback(() => {
+    isNative && setTouchStart(true)
+  }, [])
+
+  const handleActiveEnd = useCallback(() => {
+    isNative && setTouchStart(false)
+  }, [])
+
+  const onScroll = useCallback(
+    (res: PageScrollObject) => {
+      const { scrollTop } = res
+      scrollTop >= threshold ? SetBackTop(true) : SetBackTop(false)
+    },
+    [threshold]
+  )
 
   // 监听用户滑动页面事件
-  usePageScroll((res) => {
-    const { scrollTop } = res
-    scrollTop >= threshold ? SetBackTop(true) : SetBackTop(false)
-  })
+  usePageScroll(onScroll)
+
+  useEffect(() => {
+    if (!scrollRes) return
+    onScroll(scrollRes)
+  }, [onScroll, scrollRes])
 
   // 返回顶部点击事件
-  const goTop = (e: React.MouseEvent<Element, MouseEvent> | ITouchEvent) => {
-    onClick && onClick(e)
-    pageScrollTo({
-      scrollTop: 0,
-      duration: duration > 0 ? duration : 0,
-    })
-  }
+  const goTop = useCallback(
+    (e: React.MouseEvent<Element, MouseEvent> | ITouchEvent) => {
+      onClick?.(e)
+      pageScrollTo({
+        scrollTop: 0,
+        duration: duration > 0 ? duration : 0,
+      })
+    },
+    [duration, onClick]
+  )
 
-  const styles =
-    Object.keys(style || {}).length !== 0
+  const styles = useMemo(() => {
+    return Object.keys(style || {}).length !== 0
       ? {
           zIndex,
           ...style,
         }
       : {
-          [rtl ? 'left' : 'right']: '10px',
-          bottom: '20px',
+          [rtl ? 'left' : 'right']: pxTransform(10),
+          bottom: pxTransform(20),
           zIndex,
         }
+  }, [rtl, style, zIndex])
 
   return (
     <View
       className={classNames(
         classPrefix,
         {
-          show: backTop,
+          [`${classPrefix}-show`]: backTop,
+          [`${classPrefix}-show-active`]: isNative && isTouchStart,
+          [`${classPrefix}-rn`]: rn(),
         },
         className
       )}
@@ -74,6 +135,9 @@ export const BackTop: FunctionComponent<
       onClick={(e) => {
         goTop(e)
       }}
+      onTouchStart={handleActiveStart}
+      onTouchEnd={handleActiveEnd}
+      onTouchCancel={handleActiveEnd}
     >
       {children || <Top size={19} className="nut-backtop-main" />}
     </View>
