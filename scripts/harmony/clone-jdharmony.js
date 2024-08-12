8a4dd7eca86e5602d7c63ee7a6ce11174f3b7159
huxiyang3 <1872591453@qq.com>
2024-08-12 20:04:23 +0800

    fix: remove

--- scripts/harmony/clone-jdharmony.js
+++ scripts/harmony/clone-jdharmony.js
@@ -9,7 +9,7 @@ const temp = `${process.cwd()}/packages`
 
 async function cloneJdHarmony() {
   // 填写coding地址
-  const remote = 'git@coding.jd.com:DongDesign/JDHarmony.git'
+  const remote = ''
   const branch = 'master'
   const git = simpleGit({
     baseDir: temp,
