f2a1fddad4c664a21684ff66f515e6c5cad9963b
huxiyang3 <1872591453@qq.com>
2024-08-21 11:16:05 +0800

    chore: update

--- src/packages/steps/demos/taro/demo8.tsx
+++ src/packages/steps/demos/taro/demo8.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
+import { View } from '@tarojs/components'
 import { Steps, Step } from '@nutui/nutui-react-taro'
 import pxTransform from '@/utils/px-transform'
 
 const Demo8 = () => {
   return (
-    <Steps
-      direction="vertical"
-      value={2}
-      style={{ height: pxTransform(300), padding: '15px 30px' }}
+    <View
+      style={{
+        height: pxTransform(300),
+        paddingTop: pxTransform(15),
+        paddingLeft: pxTransform(30),
+        paddingRight: pxTransform(30),
+      }}
     >
-      <Step
-        value={1}
-        title="已完成"
-        description="您的订单已经打包完成，商品已发出"
-      />
-      <Step value={2} title="进行中" description="您的订单正在配送途中" />
-      <Step
-        value={3}
-        title="未开始"
-        description="收货地址为：北京市经济技术开发区科创十一街18号院京东大厦"
-      />
-    </Steps>
+      <Steps direction="vertical" value={2}>
+        <Step
+          value={1}
+          title="已完成"
+          description="您的订单已经打包完成，商品已发出"
+        />
+        <Step value={2} title="进行中" description="您的订单正在配送途中" />
+        <Step
+          value={3}
+          title="未开始"
+          description="收货地址为：北京市经济技术开发区科创十一街18号院京东大厦"
+        />
+      </Steps>
+    </View>
   )
 }
 export default Demo8
--- src/packages/steps/demos/taro/demo9.tsx
+++ src/packages/steps/demos/taro/demo9.tsx
@@ -1,29 +1,37 @@
 import React from 'react'
-import { Text } from '@tarojs/components'
+import { Text, View } from '@tarojs/components'
 import { Steps, Step } from '@nutui/nutui-react-taro'
+import pxTransform from '@/utils/px-transform'
 
 const Demo9 = () => {
   return (
-    <Steps
-      direction="vertical"
-      dot
-      value={2}
-      style={{ height: '300px', padding: '15px 30px' }}
+    <View
+      style={{
+        height: pxTransform(300),
+        paddingTop: pxTransform(15),
+        paddingLeft: pxTransform(30),
+        paddingRight: pxTransform(30),
+      }}
     >
-      <Step
-        value={1}
-        title="已完成"
-        description="您的订单已经打包完成，商品已发出"
-      />
-      <Step value={2} title="进行中" description="您的订单正在配送途中" />
-      <Step
-        value={3}
-        title="未开始"
-        description={
-          <Text>收货地址为：北京市经济技术开发区科创十一街18号院京东大厦</Text>
-        }
-      />
-    </Steps>
+      <Steps direction="vertical" dot value={2}>
+        <Step
+          value={1}
+          title="已完成"
+          description="您的订单已经打包完成，商品已发出"
+        />
+        <Step value={2} title="进行中" description="您的订单正在配送途中" />
+        <Step
+          value={3}
+          title="未开始"
+          description={
+            <>
+              <Text>收货地址为：</Text>
+              <Text>北京市经济技术开发区科创十一街18号院京东大厦</Text>
+            </>
+          }
+        />
+      </Steps>
+    </View>
   )
 }
 export default Demo9
