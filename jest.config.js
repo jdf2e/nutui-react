4dab6203061221627ac226e4f23538ca7df7f883
huxiyang <1872591453@qq.com>
2024-08-27 21:11:21 +0800

    chore: update

--- jest.config.js
+++ jest.config.js
@@ -6,18 +6,20 @@ const defineJestConfig =
 
 module.exports = defineJestConfig({
   // testEnvironment: 'jsdom',  // 测试使用的环境
-  testEnvironment: 'jest-environment-jsdom', // 测试使用的环境
+  testEnvironment: 'jsdom', // 测试使用的环境
   testMatch: ['<rootDir>/src/packages/**/*.taro.test.{tsx,ts}'], // 测试文件匹配
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/packages/**/*.taro.tsx',
     '!<rootDir>/src/packages/**/demo.taro.tsx',
   ],
+  testTimeout: 150000,
   moduleNameMapper: {
     // ...
     '@tarojs/taro': '@tarojs/taro-h5',
     '^@/packages/(.*)$': '<rootDir>/src/packages/$1',
     '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@/locales/(.*)$': '<rootDir>/src/locales/$1',
     // '@tarojs/components': '@tarojs/components/lib/react',
     // '@tarojs/plugin-framework-react/dist/runtime': '<rootDir>/__mocks__/taro-framework',
     // '@tarojs/plugin-framework-vue2/dist/runtime': '<rootDir>/__mocks__/taro-framework',
