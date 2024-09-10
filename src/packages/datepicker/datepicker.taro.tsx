4641a135a6ac9d40c486edf53c8dfd9b459e92e1
huxiyang3 <1872591453@qq.com>
2024-09-10 21:50:06 +0800

    fix: save !!

--- src/packages/datepicker/datepicker.taro.tsx
+++ src/packages/datepicker/datepicker.taro.tsx
@@ -403,7 +403,7 @@ export const DatePicker: FunctionComponent<
       catchMove
       {...(rest as any)}
     >
-      {pickerOptions.length ? (
+      {!!pickerOptions.length && (
         <Picker
           {...pickerProps}
           title={title}
@@ -422,7 +422,7 @@ export const DatePicker: FunctionComponent<
           ) => handlePickerChange(options, value, index)}
           threeDimensional={threeDimensional}
         />
-      ) : null}
+      )}
     </View>
   )
 }
