55744891cf17bd916ad21d66257e33bd8478dc5e
Alex.huxiyang <1872591453@qq.com>
2024-09-10 21:44:40 +0800

    fix(props): add mergeProps utility to resolve all defaultProps warnings (#2581)
    * fix: defaultprops
    
    * fix: test about defaultProps
    
    ---------
    
    Co-authored-by: xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>

--- src/utils/merge-props.ts
+++ src/utils/merge-props.ts
@@ -1,7 +1,33 @@
-export function mergeProps<T, U>(
-  defaultProps: T,
-  props: U
-): { [Key in Extract<keyof T, keyof U>]: U[Key] } &
-  { [Key in Exclude<keyof U, keyof T>]?: U[Key] } {
-  return { ...defaultProps, ...props }
+export function mergeProps<A, B>(a: A, b: B): B & A
+export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A
+export function mergeProps<A, B, C, D>(a: A, b: B, c: C, d: D): D & C & B & A
+export function mergeProps(...items: any[]) {
+  const ret: any = {}
+  items.forEach((item) => {
+    if (item) {
+      Object.keys(item).forEach((key) => {
+        if (item[key] !== undefined) {
+          ret[key] = item[key]
+        }
+      })
+    }
+  })
+  return ret
+}
+
+/**
+ * Merge props and return the first non-undefined value.
+ * The later has higher priority. e.g. (10, 1, 5) => 5 wins.
+ * This is useful with legacy props that have been deprecated.
+ */
+export function mergeProp<T, DefaultT extends T = T>(
+  defaultProp: DefaultT,
+  ...propList: T[]
+): T | undefined {
+  for (let i = propList.length - 1; i >= 0; i -= 1) {
+    if (propList[i] !== undefined) {
+      return propList[i]
+    }
+  }
+  return defaultProp
 }
