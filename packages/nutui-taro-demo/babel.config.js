d234438eb33be08c3abcf4483de042eb796c8dec
jqroom <jqroom.com@gmail.com>
2024-10-28 15:55:56 +0800

    feat: 增加sourceLibrary参数配置

--- packages/nutui-taro-demo/babel.config.js
+++ packages/nutui-taro-demo/babel.config.js
@@ -1,6 +1,6 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-const repleaceIcons = require('@dongdesign/replace-icons')
+const replaceIcons = require('@dongdesign/replace-icons')
 
 module.exports = {
   presets: [
@@ -15,8 +15,9 @@ module.exports = {
   plugins: [
     ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
     [
-      repleaceIcons({
-        targetIconLibary: '@nutui/jdesign-icons-react-taro',
+      replaceIcons({
+        sourceLibrary: ['@nutui/icons-react-taro', '@nutui/icons-react'],
+        targetLibrary: '@nutui/jdesign-icons-react-taro',
       }),
     ],
   ],
