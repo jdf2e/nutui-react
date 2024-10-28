d55d0112eca1bebb64f0faf2f251af55eb8c43dd
jqroom <jqroom.com@gmail.com>
2024-10-28 14:44:16 +0800

    feat: 增加icon替换插件

--- packages/nutui-taro-demo/babel.config.js
+++ packages/nutui-taro-demo/babel.config.js
@@ -1,5 +1,7 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
+const repleaceIcons = require('@dongdesign/replace-icons')
+
 module.exports = {
   presets: [
     [
@@ -12,5 +14,10 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
+    [
+      repleaceIcons({
+        targetIconLibary: '@nutui/jdesign-icons-react-taro',
+      }),
+    ],
   ],
 }
