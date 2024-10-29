7761f63dd1478de442489be85bcc4c02220a863c
songchenglin3 <353833373@qq.com>
2024-10-29 14:17:29 +0800

    fix: 优化demo写法

--- src/packages/divider/demos/taro/demo6.tsx
+++ src/packages/divider/demos/taro/demo6.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Divider } from '@nutui/nutui-react-taro'
+import { View, Text } from '@tarojs/components'
 
 const Demo6 = () => {
   return (
-    <>
+    <View>
       文本
       <Divider direction="vertical" />
-      <a href="#" style={{ color: '#1989fa' }}>
+      <Text
+        onClick={() => {
+          console.log('跳转')
+        }}
+        style={{ color: '#1989fa', verticalAlign: 'middle' }}
+      >
         链接
-      </a>
+      </Text>
       <Divider direction="vertical" />
-      <a href="#" style={{ color: '#1989fa' }}>
+      <Text
+        onClick={() => {
+          console.log('跳转')
+        }}
+        style={{ color: '#1989fa', verticalAlign: 'middle' }}
+      >
         链接
-      </a>
-    </>
+      </Text>
+    </View>
   )
 }
 export default Demo6
