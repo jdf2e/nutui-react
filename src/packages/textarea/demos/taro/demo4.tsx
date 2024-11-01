d6121cb6c056f29a94df2d012c1391e15b62f3f0
hanyuxinting <hanyuxinting@sina.cn>
2024-11-01 17:53:33 +0800

    fix: 修改cr和 skeleton

--- src/packages/textarea/demos/taro/demo4.tsx
+++ src/packages/textarea/demos/taro/demo4.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { TextArea } from '@nutui/nutui-react-taro'
 
 const Demo4 = () => {
-  return <TextArea rows={1} autoSize maxLength={-1} />
+  return <TextArea autoSize maxLength={-1} />
 }
 export default Demo4
