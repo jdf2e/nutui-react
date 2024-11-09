146e4746a35509f77459440f8769414a705a1710
oasis <suanbanren@foxmail.com>
2024-11-09 20:40:51 +0800

    feat: radio adaptation

--- src/packages/radio/index.ts
+++ src/packages/radio/index.ts
@@ -1,5 +1,15 @@
-import { Radio } from './radio'
+import React from 'react'
+import { Radio, RadioProps } from './radio'
+import { RadioGroup } from '../radiogroup/radiogroup'
 
 export type { RadioProps } from './radio'
 export type { RadioShape, RadioPosition } from './types'
-export default Radio
+type CompoundedComponent = React.FC<
+  Partial<RadioProps> & Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'>
+> & {
+  Group: typeof RadioGroup
+}
+const InnerRadio = Radio as CompoundedComponent
+InnerRadio.Group = RadioGroup
+
+export default InnerRadio
--- src/packages/radiogroup/radiogroup.taro.tsx
+++ src/packages/radiogroup/radiogroup.taro.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react'
 import classNames from 'classnames'
+import { View } from '@tarojs/components'
 import {
   RadioGroupDirection,
   RadioGroupOption,
@@ -97,9 +98,9 @@ export const RadioGroup = React.forwardRef(
           },
         }}
       >
-        <div className={cls} {...rest}>
+        <View className={cls} {...rest}>
           {options?.length ? renderOptionsChildren() : children}
-        </div>
+        </View>
       </RadioContext.Provider>
     )
   }
