c02dae1f0ff865eb0edd664ede7f1fd71f433a56
songchenglin3 <353833373@qq.com>
2024-11-11 14:43:32 +0800

    fix(rate): 多端适配

--- src/packages/rate/demo.taro.tsx
+++ src/packages/rate/demo.taro.tsx
@@ -4,18 +4,18 @@ import { View, ScrollView } from '@tarojs/components'
 import { Cell } from '@nutui/nutui-react-taro'
 import Header from '@/sites/components/header'
 import { useTranslate } from '@/sites/assets/locale/taro'
-// import Demo1 from './demos/taro/demo1'
-// import Demo2 from './demos/taro/demo2'
-// import Demo3 from './demos/taro/demo3'
-// import Demo4 from './demos/taro/demo4'
-// import Demo5 from './demos/taro/demo5'
-// import Demo6 from './demos/taro/demo6'
-// import Demo7 from './demos/taro/demo7'
-// import Demo8 from './demos/taro/demo8'
-// import Demo9 from './demos/taro/demo9'
-// import Demo10 from './demos/taro/demo10'
-// import Demo11 from './demos/taro/demo11'
-// import Demo12 from './demos/taro/demo12'
+import Demo1 from './demos/taro/demo1'
+import Demo2 from './demos/taro/demo2'
+import Demo3 from './demos/taro/demo3'
+import Demo4 from './demos/taro/demo4'
+import Demo5 from './demos/taro/demo5'
+import Demo6 from './demos/taro/demo6'
+import Demo7 from './demos/taro/demo7'
+import Demo8 from './demos/taro/demo8'
+import Demo9 from './demos/taro/demo9'
+import Demo10 from './demos/taro/demo10'
+import Demo11 from './demos/taro/demo11'
+import Demo12 from './demos/taro/demo12'
 
 const RateDemo = () => {
   const [translated] = useTranslate({
@@ -73,10 +73,9 @@ const RateDemo = () => {
       <ScrollView className={`demo ${Taro.getEnv() === 'WEB' ? 'web' : ''}`}>
         <View className="h2">{translated.basic}</View>
         <Cell style={cellStyle}>
-          TODO：icon 问题需先解决
-          {/* <Demo1 /> */}
+          <Demo1 />
         </Cell>
-        {/*
+
         <View className="h2">{translated.control}</View>
         <Cell style={cellStyle}>
           <Demo2 />
@@ -130,7 +129,7 @@ const RateDemo = () => {
         <View className="h2">{translated.touchend}</View>
         <Cell style={cellStyle}>
           <Demo12 />
-        </Cell> */}
+        </Cell>
       </ScrollView>
     </>
   )
