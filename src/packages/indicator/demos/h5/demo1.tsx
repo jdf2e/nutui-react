ea770d1d685e06d202cc3936606c2f553fa9ed8e
oasis <suanbanren@foxmail.com>
2024-11-12 17:40:40 +0800

    feat(v14): indicator adaptation

--- src/packages/indicator/demos/h5/demo1.tsx
+++ src/packages/indicator/demos/h5/demo1.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { Indicator, Cell } from '@nutui/nutui-react'
+import React, { useState } from 'react'
+import { Cell, Indicator } from '@nutui/nutui-react'
 
 const Demo1 = () => {
+  const [current, setCurrent] = useState(0)
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCurrent(current + 1 >= 3 ? 0 : current + 1)
+  //   }, 1000)
+  // })
   return (
     <>
       <Cell>
         <Indicator total={3} current={0} />
+      </Cell>
+      <Cell>
+        <Indicator total={3} current={current} type="line" />
+      </Cell>
+      <Cell>
+        <Indicator
+          total={3}
+          current={current}
+          type="line"
+          direction="vertical"
+        />
+      </Cell>
+      <Cell>
+        <Indicator total={3} current={0} color="white" />
+      </Cell>
+      <Cell>
+        <Indicator total={3} current={0} type="line" color="white" />
+      </Cell>
+      <Cell>
+        <Indicator total={3} current={0} color="white" direction="vertical" />
+      </Cell>
+      <Cell>
+        <Indicator
+          total={3}
+          current={0}
+          type="line"
+          color="white"
+          direction="vertical"
+        />
       </Cell>
       <Cell>
         <Indicator total={3} current={1} />
--- src/packages/indicator/indicator.tsx
+++ src/packages/indicator/indicator.tsx
@@ -1,15 +1,23 @@
 import React, { FunctionComponent, ReactNode } from 'react'
 import classNames from 'classnames'
 
+export type IndicatorType = 'dot' | 'line'
+export type IndicatorColor = 'primary' | 'white'
+
 export interface IndicatorProps {
   total: number
   current: number
   direction: string
+  color: IndicatorColor
+  type: IndicatorType
 }
+
 const defaultProps = {
   total: 3,
   current: 0,
   direction: 'horizontal',
+  color: 'primary',
+  type: 'dot',
 } as IndicatorProps
 
 const classPrefix = `nut-indicator`
@@ -17,16 +25,26 @@ const classPrefix = `nut-indicator`
 export const Indicator: FunctionComponent<
   Partial<IndicatorProps> & React.HTMLAttributes<HTMLDivElement>
 > = (props) => {
-  const { total, current, children, className, direction, ...rest } = {
+  const {
+    color,
+    type,
+    total,
+    current,
+    children,
+    className,
+    direction,
+    ...rest
+  } = {
     ...defaultProps,
     ...props,
   }
   const classes = classNames({
     [`${classPrefix}-vertical`]: direction === 'vertical',
+    [`${classPrefix}-white`]: color === 'white',
+    [`${classPrefix}-track`]: type === 'line',
   })
-  const classPrefixV =
-    direction === 'vertical' ? `${classPrefix}-vertical` : classPrefix
-  const renderElement = () => {
+
+  const renderDotElement = () => {
     const childs: ReactNode[] = []
     for (let item = 0; item < total; item++) {
       childs.push(
@@ -34,22 +52,49 @@ export const Indicator: FunctionComponent<
           children || (
             <div
               key={item}
-              className={`${classPrefix}-dot ${classPrefix}-active ${classPrefixV}-dot ${classPrefixV}-active`}
+              className={`${classPrefix}-dot ${classPrefix}-dot-active`}
             />
           )
         ) : (
-          <div
-            key={item}
-            className={`${classPrefix}-dot ${classPrefixV}-dot`}
-          />
+          <div key={item} className={`${classPrefix}-dot`} />
         )
       )
     }
     return childs
   }
+  const renderLineElement = () => {
+    const childs: ReactNode[] = []
+    for (let item = 0; item < total; item++) {
+      childs.push(
+        item === 0 ? (
+          children || (
+            <div
+              key={item}
+              style={{
+                transform: `translateX(${current * 100}%)`,
+              }}
+              className={`${classPrefix}-line ${classPrefix}-line-active`}
+            />
+          )
+        ) : (
+          <div key={item} className={`${classPrefix}-line`} />
+        )
+      )
+    }
+    return childs
+  }
+  const renderByType = (type: IndicatorType) => {
+    switch (type) {
+      case 'line':
+        return renderLineElement()
+      default:
+        return renderDotElement()
+    }
+  }
+
   return (
     <div className={classNames(classPrefix, classes, className)} {...rest}>
-      {renderElement()}
+      {renderByType(type)}
     </div>
   )
 }
