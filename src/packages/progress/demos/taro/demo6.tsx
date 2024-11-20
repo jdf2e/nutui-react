1e8c5d8a8eb188bdcef1e5e5898c2a258c6f7762
hanyuxinting <hanyuxinting@sina.cn>
2024-11-20 20:30:58 +0800

    fix: 修改背景色

--- src/packages/progress/demos/taro/demo6.tsx
+++ src/packages/progress/demos/taro/demo6.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Progress, Cell } from '@nutui/nutui-react-taro'
 import { Checked, Tips } from '@nutui/icons-react-taro'
-import { harmonyAndRn } from '@/utils/platform-taro'
 import pxTransform from '@/utils/px-transform'
 
 const Demo6 = () => {
@@ -15,11 +14,7 @@ const Demo6 = () => {
       <Cell>
         <Progress
           percent={30}
-          color={
-            harmonyAndRn()
-              ? 'blue'
-              : 'linear-gradient(270deg, rgba(18,126,255,1) 0%,rgba(32,147,255,1) 32.815625%,rgba(13,242,204,1) 100%)'
-          }
+          color="linear-gradient(270deg, rgba(18,126,255,1) 0%,rgba(32,147,255,1) 32.815625%,rgba(13,242,204,1) 100%)"
           animated
         />
       </Cell>
@@ -28,7 +23,11 @@ const Demo6 = () => {
         <Checked color="green" style={iconStyle} />
       </Cell>
       <Cell align="center">
-        <Progress percent={100} color="#AAFF00" strokeWidth="15" />
+        <Progress
+          percent={100}
+          color="linear-gradient(90deg, rgba(180,236,81,1) 0%,rgba(66,147,33,1) 100%)"
+          strokeWidth="15"
+        />
         <Tips color="#FF0000" style={iconStyle} />
       </Cell>
     </>
--- src/packages/progress/progress.taro.tsx
+++ src/packages/progress/progress.taro.tsx
@@ -65,7 +65,7 @@ export const Progress: FunctionComponent<
 
   const stylesInner: React.CSSProperties = {
     width: `${displayPercent}%`,
-    backgroundColor: color || '#FF0F23',
+    background: color || '#FF0F23',
   }
 
   if (strokeWidth) {
