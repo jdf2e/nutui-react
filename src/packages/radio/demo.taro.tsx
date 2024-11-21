c08869acc4c3edc5be151d98ad5f3a667578046a
oasis <suanbanren@foxmail.com>
2024-11-21 19:36:06 +0800

    feat(radio): 无自定义尺寸的视觉

--- src/packages/radio/demo.taro.tsx
+++ src/packages/radio/demo.taro.tsx
@@ -10,7 +10,6 @@ import Demo3 from './demos/taro/demo3'
 import Demo4 from './demos/taro/demo4'
 import Demo5 from './demos/taro/demo5'
 import Demo6 from './demos/taro/demo6'
-import Demo7 from './demos/taro/demo7'
 import Demo8 from './demos/taro/demo8'
 import Demo9 from './demos/taro/demo9'
 import Demo10 from './demos/taro/demo10'
@@ -23,7 +22,6 @@ const RadioDemo = () => {
       '74fc5d8a': '基础用法',
       bb7486f4: '选项',
       c1bae1ec: '水平使用',
-      '8a2e2847': '自定义尺寸',
       '70ffa5d8': '自定义图标',
       '70ffa5d9': '自定义图标，通过Group实现列表形式',
       '0f261484': '触发事件',
@@ -37,7 +35,6 @@ const RadioDemo = () => {
       '74fc5d8a': '基礎用法',
       bb7486f4: '選項',
       c1bae1ec: '水準使用',
-      '8a2e2847': '自定義尺寸',
       '70ffa5d8': '自定義圖標',
       '70ffa5d9': '自定義圖標，通過Group實現列表形式',
       '0f261484': '觸發事件',
@@ -51,7 +48,6 @@ const RadioDemo = () => {
       '74fc5d8a': 'Basic Usage',
       bb7486f4: 'Options',
       c1bae1ec: 'Horizontal use',
-      '8a2e2847': 'Custom size',
       '70ffa5d8': 'Custom Icon',
       '70ffa5d9': 'Custom Icon, render list in Group mode',
       '0f261484': 'Trigger Event',
@@ -87,10 +83,6 @@ const RadioDemo = () => {
         </Cell>
         <View className="h2">{translated.c1bae1ec}</View>
         <Demo6 />
-        <View className="h2">{translated['8a2e2847']}</View>
-        <Cell>
-          <Demo7 />
-        </Cell>
         <View className="h2">{translated['70ffa5d8']}</View>
         <Cell>
           <Demo8 />
--- src/packages/radio/demo.tsx
+++ src/packages/radio/demo.tsx
@@ -7,7 +7,6 @@ import Demo3 from './demos/h5/demo3'
 import Demo4 from './demos/h5/demo4'
 import Demo5 from './demos/h5/demo5'
 import Demo6 from './demos/h5/demo6'
-import Demo7 from './demos/h5/demo7'
 import Demo8 from './demos/h5/demo8'
 import Demo9 from './demos/h5/demo9'
 import Demo10 from './demos/h5/demo10'
@@ -20,7 +19,6 @@ const RadioDemo = () => {
       '74fc5d8a': '基础用法',
       bb7486f4: '选项',
       c1bae1ec: '水平使用',
-      '8a2e2847': '自定义尺寸',
       '70ffa5d8': '自定义图标',
       '70ffa5d9': '自定义图标，通过Group实现列表形式',
       '0f261484': '触发事件',
@@ -34,7 +32,6 @@ const RadioDemo = () => {
       '74fc5d8a': '基礎用法',
       bb7486f4: '選項',
       c1bae1ec: '水準使用',
-      '8a2e2847': '自定義尺寸',
       '70ffa5d8': '自定義圖標',
       '70ffa5d9': '自定義圖標，通過Group實現列表形式',
       '0f261484': '觸發事件',
@@ -48,7 +45,6 @@ const RadioDemo = () => {
       '74fc5d8a': 'Basic Usage',
       bb7486f4: 'Options',
       c1bae1ec: 'Horizontal use',
-      '8a2e2847': 'Custom size',
       '70ffa5d8': 'Custom Icon',
       '70ffa5d9': 'Custom Icon, render list in Group mode',
       '0f261484': 'Trigger Event',
@@ -83,10 +79,6 @@ const RadioDemo = () => {
         </Cell>
         <h2>{translated.c1bae1ec}</h2>
         <Demo6 />
-        <h2>{translated['8a2e2847']}</h2>
-        <Cell>
-          <Demo7 />
-        </Cell>
         <h2>{translated['70ffa5d8']}</h2>
         <Cell>
           <Demo8 />
