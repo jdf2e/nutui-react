7ea36196a27720f8ab34fddb0290969282d2b832
Alex.hxy <1872591453@qq.com>
2024-12-02 20:17:30 +0800

    chore: button text v14

--- src/packages/empty/demos/h5/demo1.tsx
+++ src/packages/empty/demos/h5/demo1.tsx
@@ -7,7 +7,7 @@ const Demo1 = () => {
       <Empty
         title="全屏缺省标题"
         description="内容描述内容描述内容描述"
-        actions={[{ text: '操作按钮' }, { text: '操作按钮', type: 'primary' }]}
+        actions={[{ text: '次要操作' }, { text: '主要操作', type: 'primary' }]}
       />
       <Empty
         description="内容描述内容描述内容描述"
