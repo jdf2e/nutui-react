808e9cc00340191c51c44fec9a2ff381190bed9f
hanyuxinting <hanyuxinting@sina.cn>
2024-12-09 16:34:13 +0800

    fix: popup 不该是 overflow-y auto

--- src/packages/cascader/cascader.taro.tsx
+++ src/packages/cascader/cascader.taro.tsx
@@ -483,7 +483,6 @@ const InternalCascader: ForwardRefRenderFunction<
           {...popupProps}
           visible={innerVisible}
           position="bottom"
-          style={{ overflowY: 'hidden' }}
           round
           closeIcon={closeIcon}
           closeable={closeable}
--- src/packages/popup/demos/taro/demo2.tsx
+++ src/packages/popup/demos/taro/demo2.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Popup, Cell } from '@nutui/nutui-react-taro'
-import { ScrollView, View, Text } from '@tarojs/components'
+import { ScrollView, Text } from '@tarojs/components'
 
 const Demo2 = () => {
   const [showTop, setShowTop] = useState(false)
@@ -58,17 +58,7 @@ const Demo2 = () => {
           setShowBottom(false)
         }}
         lockScroll
-      >
-        <ScrollView scrollY style={{ height: '240px' }}>
-          {Array.from({ length: 200 })
-            .fill('')
-            .map((_, i) => (
-              <View key={i}>
-                <Cell>底部弹出-{i}</Cell>
-              </View>
-            ))}
-        </ScrollView>
-      </Popup>
+      />
       <Popup
         visible={showLeft}
         style={{ width: '20%' }}
