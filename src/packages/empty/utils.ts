3a76df7146b81d9cc1c1efc1b106c3ec01459993
oasis-cloud <suanbanren@foxmail.com>
2024-12-11 17:25:38 +0800

    fix: actions type

--- src/packages/empty/utils.ts
+++ src/packages/empty/utils.ts
@@ -4,5 +4,5 @@ export const getButtonType = (actions: Array<EmptyAction>, index: number) => {
   if (!actions || actions.length === 0) return 'default'
   const action = actions[index]
   if (action.type) return action.type
-  actions.length > 1 && index === 0 ? 'default' : 'primary'
+  return actions.length > 1 && index === 0 ? 'default' : 'primary'
 }
