966a829e83db60c8c51f2b2df40c5f8b3c57159c
oasis <suanbanren@foxmail.com>
2024-12-23 19:34:17 +0800

    feat(navbar): v15

--- src/packages/navbar/demos/h5/demo2.tsx
+++ src/packages/navbar/demos/h5/demo2.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavBar, Toast } from '@nutui/nutui-react'
-import { Share, More, Cart, ArrowLeft, Close } from '@nutui/icons-react'
+import { ArrowLeft, Close, More, Share } from '@nutui/icons-react'
 
 const Demo2 = () => {
   const styles = {
@@ -23,35 +23,24 @@ const Demo2 = () => {
   return (
     <>
       <NavBar
-        titleAlign="left"
         back={
           <>
             <ArrowLeft />
             返回
           </>
         }
-        right={
-          <span style={styles.flexCenter} onClick={(e) => Toast.show('icon')}>
-            <Share />
-          </span>
-        }
+        right={<Share onClick={(e) => Toast.show('icon')} />}
         onBackClick={(e) => Toast.show('返回')}
       >
-        订单详情
+        页面标题
       </NavBar>
       <NavBar
-        titleAlign="left"
-        right={
-          <span style={styles.flexCenter} onClick={(e) => Toast.show('icon')}>
-            <Share />
-          </span>
-        }
+        right={<Share onClick={(e) => Toast.show('icon')} />}
         onBackClick={(e) => Toast.show('返回')}
       >
-        订单详情
+        页面标题
       </NavBar>
       <NavBar
-        titleAlign="left"
         right={<span onClick={(e) => Toast.show('清空')}>清空</span>}
         left={<Close />}
         back={<ArrowLeft />}
@@ -59,34 +48,22 @@ const Demo2 = () => {
       >
         <div style={{ ...styles.flexCenter, flexDirection: 'column' }}>
           <span style={styles.title} onClick={(e) => Toast.show('标题')}>
-            浏览记录
+            页面标题
           </span>
-          <span style={styles.description}>浏览记录</span>
+          <span style={styles.description}>副标题</span>
         </div>
       </NavBar>
       <NavBar
-        titleAlign="left"
         back={<ArrowLeft />}
         right={
           <>
-            <span
-              style={{ marginRight: '5px' }}
-              onClick={(e) => Toast.show('编辑')}
-            >
-              编辑
-            </span>
+            <span onClick={(e) => Toast.show('编辑')}>编辑</span>
             <More onClick={(e) => Toast.show('icon')} />
           </>
         }
         onBackClick={(e) => Toast.show('返回')}
       >
-        <span onClick={(e) => Toast.show('标题')}>购物车</span>
-        <i
-          style={{ ...styles.flexCenter, margin: '0 5px' }}
-          onClick={(e) => Toast.show('icon')}
-        >
-          <Cart />
-        </i>
+        <span onClick={(e) => Toast.show('页面标题')}>页面标题</span>
       </NavBar>
     </>
   )
