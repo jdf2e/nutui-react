35916b28792a7ec3e8b70e7fde086c7e590da84f
oasis <suanbanren@foxmail.com>
2024-12-30 17:10:27 +0800

    feat(pulltorefresh): v15

--- src/packages/pulltorefresh/pulltorefresh.taro.tsx
+++ src/packages/pulltorefresh/pulltorefresh.taro.tsx
@@ -39,7 +39,7 @@ const defaultProps = {
   completeText: '',
   completeDelay: 500,
   disabled: false,
-  headHeight: 64 + 16,
+  headHeight: 80,
   threshold: 60,
   scrollTop: 0,
   onRefresh: () => {},
