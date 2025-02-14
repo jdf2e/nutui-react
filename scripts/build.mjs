765088be7d4607e5b0732b6edf311ef36c356d9a
oasis <suanbanren@foxmail.com>
2025-02-14 20:08:12 +0800

    chore(release): v3.0.0-beta.14

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -360,6 +360,10 @@ function generateReleasePackageJson() {
     main: packageJson.main,
     module: packageJson.module,
     typings: packageJson.typings,
+    scripts: {
+      "publish:beta": "npm publish --tag=beta",
+      "publish:latest": "npm publish"
+    },
     sideEffects: packageJson.sideEffects,
     description: packageJson.description,
     keywords: packageJson.keywords,
