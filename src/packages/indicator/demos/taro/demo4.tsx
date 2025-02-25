136294b278c30eb3e4ff8b48532f937a2fe0c10a
oasis <suanbanren@foxmail.com>
2025-02-25 14:35:47 +0800

    fix: indicator

--- src/packages/indicator/demos/taro/demo4.tsx
+++ src/packages/indicator/demos/taro/demo4.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Cell, Indicator } from '@nutui/nutui-react-taro'
 import { View } from '@tarojs/components'
-import { harmony } from '@/utils/platform-taro'
 import pxTransform from '@/utils/px-transform'
 
 const Demo4 = () => {
@@ -10,47 +9,21 @@ const Demo4 = () => {
       <Cell>
         <Indicator total={6} current={5} direction="vertical">
           <View
-            style={
-              !harmony()
-                ? {
-                    display: 'inline-block',
-                    width: '14px',
-                    height: '14px',
-                    lineHeight: '14px',
-                    textAlign: 'center',
-                    fontSize: '12px',
-                    color: '#FFFFFF',
-                    border: '1px solid #FFFFFF',
-                    borderRadius: '50%',
-                    margin: '4px',
-                    backgroundColor: 'var(--nutui-color-primary)',
-                    boxShadow: `0 0 1px 1px var(--nutui-color-primary)`,
-                  }
-                : {
-                    display: 'inline-block',
-                    width: pxTransform(14),
-                    height: pxTransform(14),
-                    lineHeight: pxTransform(14),
-                    textAlign: 'center',
-                    fontSize: pxTransform(12),
-                    color: '#FFFFFF',
-                    borderWidth: 1,
-                    borderColor: '#FFFFFF',
-                    borderRadius: pxTransform(14),
-                    margin: 4,
-                    backgroundColor: '#ff0f23',
-                    // @ts-ignore
-                    shadowColor: '#ff0f23',
-                    shadowOffset: {
-                      width: 0,
-                      height: 0,
-                    },
-                    shadowOpacity: 1,
-                    shadowRadius: 1,
-                  }
-            }
+            style={{
+              display: 'flex',
+              width: pxTransform(14),
+              height: pxTransform(14),
+              justifyContent: 'center',
+              fontSize: pxTransform(12),
+              color: '#FFFFFF',
+              borderWidth: pxTransform(1),
+              borderColor: '#FFFFFF',
+              borderRadius: pxTransform(14),
+              margin: pxTransform(4),
+              backgroundColor: '#ff0f23',
+            }}
           >
-            {5}
+            5
           </View>
         </Indicator>
         <Indicator
