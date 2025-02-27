6befb47b8fd4e38dd538ddc1e9cf6b301b44e86e
songchenglin3 <353833373@qq.com>
2025-02-27 15:34:44 +0800

    fix: demo丰富

--- src/packages/datepickerview/demo.taro.tsx
+++ src/packages/datepickerview/demo.taro.tsx
@@ -4,25 +4,32 @@ import { ScrollView, View } from '@tarojs/components'
 import { useTranslate } from '@/sites/assets/locale/taro'
 import Header from '@/sites/components/header'
 import Demo1 from './demos/taro/demo1'
+import Demo2 from './demos/taro/demo2'
 
 const DatePickerViewDemo = () => {
   const [translated] = useTranslate({
     'zh-CN': {
-      title: '基础用法',
+      basic: '选择日期',
+      mmdd: '选择月日',
+    },
+
+    'zh-TW': {
+      basic: '選擇日期',
+      mmdd: '選擇月日',
     },
     'en-US': {
-      title: 'Basic Usage',
-    },
-    'zh-TW': {
-      title: '基礎用法',
+      basic: 'Choose Date',
+      mmdd: 'Choose Month-Day',
     },
   })
   return (
     <>
       <Header />
       <ScrollView className={`demo ${Taro.getEnv() === 'WEB' ? 'web' : ''}`}>
-        <View className="h2">{translated.title}</View>
+        <View className="h2">{translated.basic}</View>
         <Demo1 />
+        <h2>{translated.mmdd}</h2>
+        <Demo2 />
       </ScrollView>
     </>
   )
--- src/packages/datepickerview/demo.tsx
+++ src/packages/datepickerview/demo.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 import { useTranslate } from '@/sites/assets/locale'
 import Demo1 from './demos/h5/demo1'
 import Demo2 from './demos/h5/demo2'
-import Demo3 from './demos/h5/demo3'
-import Demo4 from './demos/h5/demo4'
-import Demo5 from './demos/h5/demo5'
-import Demo6 from './demos/h5/demo6'
-import Demo7 from './demos/h5/demo7'
-import Demo8 from './demos/h5/demo8'
 
 const DatePickerViewDemo = () => {
   const [translated] = useTranslate({
@@ -51,17 +45,6 @@ const DatePickerViewDemo = () => {
         <h2>{translated.mmdd}</h2>
         <Demo2 />
         <h2>{translated.showAll}</h2>
-        <Demo3 />
-        <h2>{translated.time}</h2>
-        <Demo4 />
-        <h2>{translated.hourMinutes}</h2>
-        <Demo5 />
-        <h2>{translated.format}</h2>
-        <Demo6 />
-        <h2>{translated.stepMins}</h2>
-        <Demo7 />
-        <h2>{translated.filter}</h2>
-        <Demo8 />
       </div>
     </>
   )
