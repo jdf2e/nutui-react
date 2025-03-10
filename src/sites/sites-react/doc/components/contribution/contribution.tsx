368c7bdac55d9d237d72d0bc72fe7c0b12165f08
Alex.hxy <1872591453@qq.com>
2025-03-10 15:51:36 +0800

    fix: 移除emoji

--- src/sites/sites-react/doc/components/contribution/contribution.tsx
+++ src/sites/sites-react/doc/components/contribution/contribution.tsx
@@ -53,7 +53,8 @@ const Contribution: FunctionComponent<ContributionMDXProps> = (props) => {
             >
               {item.content
                 .replace(/@[^\s]+$/, '')
-                .replace(/\(\[(#\d+)\]\([^)]+\)\)/, '$1')}
+                .replace(/\(\[(#\d+)\]\([^)]+\)\)/, '$1')
+                .replace(/[\u{1F300}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '')}
             </a>
             <code style={{fontSize: 14, color: '#666'}}>{item.version}</code>
           </li>
