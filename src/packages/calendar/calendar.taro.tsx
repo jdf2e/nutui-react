7d44b518b35ebb9aa022352d90bca611a4b710d5
hanyuxinting <hanyuxinting@sina.cn>
2025-03-13 16:58:59 +0800

    fix: type

--- src/packages/calendar/calendar.taro.tsx
+++ src/packages/calendar/calendar.taro.tsx
@@ -161,7 +161,6 @@ export const Calendar = React.forwardRef<
             scrollAnimation={scrollAnimation}
             renderDay={renderDay}
             onItemClick={onItemClick}
-            onPageChange={yearMonthChange}
           />
         ) : (
           <CalendarItem
