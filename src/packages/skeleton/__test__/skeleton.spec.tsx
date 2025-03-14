01ed58a77338b6f8ceedee570beda9d5e386adc2
oasis <suanbanren@foxmail.com>
2025-03-14 12:57:15 +0800

    test: update

--- src/packages/skeleton/__test__/skeleton.spec.tsx
+++ src/packages/skeleton/__test__/skeleton.spec.tsx
@@ -8,17 +8,3 @@ test('should allow to disable animation', () => {
   const { container } = render(<Skeleton rows={1} animated={false} />)
   expect(container.querySelector('.skeleton-animation')).toBeFalsy()
 })
-
-test('should change avatar size when using avatarSize prop', () => {
-  const { container } = render(<Skeleton avatar avatarSize="20px" />)
-  expect(container.querySelector('.nut-avatar')).toHaveStyle({
-    width: '20px',
-    height: '20px',
-  })
-  expect(container).toMatchSnapshot()
-})
-
-test('should change avatar shape when using avatarShape prop', () => {
-  const { container } = render(<Skeleton avatar avatarShape="square" />)
-  expect(container.querySelector('.nut-avatar')).toMatchSnapshot()
-})
