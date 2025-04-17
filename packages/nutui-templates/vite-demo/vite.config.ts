8468ae23ab929423219c78cc37a6dc3ef7a900ce
oasis <suanbanren@foxmail.com>
2025-04-17 11:31:45 +0800

    docs: 主题使用的文档更新

--- packages/nutui-templates/vite-demo/vite.config.ts
+++ packages/nutui-templates/vite-demo/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import vitePluginImp from 'vite-plugin-imp'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +11,24 @@ export default defineConfig({
         {
           libName: '@nutui/nutui-react',
           style: (name) => {
-            return `@nutui/nutui-react/dist/esm/${name}/style/css`
+            return `@nutui/nutui-react/dist/es/packages/${name.toLowerCase()}/style/index`
+            // return `@nutui/nutui-react/dist/es/packages/${name.toLowerCase()}/style-jmapp/index`
+            // return `@nutui/nutui-react/dist/es/packages/${name.toLowerCase()}/style-jmapp/index`
           },
           replaceOldImport: false,
           camel2DashComponentName: false,
-        }
-      ]
-    })
+        },
+      ],
+    }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // 或 "modern"，"legacy"
+        additionalData: `@import '@nutui/nutui-react/dist/styles/variables.scss';`
+        // jmapp 需要在 app 入口引入 import '@nutui/nutui-react/dist/styles/themes/jmapp.css'
+        // additionalData: `@import '@nutui/nutui-react/dist/styles/variables-jmapp.scss';`
+      }
+    }
+  },
 })
--- src/sites/sites-react/doc/docs/taro/official-theme-react.en-US.md
+++ src/sites/sites-react/doc/docs/taro/official-theme-react.en-US.md
@@ -9,7 +9,7 @@ NutUI provides multiple sets of official `UI` themes by default, while allowing 
 | theme description | scss file name |
 | --- | --- |
 | Jingdong APP 10.0 theme (default) | `variables.scss` |
-| Jingdong JDesign Theme | `variables-jdb.scss ` |
+| Jingdong B client Theme | `variables-jdb.scss ` |
 
 ## How to use
 
@@ -19,7 +19,7 @@ It is important to note that when configuring the theme, you also need to import
 | --- | --- |
 | JD APP Theme (Light Mode) | `@nutui/nutui-react-taro/dist/styles/themes/default.css` |
 | JD APP Theme (Dark Mode) | `@nutui/nutui-react-taro/dist/styles/themes/dark.css` |
-| JD JDesign Theme | `@nutui/nutui-react-taro/dist/styles/themes/jmapp.css` |
+| JD B client Theme | `@nutui/nutui-react-taro/dist/styles/themes/jmapp.css` |
 | JD JRKF Theme | `@nutui/nutui-react-taro/dist/styles/themes/jrkf.css` |
 
 Currently, only the default theme provides support for dark mode.
