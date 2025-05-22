6c7e0eb2bec1a9459ff4a5f09802400393c3d11f
hanyuxinting <hanyuxinting@sina.cn>
2025-05-22 17:43:06 +0800

    fix(configprovider): 调整demo展示

--- src/packages/configprovider/demos/h5/demo1.tsx
+++ src/packages/configprovider/demos/h5/demo1.tsx
@@ -2,9 +2,26 @@ import React from 'react'
 import { ConfigProvider, Cell, Rate, Button } from '@nutui/nutui-react'
 
 const Demo1 = () => {
+  const darkTheme = {
+    nutuiColorPrimaryIcon: 'green',
+    nutuiColorPrimaryStop1: 'green',
+    nutuiColorPrimaryStop2: 'green',
+  }
   return (
     <>
       <ConfigProvider>
+        <Cell.Group>
+          <Cell>
+            <Rate defaultValue={3} />
+          </Cell>
+          <Cell>
+            <Button type="primary" block>
+              提交
+            </Button>
+          </Cell>
+        </Cell.Group>
+      </ConfigProvider>
+      <ConfigProvider theme={darkTheme}>
         <Cell.Group>
           <Cell>
             <Rate defaultValue={3} />
