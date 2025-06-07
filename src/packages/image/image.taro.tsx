69490d5cfcd35992a7ef6deb6ecbbe49c618a412
Alex.hxy <1872591453@qq.com>
2025-06-07 15:16:33 +0800

    fix(tabs): css变量修复

--- src/packages/image/image.taro.tsx
+++ src/packages/image/image.taro.tsx
@@ -1,9 +1,4 @@
-import React, {
-  CSSProperties,
-  FunctionComponent,
-  useCallback,
-  useState,
-} from 'react'
+import React, { FunctionComponent, useCallback, useState } from 'react'
 import Taro from '@tarojs/taro'
 import {
   Image as TImage,
@@ -59,7 +54,6 @@ export const Image: FunctionComponent<Partial<TaroImageProps>> = (props) => {
   }
 
   const containerStyle = {
-    ...(style as CSSProperties),
     ...(height ? { height: pxCheck(height) } : {}),
     ...(width ? { width: pxCheck(width) } : {}),
     ...(radius !== undefined && radius !== null
