83a111ccab6605b6a9b1707cee7c81db30d4ca72
jqroom <jqroom.com@gmail.com>
2025-08-29 17:13:28 +0800

    feat: 调整文件结构

--- src/packages/table/index.virtual.ts
+++ src/packages/table/index.virtual.ts
@@ -2,13 +2,15 @@ import {
   TableVirtual,
   VirtualTableRef,
   VirtualTableProps,
-} from './table-virtual'
-import { useVirtualScroll } from './virtual-scroll'
+  useVirtualScroll,
+  useThrottle,
+  useDebounce,
+} from './virtual'
 import { TableColumnProps } from '@/types/spec/table/base'
 
 // 导出TableVirtual组件，使其能够接收正确的props
 const TableVirtualWrapper = TableVirtual
 
-export { useVirtualScroll }
+export { useVirtualScroll, useThrottle, useDebounce }
 export type { TableColumnProps, VirtualTableRef, VirtualTableProps }
 export default TableVirtualWrapper
