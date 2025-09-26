a17771b8c3d85216ce8e677b00ee8a5e316c124c
hanyuxinting <hanyuxinting@sina.cn>
2025-09-26 23:15:39 +0800

    feat: 鸿蒙适配td

--- src/packages/input/input.taro.tsx
+++ src/packages/input/input.taro.tsx
@@ -7,13 +7,26 @@ import React, {
 } from 'react'
 import { Input as TaroInput, View } from '@tarojs/components'
 import { MaskClose } from '@nutui/icons-react-taro'
-import Taro, { ENV_TYPE, getEnv } from '@tarojs/taro'
+import Taro, { getEnv } from '@tarojs/taro'
 import { BaseEventOrig } from '@tarojs/components/types/common'
 import { formatNumber } from './utils'
 import { useConfig, useRtl } from '@/packages/configprovider/index.taro'
 import { ComponentDefaults } from '@/utils/typings'
 import { usePropsValue } from '@/hooks/use-props-value'
 import { InputFormatTrigger, TaroInputProps } from '@/types'
+
+const ENV_TYPE = {
+  WEAPP: 'WEAPP',
+  SWAN: 'SWAN',
+  ALIPAY: 'ALIPAY',
+  TT: 'TT',
+  QQ: 'QQ',
+  JD: 'JD',
+  WEB: 'WEB',
+  RN: 'RN',
+  HARMONY: 'HARMONY',
+  QUICKAPP: 'QUICKAPP',
+}
 
 const defaultProps = {
   ...ComponentDefaults,
