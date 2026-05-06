b2d3e88e5b79fdc6dab88209659d4e73884ce412
hanyuxinting <hanyuxinting@sina.cn>
2026-05-06 21:07:08 +0800

    fix: 兼容taro多端处理

--- src/packages/calendarcard/icon.taro.tsx
+++ src/packages/calendarcard/icon.taro.tsx
@@ -1,34 +1,32 @@
 import React, { FC } from 'react'
 import { Image } from '@tarojs/components'
-import { pxTransform } from '@nutui/nutui-react-taro'
-import leftSVG from './icons/singleLeft.svg'
-import rightSVG from './icons/singleRight.svg'
-import doubleLeftSVG from './icons/doubleLeft.svg'
-import doubleRightSVG from './icons/doubleRight.svg'
-
-let left = leftSVG as string
-let right = rightSVG as string
-let doubleLeft = doubleLeftSVG as string
-let doubleRight = doubleRightSVG as string
 
 interface IconProps {
   url: string
 }
 
-if (process.env.TARO_ENV === 'jdharmony_cpp') {
-  left =
-    'https://img11.360buyimg.com/imagetools/jfs/t1/187031/35/51586/196/6731c464F9b9f8f00/5506e32bf15e29dc.png'
-  right =
-    'https://img12.360buyimg.com/imagetools/jfs/t1/181006/25/51824/185/6731c452F9e252322/7493147dd6b4a88d.png'
-  doubleLeft =
-    'https://img13.360buyimg.com/imagetools/jfs/t1/221244/29/45876/259/6731c6bdF515df0a2/624e8eee3c8494a2.png'
-  doubleRight =
-    'https://img11.360buyimg.com/imagetools/jfs/t1/238382/25/24000/235/6731c6bdF0153286c/4a57e60b6e889af3.png'
-}
+const left =
+  'https://storage.360buyimg.com/imgtools/6437a2b149-bb511b40-4942-11f1-b1e1-7158b9aae0a9.svg'
+const right =
+  'https://storage.360buyimg.com/imgtools/8aa060341d-baf78990-4942-11f1-9368-cd56fc5fadc6.svg'
+const doubleLeft =
+  'https://storage.360buyimg.com/imgtools/b5e798f4ed-bbe8c760-4942-11f1-b1e1-7158b9aae0a9.svg'
+const doubleRight =
+  'https://storage.360buyimg.com/imgtools/d6979fa16d-bbb4bf10-4942-11f1-9368-cd56fc5fadc6.svg'
 
 const Icon: FC<IconProps> = ({ url }) => {
-  const iconStyle = { width: pxTransform(18), height: pxTransform(18) }
-  return <Image src={url} mode="aspectFit" style={iconStyle} />
+  const iconStyle = {
+    color: 'var(--nutui-color-text)',
+  }
+  return (
+    <Image
+      className="nut-calendarcard-icon"
+      svg
+      src={url}
+      mode="aspectFit"
+      style={iconStyle}
+    />
+  )
 }
 
 export const ArrowLeft: FC = () => <Icon url={left} />
